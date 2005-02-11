72094e310c79e913542afa0e73b50888605ee0ab
tg <>
2005-02-11 14:34:13 +0000

    sync

--- src/gnu/share/locations.mk
+++ src/gnu/share/locations.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: src/gnu/share/locations.mk,v 1.1.7.1 2005/02/11 14:29:40 tg Exp $
 
 # for BSDSRCDIR
 .include <bsd.own.mk>
@@ -15,12 +15,10 @@ GNU_BINUTILS_SRC?=	${GNU_ROOTDIR}/usr.bin/binutils
 GNU_BINUTILS_AS=	${GNU_BINUTILS_SRC}/gas
 GNU_BINUTILS_BFD=	${GNU_BINUTILS_SRC}/bfd
 GNU_BINUTILS_BU=	${GNU_BINUTILS_SRC}/binutils
-GNU_BINUTILS_LD=	${GNU_BINUTILS_SRC}/ld
-GNU_BINUTILS_OPC=	${GNU_BINUTILS_SRC}/opcodes
-
 # not ready yet
 GNU_BINUTILS_GDB=	${GNU_BINUTILS_SRC}/gdb
-GNU_BINUTILS_GPROF=	${GNU_BINUTILS_SRC}/gprof
+GNU_BINUTILS_LD=	${GNU_BINUTILS_SRC}/ld
+GNU_BINUTILS_OPC=	${GNU_BINUTILS_SRC}/opcodes
 
 # gcc3-core location
 GNU_COREGCC_SRC?=	${BSDSRCDIR}/gcc/gcc
@@ -28,9 +26,6 @@ GNU_COREGCC_SRC?=	${BSDSRCDIR}/gcc/gcc
 # libiberty and binutils includes
 GNU_LIBIBERTY_SRC?=	${GNU_BINUTILS_SRC}/libiberty
 GNU_LIBIBERTY_INC?=	${GNU_BINUTILS_SRC}/include
-
-# libreadline
-GNU_READLINE_SRC?=	${GNU_BINUTILS_SRC}/readline
 
 # contrib_dir for ports/infrastructure/db et al.
 GNU_AUX_CONTRIB_DIR?=	${BSDSRCDIR}/contrib
