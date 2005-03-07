8c9b572a56c9eed8087532c1d7b7005becd302c0
tg <>
2005-03-07 15:14:20 +0000

    no commit message, it might violate a patent

--- www/index.php
+++ www/index.php
@@ -1,92 +1,40 @@
 <?php
-/* $MirOS: www/index.php,v 1.15 2005/01/09 11:55:47 tg Exp $
- *-
- * Copyright (c) 1999, 2002, 2003, 2005
+/* $MirOS: src/share/misc/licence.template,v 1.2 2005/03/03 19:43:30 tg Rel $ */
+
+/*-
+ * Copyright (c) 2005
  *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
- * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Licensee is hereby permitted to deal in this work without restric-
+ * tion, including unlimited rights to use, publicly perform, modify,
+ * merge, distribute, sell, give away or sublicence, provided all co-
+ * pyright notices above, these terms and the disclaimer are retained
+ * in all redistributions or reproduced in accompanying documentation
+ * or other materials provided with binary redistributions.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
- * 3. All marketing or advertising materials mentioning features or
- *    use of this product must display the following acknowledgement:
- *    * This product includes material provided by Thorsten Glaser.
- * 4. Neither the name of Thorsten Glaser or the MirOS project nor
- *    the names of their contributors may be used to endorse or pro-
- *    mote products derived from this software without specific prior
- *    written permission.
- * 5. If this work is used on world wide web pages, the authors must
- *    be given due credits in the site's imprint.
- * 6. This licence automatically ends if the site of licensee is not
- *    easily distinguishable from Thorsten Glaser's personal homepage
- *    or the MirOS project's site.
- * 7. This licence does not cover usage of the pictures used on the
- *    pages, especially not the BSD Dæmon or the MirOS/MirAbile logo.
+ * All advertising materials mentioning features or use of this soft-
+ * ware must display the following acknowledgement:
+ *	This product includes material provided by Thorsten Glaser.
  *
- * This work is provided "as is" with no explicit or implicit warran-
- * ties whatsoever to the maximum extend permitted by applicable law;
- * in no event may an author or contributor be held liable for damage
- * that is, directly or indirectly, caused by the work, even if advi-
- * sed of the possibility of such damage.
+ * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
+ * any kind, expressed or implied, to the maximum extent permitted by
+ * applicable law, but with the warranty of being written without ma-
+ * licious intent or gross negligence; in no event shall licensor, an
+ * author or contributor be held liable for any damage, direct, indi-
+ * rect or other, however caused, arising in any way out of the usage
+ * of this work, even if advised of the possibility of such damage.
  *-
- * This is the "master" CMS manager file. It streams content.
+ * I hope neither the licence terms above nor these few lines violate
+ * an existing software patent.
  */
 
-	require("vfuncs.php");
+header("Content-Type: text/plain; charset=ISO-646.irv:1991");
 
-	if (!empty($_SERVER["QUERY_STRING"])) {
-		$qs = $_SERVER["QUERY_STRING"];
-		$rq = urldecode($qs);
-		$qs = "?".$qs;
-	} else {
-		$rq = "main";
-		$qs = "";
-	}
-	$ridx="content/".mybase64($rq).".index";
-	$rq = "content/".mybase64($rq).".inc";
-	if (!is_readable($rq)) {
-		if (!empty($_SERVER["HTTPS"])) {
-			$proto="https";
-		} else {
-			$proto="http";
-		}
-		header("Status: 404 Content Not Found");
-		header("Location: ".$proto."://".
-		    $_SERVER['HTTP_HOST']."/404.php".$qs);
-		exit;
-	}
-	if (!is_readable($ridx)) {
-		$ridx="vindex.inc";
-	}
-	require("vbegin.php");
+print "Due to the European Council ratifying the directive regarding\n";
+print "patentability of algorithms, data structures and business mo-\n";
+print "dels, the MirOS Project is currently not making any code, nor\n";
+print "other work, available to the general public.\n\n";
+print "Contact the MirOS Project at <miros-discuss@mirbsd.org> (mai-\n";
+print "ling list) directly for further information. You are also in-\n";
+print "vited to help us fight these patents:  http://swpat.ffii.org/\n";
 ?>
- <title>MirOS/MirPorts: a wonderful operating system for a world of peace</title>
- <meta name="robots" content="index, follow" />
- <!--
-	This is copyrighted material. For the full licence text, see the
-	MirOS CVSweb or the CSS file. In no event may text from this page
-	be simply copied into another website under an unfree or otherwise
-	mismatching licence, such as the GNU Free Documentation License.
- -->
-</head><body>
-<div class="index">
- <ul class="lv1">
-  <?php include($ridx); ?>
- </ul>
-</div>
-<div class="content">
- <?php
-	include($rq);
- ?>
-</div>
-<div class="heading">
- <?php include("vhead.inc"); ?>
-</div>
-<?php include("vend.php"); ?>
