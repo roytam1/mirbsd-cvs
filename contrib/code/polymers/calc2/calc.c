ef22ca0bd4304321fdea04fec8ab94bbfeb6276a
bsiegert <>
2005-03-23 18:59:39 +0000

    Add a different calculation method for the polymer chain.
    For each step, two random angles, theta and phi, are chosen. Using spherical
    coordinates, the bext point is calculated. Contrary to the first version,
    collisions are not allowed. Thus, if the distance to any other point is < 1,
    a different point is randomly calculated. This assumes that monomers are
    spheres with radius 1/2.

--- contrib/code/polymers/calc2/calc.c
+++ contrib/code/polymers/calc2/calc.c
@@ -1,4 +1,4 @@
-/* $MirOS: contrib/code/polymers/calc/calc.c,v 1.2 2005/03/20 17:49:44 bsiegert Exp $
+/* $MirOS: contrib/code/polymers/calc2/calc.c,v 1.1 2005/03/20 18:20:16 bsiegert Exp $
  *-
  * Copyright (c) 2005
  *	Benny Siegert <bsiegert@gmx.de>
@@ -26,27 +26,47 @@
  */
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* degree of polymerization (number of steps) */
 #define DP 100
 
+struct point {
+	double x, y, z;
+};
+
 int
 calc(FILE *outfile)
 {
-	double x = 0;
-	double y = 0;
-	int i;
-	double angle;
+	struct point points[DP + 1];
+	int i, j;
+	double theta, phi;
+	bool ok;
 
-	fprintf(outfile, "#x\ty\n0\t0\n");
+	memset(&points, 0, sizeof(points));
+	fprintf(outfile, "#x\ty\tz\n0\t0\t0\n");
 
-	for (i = 0; i < DP; i++) {
-		angle = (double)arc4random() / RAND_MAX * M_PI;
-		x += sin(angle);
-		y += cos(angle);
-		fprintf(outfile, "%.4f\t%.4f\n", x, y);
+	for (i = 1; i <= DP; i++) {
+		do {
+			ok = true;
+			theta = (double)arc4random() / RAND_MAX * M_PI;
+			phi = (double)arc4random() / RAND_MAX * M_PI;
+			points[i].x = points[i - 1].x + sin(theta) * cos(phi);
+			points[i].y = points[i - 1].y + sin(theta) * sin(phi);
+			points[i].z = points[i - 1].z + cos(theta);
+
+			for (j = 0; j < i - 1; j++) {
+				if (hypot(hypot(points[i].x - points[j].x,
+				    points[i].y - points[j].y),
+				    points[i].z - points[j].z) < 1.0)
+					ok = false;
+			}
+		} while (!ok);
+
+		fprintf(outfile, "%.4f\t%.4f\t%.4f\n", points[i].x,
+				points[i].y, points[i].z);
 	}
-	return hypot(x, y) + 0.5;
+	return hypot(hypot(points[i - 1].x, points[i - 1].y), points[i - 1].z) + 0.5;
 }
