330aa17b5452f20f975934fab534b70705e73a69
tg <>
2005-03-27 18:37:20 +0000

    so make them build

--- ports/infrastructure/install/mirports.miros.mk
+++ ports/infrastructure/install/mirports.miros.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: ports/infrastructure/mk/mirports.miros.mk,v 1.1.7.1 2005/03/18 15:47:19 tg Exp $
 #-
 # MirPorts plug-in for current MirOS versions
 
@@ -7,7 +7,7 @@ FETCH_CMD?=		/usr/bin/ftp -EV -m
 HAS_CXX=		port
 HAS_TIMET64=		Yes
 SYSTRACE_ARGS_ADD+=	-e
-_CXX_BR_DEPENDS=	:gcc->=3.4.0:
+_CXX_BR_DEPENDS=
 _CXX_LIB_DEPENDS=
 
 # Version dependencies
