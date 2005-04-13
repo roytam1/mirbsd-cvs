6e541403ce92ebff121f31e792d8ebf6a6856864
tg <>
2005-04-13 09:10:56 +0000

    "This 404 presented by Google ads"
    *giggle*

--- www/getnam.php
+++ www/getnam.php
@@ -1,5 +1,5 @@
 <?php
-/* $MirOS$ */
+/* $MirOS: www/getnam.php,v 1.3 2005/03/15 17:13:04 tg Exp $ */
 /*-
  * The MirOS Project - Webpages
  * Copyrighted material; read LICENCE for terms of use.
@@ -32,4 +32,19 @@
  if it sometimes sucks. Character set to use for include files is
  ISO_646.irv:1991 (US-ASCII compatible); utf-8 is tolerated but you
  <strong>will</strong> get a problem with me! :)</p>
+<script type="text/javascript"><!--
+	google_ad_client = "pub-0266998858986985";
+	google_ad_width = 728;
+	google_ad_height = 90;
+	google_ad_format = "728x90_as";
+	google_ad_channel ="";
+	google_color_border = "555555";
+	google_color_bg = "000000";
+	google_color_link = "CC7777";
+	google_color_url = "FFAA00";
+	google_color_text = "BBBBBB";
+//--></script>
+<script type="text/javascript"
+ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 <?php $tg_realm=0; include("vend.php"); ?>
