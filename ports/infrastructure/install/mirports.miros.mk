4e5281e0f86d3a321ca8fcd9bad630367124897b
tg <>
2005-04-26 06:37:36 +0000

    we did fix g++ recently

--- ports/infrastructure/install/mirports.miros.mk
+++ ports/infrastructure/install/mirports.miros.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/mirports.miros.mk,v 1.4 2005/03/29 12:41:56 tg Exp $
+# $MirOS: ports/infrastructure/mk/mirports.miros.mk,v 1.5 2005/04/11 14:52:48 tg Exp $
 #-
 # MirPorts plug-in for current MirOS versions
 
@@ -6,11 +6,14 @@ CXX?=			c++
 FETCH_CMD?=		/usr/bin/ftp -EV -m
 HAS_TIMET64=		Yes
 SYSTRACE_ARGS_ADD+=	-e
-HAS_CXX=		reason
-NO_CXX=			C++ is still broken
 OSREV?=			${OSrev}
 
 # Version dependencies
 .if ${OSrev} < 8
 ERRORS+=		'Configuration error. Do a "cd /usr/ports; make setup".'
+.endif
+
+.if ${OSrpl} < 40
+HAS_CXX=		reason
+NO_CXX=			C++ is still broken, please update
 .endif
