edd6fced624507e7a10717db448dae81850753c7
tg <>
2005-05-04 23:24:45 +0000

    update licence boilerplate

--- ports/mailnews/pine/files/ckp_bso.c
+++ ports/mailnews/pine/files/ckp_bso.c
@@ -1,15 +1,15 @@
-/**	$MirOS$ */
+/* $MirOS: src/share/misc/licence.template,v 1.2 2005/03/03 19:43:30 tg Rel $ */
 
 /*-
  * Copyright (c) 2004
  *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
  *
  * Licensee is hereby permitted to deal in this work without restric-
- * tion, including unlimited rights to use, publically perform, modi-
- * fy, merge, distribute, sell, give away or sublicence, provided the
- * above copyright notices, these terms and the disclaimer are retai-
- * ned in all redistributions, or reproduced in accompanying documen-
- * tation or other materials provided with binary redistributions.
+ * tion, including unlimited rights to use, publicly perform, modify,
+ * merge, distribute, sell, give away or sublicence, provided all co-
+ * pyright notices above, these terms and the disclaimer are retained
+ * in all redistributions or reproduced in accompanying documentation
+ * or other materials provided with binary redistributions.
  *
  * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
  * any kind, expressed or implied, to the maximum extent permitted by
@@ -17,11 +17,14 @@
  * licious intent or gross negligence; in no event shall licensor, an
  * author or contributor be held liable for any damage, direct, indi-
  * rect or other, however caused, arising in any way out of the usage
- * of covered work, even if advised of the possibility of such damage.
+ * of this work, even if advised of the possibility of such damage.
  */
 
+#include <sys/cdefs.h>
 #include <login_cap.h>
 #include <bsd_auth.h>
+
+__RCSID("$MirOS$");
 
 struct passwd *
 checkpw (struct passwd *pw, char *pass, int argc, char *argv[])
