2ebc91f902bc74785bd53926b8455457a275f612
tg <>
2005-05-14 15:52:28 +0000

    POSIX thread support for MirOS

--- gcc/boehm-gc/threadlibs.c
+++ gcc/boehm-gc/threadlibs.c
@@ -10,9 +10,12 @@ int main()
 	       "-Wl,--wrap -Wl,pthread_sigmask -Wl,--wrap -Wl,sleep\n");
 #   endif
 #   if defined(GC_LINUX_THREADS) || defined(GC_IRIX_THREADS) \
-	|| defined(GC_FREEBSD_THREADS) || defined(GC_SOLARIS_PTHREADS) \
+	|| defined(GC_SOLARIS_PTHREADS) \
 	|| defined(GC_DARWIN_THREADS) || defined(GC_AIX_THREADS)
         printf("-lpthread\n");
+#   endif
+#   if defined(GC_FREEBSD_THREADS)
+        printf("-pthread\n");
 #   endif
 #   if defined(GC_HPUX_THREADS) || defined(GC_OSF1_THREADS)
 	printf("-lpthread -lrt\n");
