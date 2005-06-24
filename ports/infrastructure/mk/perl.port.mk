6f766f7b30921a5656d04f55ae459b68e24e8ada
tg <>
2005-06-24 17:37:13 +0000

    allow WRKSRC override

--- ports/infrastructure/mk/perl.port.mk
+++ ports/infrastructure/mk/perl.port.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: ports/infrastructure/mk/perl.port.mk,v 1.1.7.1 2005/03/18 15:47:19 tg Exp $
 # $OpenBSD: perl.port.mk,v 1.9 2004/06/08 20:28:19 sturm Exp $
 # Based on bsd.port.mk, originally by Jordan K. Hubbard.
 
@@ -9,12 +9,13 @@ MODPERL_ARCH!=	/usr/bin/perl -e 'use Config; print $$Config{archname}, "\n";'
 MODPERL_PRE=	${MODPERL_DESTDIR}${PREFIX}
 MODPERL_MANPRE=	${MODPERL_PRE}/man/man
 MODPERL_SITEPRE=${MODPERL_PRE}/${P5SITE}
+MODPERL_SRC?=	${WRKSRC}
 
 .if ${CONFIGURE_STYLE:L:Mmodbuild}
 .  if !${PKGNAME:Mp5-Module-Build*}
 BUILD_DEPENDS+=	::devel/p5-Module-Build
 .  endif
-MODPERL_configure= cd ${WRKSRC}; ${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_ENV} \
+MODPERL_configure= cd ${MODPERL_SRC}; ${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_ENV} \
 	/usr/bin/perl Build.PL install_path=lib="${MODPERL_SITEPRE}" \
 	install_path=arch="${MODPERL_SITEPRE}/${MODPERL_ARCH}" \
 	install_path=libdoc="${MODPERL_MANPRE}3" \
@@ -22,7 +23,7 @@ MODPERL_configure= cd ${WRKSRC}; ${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_ENV} \
 	install_path=bin="${MODPERL_PRE}/bin" \
 	install_path=script="${MODPERL_PRE}/bin" ${CONFIGURE_ARGS} 
 .else
-MODPERL_configure= cd ${WRKSRC}; ${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_ENV} \
+MODPERL_configure= cd ${MODPERL_SRC}; ${_SYSTRACE_CMD} ${SETENV} ${CONFIGURE_ENV} \
 	CC="${CC}" CFLAGS="${CFLAGS:C/ *$//}" CXX="${CXX}" \
 	CXXFLAGS="${CXXFLAGS:C/ *$//}" YACC="${YACC}" LD="${LD}" \
 	LDFLAGS="${LDFLAGS}" /usr/bin/perl Makefile.PL \
@@ -46,18 +47,18 @@ MODPERL_pre_fake= \
 .if ${CONFIGURE_STYLE:L:Mmodbuild}
 .  if !target(do-build)
 do-build: 
-	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
+	@cd ${MODPERL_SRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
 		${MODPERL_BUILD} build
 .  endif
 .  if !target(do-regress)
 do-regress:
-	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
+	@cd ${MODPERL_SRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
 		${MODPERL_BUILD} ${REGRESS_TARGET}
 
 .  endif
 .  if !target(do-install)
 do-install:
-	@cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
+	@cd ${MODPERL_SRC} && ${SETENV} ${MAKE_ENV} /usr/bin/perl \
 		${MODPERL_BUILD} destdir=${WRKINST} ${FAKE_TARGET}
 .  endif
 .endif
