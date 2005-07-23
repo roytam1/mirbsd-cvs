d546cbbf9ec0b6e63098b0fca42542d3b626d188
tg <>
2005-07-23 18:54:12 +0000

    * MirOSify * make possible to use this as a shell builtin * build with __CRAZY=yes

--- src/bin/echo/echo.c
+++ src/bin/echo/echo.c
@@ -1,3 +1,4 @@
+/**	$MirOS$ */
 /*	$OpenBSD: echo.c,v 1.6 2003/06/02 23:32:07 millert Exp $	*/
 /*	$NetBSD: echo.c,v 1.6 1995/03/21 09:04:27 cgd Exp $	*/
 
@@ -30,27 +31,26 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
+#if 0
 static char copyright[] =
 "@(#) Copyright (c) 1989, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
-
-#ifndef lint
-#if 0
-static char sccsid[] = "@(#)echo.c	8.1 (Berkeley) 5/31/93";
-#else
-static char rcsid[] = "$OpenBSD: echo.c,v 1.6 2003/06/02 23:32:07 millert Exp $";
-#endif
 #endif /* not lint */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+__SCCSID("@(#)echo.c	8.1 (Berkeley) 5/31/93");
+__RCSID("$MirOS$");
+
+#ifdef SHELL
+#define	main	echocmd
+#endif
+
 /* ARGSUSED */
 int
-main(int argc, char *argv[])
+main(int argc __attribute__((unused)), char *argv[])
 {
 	int nflag;
 
