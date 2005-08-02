1909dde597969ca2c50462dc08c96241456d841c
tg <>
2005-08-02 10:54:46 +0000

    in calibrate_cyclecounter() we have a one second delay; account for it in kernel timekeeping
    XXX revisit this after the move to timecounters

--- src/sys/arch/i386/isa/clock.c
+++ src/sys/arch/i386/isa/clock.c
@@ -1,4 +1,4 @@
-/**	$MirOS$ */
+/**	$MirOS: src/sys/arch/i386/isa/clock.c,v 1.2 2005/03/06 21:27:00 tg Exp $ */
 /*	$OpenBSD: clock.c,v 1.31 2004/02/27 21:07:49 grange Exp $	*/
 /*	$NetBSD: clock.c,v 1.39 1996/05/12 23:11:54 mycroft Exp $	*/
 
@@ -37,28 +37,28 @@
  *
  *	@(#)clock.c	7.2 (Berkeley) 5/12/91
  */
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990,1989 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
@@ -448,12 +448,26 @@ findcpuspeed(void)
 void
 calibrate_cyclecounter(void)
 {
-	unsigned long long count, last_count;
+	unsigned long long last_count;
+	/* XXX this is a hack, we'd better do precision timekeeping */
+	struct timeval tv;
+
+	disable_intr();
+	tv = time;
+	enable_intr();
+	/* it's probably not exactly one second, but pretty good */
+	tv.tv_sec++;
 
 	__asm __volatile("rdtsc" : "=A" (last_count));
 	delay(1000000);
-	__asm __volatile("rdtsc" : "=A" (count));
-	pentium_mhz = ((count - last_count) + 500000) / 1000000;
+
+	disable_intr();
+
+	__asm __volatile("rdtsc" : "=A" (pentium_base_tsc));
+	pentium_mhz = ((pentium_base_tsc - last_count) + 500000) / 1000000;
+
+	time = tv;
+	enable_intr();
 }
 #endif
 
