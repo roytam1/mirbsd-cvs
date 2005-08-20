19b572c2e579eb2ec3996420053eb96a7c9442f2
cvs2git <admin@example.com>
2005-08-20 12:33:59 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.40.2'.

--- ports/converters/libiconv/libiconv.port.mk
+++
@@ -1,7 +0,0 @@
-# $OpenBSD: iconv.port.mk,v 1.4 2001/11/27 17:44:04 brad Exp $
-
-# The RUN_DEPENDS entry is to ensure libiconv is installed. This is
-# necessary so that we have charset.alias installed on static archs.
-# Typically installed in PREFIX/lib.
-LIB_DEPENDS+=	iconv.2::converters/libiconv
-RUN_DEPENDS+=	:libiconv-*:converters/libiconv
