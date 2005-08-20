dbaf0b8bbfdf4919616099feb66b332bcd7bfcf2
tg <>
2005-08-20 13:06:08 +0000

    mention mksh R24 is needed fix URL to mksh homepage

--- contrib/code/mirmake/Build.sh
+++ contrib/code/mirmake/Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $MirOS: contrib/code/mirmake/Build.sh,v 1.12 2005/06/09 22:00:09 tg Exp $
+# $MirOS: contrib/code/mirmake/Build.sh,v 1.13 2005/07/07 22:17:45 tg Exp $
 #-
 # Copyright (c) 2004, 2005
 #	Thorsten "mirabile" Glaser <tg@66h.42h.de>
@@ -141,8 +141,9 @@ fi
 
 if [ x"$ms" = x"NONE" ]; then
 	echo "Error: could not find mirbsdksh compatible shell."
-	echo "Please go to http://wiki.mirbsd.de/MirbsdKsh and"
-	echo "install the shell before continuing."
+	echo "Please go to: http://mirbsd.mirsolutions.de/?mksh"
+	echo "and install the shell before continuing."
+	echo "At least mksh R24 is required currently."
 	exit 1
 fi
 
