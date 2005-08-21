6fe1f4398f547571dbfe0f928123ef9e3f4e65c9
tg <>
2005-08-21 11:49:28 +0000

    not necessary with mirmake-current

--- ports/infrastructure/mk/mirports.bsd.mk
+++ ports/infrastructure/mk/mirports.bsd.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/mirports.bsd.mk,v 1.1 2005/06/17 20:15:22 tg Exp $
+# $MirOS: ports/infrastructure/mk/mirports.bsd.mk,v 1.2 2005/06/17 20:20:03 tg Exp $
 #-
 # This file replaces <bsd.own.mk> as EXTRA_SYS_MK_INCLUDES
 # during MirPorts builds.
@@ -11,11 +11,6 @@ _MIRPORTS_BSD_MK=1
 
 # Second, override where necessary.
 DEBUGLIBS=		No
-
-BINDIR=			${PREFIX}/bin
-LIBDIR=			${PREFIX}/lib
-MANDIR=			${PREFIX}/man/cat
-SHAREDIR=		${PREFIX}/share
 
 .if defined(DESTDIR) && !empty(DESTDIR) && defined(TRUEPREFIX)
 PREFIX:=		${TRUEPREFIX}
