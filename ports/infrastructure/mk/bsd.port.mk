c2f729239c5c629c01552aab55e60198eb90b405
tg <>
2005-09-11 02:35:31 +0000

    merge r1.43

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.40.2.5 2005/09/11 01:24:56 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.40.2.6 2005/09/11 02:12:20 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -59,6 +59,7 @@ DISTDIR?=		${PORTSDIR}/Distfiles
 BULK_COOKIES_DIR?=	${PORTSDIR}/Bulk
 TEMPLATES?=		${PORTSDIR}/infrastructure/templates
 TMPDIR?=		/tmp
+WWW_PREFIX?=		/var/www
 
 PKGREPOSITORY?=		${PORTSDIR}/Packages
 CDROM_PACKAGES?=	${PKGREPOSITORY}/CDROM
