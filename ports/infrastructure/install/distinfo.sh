5280544064762b146f92a164a515a0f39395c07e
tg <>
2005-09-12 20:36:32 +0000

    * Setup.sh: use TZ=UTC for ls * distinfo.sh: add sums for all other new distfiles of today   (gzsig'd as all stuff in /MirOS/dist/{jupp,mir}/ except joe-2.8 and mirex-6)

--- ports/infrastructure/install/distinfo.sh
+++ ports/infrastructure/install/distinfo.sh
@@ -1,19 +1,68 @@
-# $MirOS$
+# $MirOS: ports/infrastructure/install/distinfo.sh,v 1.1.2.1 2005/09/12 20:10:10 tg Exp $
 
 case $what in
+cksum)
+	f_dist=mircksum-20050912.cpio.gz
+	f_path=mir/cksum
+	distinfo_md5=c072abe84ab399ef2fba8a8e505154fc
+	distinfo_sum=3300899251
+	distinfo_size=14479
+	distinfo_date="Sep 12 19:06"
+	;;
+cpio)
+	f_dist=paxmirabilis-20050912.cpio.gz
+	f_path=mir/cpio
+	distinfo_md5=3ebf552572a9f561f8590ef3f44b8740
+	distinfo_sum=1040864534
+	distinfo_size=118793
+	distinfo_date="Sep 12 19:09"
+	;;
+make)
+	f_dist=mirmake-20050912.cpio.gz
+	f_path=mir/make
+	distinfo_md5=99c50076012e0a5c80eb59fd8144c08d
+	distinfo_sum=2751089072
+	distinfo_size=314265
+	distinfo_date="Sep 12 19:12"
+	;;
 mksh)
 	f_dist=mksh-R24c.cpio.gz
-	f_path=mir/mksh/$f_dist
+	f_path=mir/mksh
 	distinfo_md5=7b7b7afb595da75d27b2f08655320600
 	distinfo_sum=2460615772
 	distinfo_size=224603
 	distinfo_date="Sep 12 19:29"
+	;;
+mtree)
+	f_dist=mirmtree-20050912.cpio.gz
+	f_path=mir/mtree
+	distinfo_md5=86cf94a633f136d650f76d065bbd7e0e
+	distinfo_sum=3117223138
+	distinfo_size=17999
+	distinfo_date="Sep 12 19:19"
+	;;
+nroff)
+	f_dist=mirnroff-20050912.cpio.gz
+	f_path=mir/nroff
+	distinfo_md5=c9336d68a54a1965e37fcf4dab19cba3
+	distinfo_sum=1195567255
+	distinfo_size=224960
+	distinfo_date="Sep 12 19:34"
+	;;
+sort)
+	f_dist=mirsort-20050912.cpio.gz
+	f_path=mir/sort
+	distinfo_md5=409a55b90312b306024f9e22e32e06a6
+	distinfo_sum=108354838
+	distinfo_size=21356
+	distinfo_date="Sep 12 19:36"
 	;;
 *)
 	echo Do not call me directly. >&2
 	exit 1
 	;;
 esac
+f_path=$f_path/$f_dist
 f_md5="MD5 ($f_dist) = $distinfo_md5"
 f_sum="$distinfo_sum $distinfo_size $f_dist"
 f_md5sum="$distinfo_md5  $f_dist"
