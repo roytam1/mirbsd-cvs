272931d98b6ef2a79e1f06144695d92fcad67382
tg <>
2005-09-29 12:42:28 +0000

    if unlibtoolise fails, make the package build fail too (else we'd end up with an empty package) - noticed by bsiegert@
    Idea: if unlibtoolise exits abnormally, we remove PLIST.tmp and make the
    following "mv PLIST.tmp PLIST" fail thusly, exiting make.

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.47 2005/09/24 11:57:14 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.48 2005/09/24 12:13:18 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -338,7 +338,7 @@ ERRORS+=		"No flavours for this port."
 .  endif
 .endif
 
-SED_PLIST+=		| (cd ${WRKINST}${PREFIX}; LOCALBASE=${LOCALBASE} perl -W ${PORTSDIR}/infrastructure/scripts/unlibtoolise)
+SED_PLIST+=		| (cd ${WRKINST}${PREFIX}; LOCALBASE=${LOCALBASE} perl -W ${PORTSDIR}/infrastructure/scripts/unlibtoolise || rm -f $@.tmp)
 
 .if defined(DIST_NAME) && defined(DIST_DATE)
 PKGNAME?=		${DIST_NAME}-${DIST_DATE}-0
@@ -1271,7 +1271,7 @@ ${WRKPKG}/PLIST${SUBPACKAGE}: ${PLIST} ${WRKPKG}/depends${SUBPACKAGE}
 	    -e '/^%%SHARED%%$$/r${PKGDIR}/PFRAG.shared${SUBPACKAGE}' \
 	    -e '//d' <${PLIST} ${SED_PLIST} >>$@.tmp
 .endif
-	mv -f $@.tmp $@
+	mv $@.tmp $@
 
 ${WRKPKG}/depends${SUBPACKAGE}:
 	@mkdir -p ${WRKPKG}
