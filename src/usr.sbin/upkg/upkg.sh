bd563f74b1debbc4b96f6514c4bb4d43f1d9743d
bsiegert <>
2005-10-10 19:13:24 +0000

    Okay, here is the very first prototype for upkg. Attention: The shell code has not been tested (the awk code was tested though).
    At the moment, it should be able to install a package. Support for @exec
    and updating the shareddirs database is still missing. Other stuff should
    mostly work. YMMV. There might be syntax errors lurking. Take care.

--- src/usr.sbin/upkg/upkg.sh
+++ src/usr.sbin/upkg/upkg.sh
@@ -1,5 +1,5 @@
 #!/bin/mksh
-# $MirOS: src/usr.sbin/upkg/upkg.sh,v 1.1 2005/08/23 20:57:28 bsiegert Exp $
+# $MirOS: src/usr.sbin/upkg/upkg.sh,v 1.2 2005/08/24 09:59:14 tg Exp $
 #-
 # Copyright (c) 2005
 #	Benny Siegert <bsiegert@66h.42h.de>
@@ -21,21 +21,152 @@
 #-
 # a minimal version of pkg_add, written in shell. Requires mksh.
 
+function upkg_get_file
+{
+	case $1 in
+	*://*)
+		# try it as a url
+		ftp -vo - "$1"
+	*)
+		cat "$1"
+	esac
+}
+		
+
+
+function upkg_add
+{
+	set -e # abort on error
+
+	TMPDIR=$(mktemp -d /tmp/pkg_upgrade.XXXXXXXXXX)
+	trap 'rm -rf $TMPDIR ; exit 0' 0
+	trap 'rm -rf $TMPDIR ; exit 1' 1 2 3 13 15
+	
+	upkg_get_file "$1" | (cd $TMPDIR ; tar xf -)
+	cd $TMPDIR
+	
+	pkgname=$(awk '$1=="@name" { print $2 ; exit }' ./+CONTENTS)
+	pkgdir=$PKG_DBDIR/$pkgname
+
+	if [[ -f +REQUIRE ]] ; then
+		chmod +x +REQUIRE
+		if ! ./+REQUIRE $pkgname INSTALL ; then
+			print -u2 "Package $pkgname fails requirements!"
+			exit 1
+		fi
+	fi
+	if [[ -f +INSTALL ]] ; then
+		chmod +x +INSTALL
+		if ! ./+INSTALL $pkgname PRE-INSTALL ; then
+			print -u2 "Package $pkgname install script returned error status"
+			exit 1
+		fi
+	fi
+	
+	awk '
+	BEGIN {
+		copycmd = "cpio -pld "
+		mkdir_p = "mkdir -p "
+	}
+
+	$1 == "@cwd" {
+		prefix = $2
+	}
+	
+	/^[^@].*[^\/]$/ {
+		last_file = $0
+		if (user)
+			user_files = user_files " " $0
+		if (group)
+			group_files = group_files " " $0
+		if (mode)
+			mode_files = mode_files " " $0
+		print $0 | copycmd prefix
+	}
+
+	/^[^@].*\/$/ {
+		system(mkdir_p prefix "/" $0)
+	}
+
+	$1 == "@user" {
+		if (user_files != "") {
+			system("chown", user, user_files) 
+			user_files = ""
+		}
+		user = $2
+	}
+
+	$1 == "@group" {
+		if (group_files != "") {
+			system("chgrp", group, group_files) 
+			group_files = ""
+		}
+		group = $2
+	}
+
+	$1 == "@mode" {
+		if (mode_files != "") {
+			system("chmod", mode, user_files) 
+			mode_files = ""
+		}
+		mode = $2
+	}
+	
+	$1 == "@sample" {
+		if (substr($2, length($2), 1) == "/")
+			system(mkdir_p prefix "/" $2)
+	}
+
+	END {
+		if (user_files != "")
+			system("chown", user, user_files) 
+		if (group_files != "")
+			system("chgrp", group, group_files) 
+		if (mode_files != "")
+			system("chmod", mode, user_files) 
+	}	
+	' +CONTENTS
+	
+	# Record the installation
+	mkdir -p $pkgdir
+	chmod a+rx $pkgdir
+	cp -- +COMMENT +CONTENTS +DESC $pkgdir
+	chmod 644 +COMMENT +CONTENTS +DESC
+	for i in +INSTALL +DEINSTALL +REQUIRE +DISPLAY ; do
+		if [[ -f $i ]] ; then
+			cp $i $pkgdir
+			chmod 644 $pkgdir/$i
+		fi
+	done
+
+	if [[ -f +INSTALL ]] ; then
+		chmod +x +INSTALL
+		if ! ./+INSTALL $pkgname POST-INSTALL ; then
+			print -u2 "Package $pkgname install script returned error status"
+			exit 1
+		fi
+	fi
+	if [[ -f +DISPLAY ]] ; then
+		cat ./+DISPLAY
+	fi
+	
+}
+
 function usage
 {
 	print -u2 "Usage:"
-	print -u2 " $0 [-L localbase] install [pkgname]"
+	print -u2 " $0 [-L localbase] add [pkgname]"
 	exit 1
 }
 
 [[ -n $1 ]] || usage
 
-localbase=/usr/mpkg
+LOCALBASE=/usr/mpkg
 while getopts "hL" c; do
 	case $c {
 	(h)	usage
 		;;
-	(L)	localbase=$OPTARG
+	(L)	LOCALBASE=$OPTARG
 		;;
 	(*)	usage
 		;;
@@ -43,4 +174,17 @@ while getopts "hL" c; do
 done
 shift $((OPTIND - 1))
 
-PKG_DBDIR=$localbase/pkg
+PKG_DBDIR=$localbase/db/pkg
+if [[ ! -d $PKG_DBDIR ]] ; then
+	mkdir -p $PKG_DBDIR
+fi
+
+if [[ "x$1" == "xadd" ]] ; then
+	while [[ "x$2" != "x" ]] ; do
+		upkg_add "$2"
+		shift
+	done
+else
+	usage
+fi
+
