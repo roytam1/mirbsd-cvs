377c3c5bc5b8ef93dadc9b424f58f67e86f15d72
tg <>
2005-10-25 21:04:16 +0000

    remove redundant licences already contained in ./copyright to make the tarball smaller

--- src/bin/mksh/compat.c
+++ src/bin/mksh/compat.c
@@ -1,30 +1,6 @@
-/* $MirOS: src/bin/mksh/compat.c,v 1.1.2.3 2005/10/25 19:53:26 tg Exp $ */
-
-/*-
- * Copyright (c) 2005
- *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
- *
- * Licensee is hereby permitted to deal in this work without restric-
- * tion, including unlimited rights to use, publicly perform, modify,
- * merge, distribute, sell, give away or sublicence, provided all co-
- * pyright notices above, these terms and the disclaimer are retained
- * in all redistributions or reproduced in accompanying documentation
- * or other materials provided with binary redistributions.
- *
- * All advertising materials mentioning features or use of this soft-
- * ware must display the following acknowledgement:
- *	This product includes material provided by Thorsten Glaser.
- *
- * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
- * any kind, expressed or implied, to the maximum extent permitted by
- * applicable law, but with the warranty of being written without ma-
- * licious intent or gross negligence; in no event shall licensor, an
- * author or contributor be held liable for any damage, direct, indi-
- * rect or other, however caused, arising in any way out of the usage
- * of this work, even if advised of the possibility of such damage.
- */
-
 #include "sh.h"
+
+__RCSID("$MirOS$");
 
 #ifndef __SCCSID
 #define	__SCCSID(x)	static const char __sccsid[] __attribute__((used)) = (x)
--- src/bin/mksh/compat.h
+++ src/bin/mksh/compat.h
@@ -1,28 +1,4 @@
-/* $MirOS: src/bin/mksh/compat.h,v 1.1.2.4 2005/10/25 20:24:41 tg Exp $ */
-
-/*-
- * Copyright (c) 2005
- *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
- *
- * Licensee is hereby permitted to deal in this work without restric-
- * tion, including unlimited rights to use, publicly perform, modify,
- * merge, distribute, sell, give away or sublicence, provided all co-
- * pyright notices above, these terms and the disclaimer are retained
- * in all redistributions or reproduced in accompanying documentation
- * or other materials provided with binary redistributions.
- *
- * All advertising materials mentioning features or use of this soft-
- * ware must display the following acknowledgement:
- *	This product includes material provided by Thorsten Glaser.
- *
- * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
- * any kind, expressed or implied, to the maximum extent permitted by
- * applicable law, but with the warranty of being written without ma-
- * licious intent or gross negligence; in no event shall licensor, an
- * author or contributor be held liable for any damage, direct, indi-
- * rect or other, however caused, arising in any way out of the usage
- * of this work, even if advised of the possibility of such damage.
- */
+/* $MirOS: src/bin/mksh/compat.h,v 1.1.2.5 2005/10/25 20:48:18 tg Exp $ */
 
 /* Part I: extra headers */
 
