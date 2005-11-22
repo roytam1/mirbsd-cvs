1e6eecb2c7c5088543197a482059d9e65fe2ccc1
tg <>
2005-11-22 17:17:12 +0000

    on non-OpenBSD/MirOS, use #include_next to find a matching md5.h, usually the one from MirMake (gcc extension, yes)

--- src/gnu/usr.bin/binutils/include/md5.h
+++ src/gnu/usr.bin/binutils/include/md5.h
@@ -1,11 +1,16 @@
-/* $MirOS$ */
+/* $MirOS: src/gnu/usr.bin/binutils/include/md5.h,v 1.2 2005/03/13 16:07:00 tg Exp $ */
 
 #ifndef _MD5_H
 
+#ifdef __OpenBSD__
 /* include md5 functions from libc */
 #include "/usr/include/md5.h"
-#ifdef	_MD5_H_
-#define	_MD5_H
+#else
+#include_next "md5.h"
+#endif
+
+#ifdef _MD5_H_
+#define _MD5_H
 
 #define	md5_ctx				MD5Context
 #define	md5_init_ctx			MD5Init
