a0df1a09d3bc01e70521838548a4861fcaae2531
tg <>
2005-12-15 14:24:25 +0000

    * remove remnants of libc-client.so * honour SYSCONFDIR, LOCALBASE, TRUEPREFIX * certificate now in SYSCONFDIR/ssl/*.pem instead * bump versions on all packages

--- ports/mailnews/pine/files/ckp_bso.c
+++ ports/mailnews/pine/files/ckp_bso.c
@@ -1,8 +1,8 @@
-/* $MirOS: src/share/misc/licence.template,v 1.2 2005/03/03 19:43:30 tg Rel $ */
+/* $MirOS: ports/mailnews/pine/files/ckp_bso.c,v 1.2 2005/05/04 23:24:45 tg Exp $ */
 
 /*-
  * Copyright (c) 2004
- *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
+ *	Thorsten "mirabile" Glaser <tg@mirbsd.de>
  *
  * Licensee is hereby permitted to deal in this work without restric-
  * tion, including unlimited rights to use, publicly perform, modify,
@@ -11,20 +11,21 @@
  * in all redistributions or reproduced in accompanying documentation
  * or other materials provided with binary redistributions.
  *
- * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
- * any kind, expressed or implied, to the maximum extent permitted by
- * applicable law, but with the warranty of being written without ma-
- * licious intent or gross negligence; in no event shall licensor, an
- * author or contributor be held liable for any damage, direct, indi-
- * rect or other, however caused, arising in any way out of the usage
- * of this work, even if advised of the possibility of such damage.
+ * Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
+ * express, or implied, to the maximum extent permitted by applicable
+ * law, without malicious intent or gross negligence; in no event may
+ * licensor, an author or contributor be held liable for any indirect
+ * or other damage, or direct damage except proven a consequence of a
+ * direct error of said person and intended use of this work, loss or
+ * other issues arising in any way out of its use, even if advised of
+ * the possibility of such damage or existence of a nontrivial bug.
  */
 
 #include <sys/cdefs.h>
 #include <login_cap.h>
 #include <bsd_auth.h>
 
-__RCSID("$MirOS$");
+__RCSID("$MirOS: ports/mailnews/pine/files/ckp_bso.c,v 1.2 2005/05/04 23:24:45 tg Exp $");
 
 struct passwd *
 checkpw (struct passwd *pw, char *pass, int argc, char *argv[])
