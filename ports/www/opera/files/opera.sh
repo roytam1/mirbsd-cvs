69e831a48caf8a02fd1a4c38019e6702ac2c6b41
tg <>
2005-12-16 14:42:59 +0000

    Update Opera to 8.51, mostly from OpenBSD, but * don't hardcode /usr/local, use $PREFIX * don't write to /etc, it runs without the samples * fix... etc.
    It works, I can even install the Cocoa theme (but I prefer
    the Minimalist Native one), but I can't read the menu text
    (neither context menus), I only see empty squares.

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-#
+#!@SHELL@
+# $MirOS$
 # $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
 PLUGIN_PATH=${PLUGIN_PATH}:${HOME}/.netscape/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/netscape/plugins
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/libexec
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:@PREFIX@/libexec
 
 export OPERA_DIR=@PREFIX@/share/opera
 export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
