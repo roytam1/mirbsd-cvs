172f65cf54619e41619c1e280a4e25287f766887
tg <>
2005-12-20 19:17:39 +0000

    include <bsd.own.mk> _after_ SetEnv.make, else it won't help MAKECONF NOTE: bsd.own.mk has to make sure to use ?= to not overwrite here

--- ports/infrastructure/install/mirports.sys.mk
+++ ports/infrastructure/install/mirports.sys.mk
@@ -1,10 +1,6 @@
-# $MirOS: ports/infrastructure/install/mirports.sys.mk,v 1.1.7.1.2.3 2005/09/01 22:54:21 tg Exp $
+# $MirOS: ports/infrastructure/install/mirports.sys.mk,v 1.2 2005/09/12 22:53:17 tg Exp $
 
 .ifndef MIRPORTS_SYS_MK
-
-.ifndef BSD_OWN_MK
-.  include <bsd.own.mk>
-.endif
 
 # Note these two are not necessarily coupled to each other
 PORTSDIR?=	@
@@ -12,6 +8,10 @@ LOCALBASE?=	@
 
 .if exists(${LOCALBASE}/db/SetEnv.make)
 .  include "${LOCALBASE}/db/SetEnv.make"
+.endif
+
+.ifndef BSD_OWN_MK
+.  include <bsd.own.mk>
 .endif
 
 .if exists(${PORTSDIR}/infrastructure/mk/mirports.sys.mk)
