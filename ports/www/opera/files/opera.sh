a938c06819c91e25cce4eb7038ce4f749ede3fe1
tg <>
2006-01-18 11:26:17 +0000

    depend on emulators/debian (confirmed it works without emulators/redhat)

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,5 +1,5 @@
 #!@SHELL@
-# $MirOS$
+# $MirOS: ports/www/opera/files/opera.sh,v 1.2 2005/12/16 14:42:58 tg Exp $
 # $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
@@ -12,4 +12,4 @@ export OPERA_DIR=@PREFIX@/share/opera
 export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
 export LD_LIBRARY_PATH
 
-exec @PREFIX@/libexec/opera "$@"
+exec @PREFIX@/emul/debian/run @PREFIX@/libexec/opera "$@"
