ebdf8441dec21079dc6dca96e4ed921df6dcd80d
tg <>
2006-01-25 12:48:09 +0000

    first step in upgrading php: * mark as broken - upgrade in progress * sync with OpenBSD * update hardened-php patch version
    note: we use php 5.0.5 not 5.1.2 like I would, because the
    OpenBSD team has experienced problems with the 5.1 series,
    and I don't want to dig deeper into that again

--- ports/www/php/files/autogen.sh
+++ ports/www/php/files/autogen.sh
@@ -1,5 +1,5 @@
-#!/bin/ksh
-# $MirOS$
+#!/bin/sh
+# $MirOS: ports/www/php/files/autogen.sh,v 1.1 2005/03/27 22:20:27 tg Exp $
 
 set -e
 set -x
@@ -7,5 +7,5 @@ aclocal -I .
 autoheader
 set +e
 autoconf
-[[ -e autom4te.cache ]] && rm -rf autom4te.cache
+rm -rf autom4te.cache
 exit 0
