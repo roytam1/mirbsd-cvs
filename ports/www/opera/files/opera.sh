b1edfa953182196a922de339342c6655981a8460
tg <>
2006-02-01 20:38:49 +0000

    revamp compat_linux(8) support library port structure to allow for undisturbed coexistence of more than one set of libraries and dyld and patch the opera binary to call /dbl/ld-linux.so.2 instead

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,15 +1,15 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.2 2005/12/16 14:42:58 tg Exp $
+# $MirOS: ports/www/opera/files/opera.sh,v 1.3 2006/01/18 11:26:17 tg Exp $
 # $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
 PLUGIN_PATH=${PLUGIN_PATH}:${HOME}/.netscape/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/netscape/plugins
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:@PREFIX@/libexec
+LD_LIBRARY_PATH=/dbl:${LD_LIBRARY_PATH}:@PREFIX@/libexec
 
 export OPERA_DIR=@PREFIX@/share/opera
 export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
 export LD_LIBRARY_PATH
 
-exec @PREFIX@/emul/debian/run @PREFIX@/libexec/opera "$@"
+exec @PREFIX@/libexec/opera "$@"
