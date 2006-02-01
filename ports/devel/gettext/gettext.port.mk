4d3cc86ca512e41119e5b837f431e0c9b2fe6634
tg <>
2006-02-01 17:48:29 +0000

    intl.1:: -> intl:: (.major[.minor] doesn't work with dylib, sadly)

--- ports/devel/gettext/gettext.port.mk
+++ ports/devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: ports/devel/gettext/gettext.port.mk,v 1.1.7.1 2005/03/18 15:44:38 tg Exp $
 # $OpenBSD: gettext.port.mk,v 1.8 2001/11/27 17:44:04 brad Exp $
 
 MODULES+=	converters/libiconv
@@ -6,6 +6,6 @@ MODULES+=	converters/libiconv
 # The RUN_DEPENDS entry is to ensure gettext is installed. This is
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS+=	intl.1:gettext->=0.10.38:devel/gettext
+LIB_DEPENDS+=	intl:gettext->=0.10.38:devel/gettext
 BUILD_DEPENDS+=	:gettext->=0.10.38:devel/gettext
 RUN_DEPENDS+=	:gettext->=0.10.38:devel/gettext
