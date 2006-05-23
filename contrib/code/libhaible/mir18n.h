5c759e8af011d64560c6559c5f6230c6ff08d9db
tg <>
2006-05-23 10:18:05 +0000

    copy this file from libc

---
+++ contrib/code/libhaible/mir18n.h
@@ -0,0 +1,14 @@
+/* $MirOS: src/lib/libc/include/mir18n.h,v 1.1 2006/05/21 12:12:30 tg Exp $ */
+
+#ifndef _LIBC_MIR18N_H
+#define _LIBC_MIR18N_H
+
+#ifndef __bool_true_false_are_defined
+#include <stdbool.h>
+#endif
+
+__BEGIN_DECLS
+extern bool __locale_is_utf8;
+__END_DECLS
+
+#endif
