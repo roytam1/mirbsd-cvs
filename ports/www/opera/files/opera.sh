27f9403200c4e423f730b7317b2feb727d507998
tg <>
2006-06-20 18:20:37 +0000

    opera 9.0, wfm

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,6 +1,9 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.3 2006/01/18 11:26:17 tg Exp $
+# $MirOS: ports/www/opera/files/opera.sh,v 1.4 2006/02/01 20:38:49 tg Exp $
 # $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
+
+case $0 in /*) OPERA_SCRIPT_PATH=$0;; *) OPERA_SCRIPT_PATH=`/bin/pwd`/$0;; esac
+export OPERA_SCRIPT_PATH
 
 PLUGIN_PATH=@PREFIX@/lib/opera/plugins
 PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
