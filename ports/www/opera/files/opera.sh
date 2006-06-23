27a49b1e3c885c0b59d89db88b02370f8e59e945
tg <>
2006-06-23 14:27:15 +0000

    I originally wanted to make flashplugin work with these changes, but it doesn't even find libnpp.so, still I commit this because of the other cleanup changes in this port.

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,18 +1,7 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.4 2006/02/01 20:38:49 tg Exp $
-# $OpenBSD: opera.sh,v 1.5 2003/11/09 09:46:26 sturm Exp $
-
-case $0 in /*) OPERA_SCRIPT_PATH=$0;; *) OPERA_SCRIPT_PATH=`/bin/pwd`/$0;; esac
-export OPERA_SCRIPT_PATH
-
-PLUGIN_PATH=@PREFIX@/lib/opera/plugins
-PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/lib/ns-plugins
-PLUGIN_PATH=${PLUGIN_PATH}:${HOME}/.netscape/plugins
-PLUGIN_PATH=${PLUGIN_PATH}:@LOCALBASE@/netscape/plugins
-LD_LIBRARY_PATH=/dbl:${LD_LIBRARY_PATH}:@PREFIX@/libexec
+# $MirOS: ports/www/opera/files/opera.sh,v 1.5 2006/06/20 18:20:37 tg Exp $
 
 export OPERA_DIR=@PREFIX@/share/opera
-export OPERA_PLUGIN_PATH=${OPERA_PLUGIN_PATH-${PLUGIN_PATH}}
-export LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
 
 exec @PREFIX@/libexec/opera "$@"
