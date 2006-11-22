e5e6116bcad2b621fbe0b91021df96da74efd459
tg <>
2006-11-22 18:08:20 +0000

    fix deps, cought by bsiegert@
    I never use gettext, so… ;)

--- ports/devel/gettext/gettext.port.mk
+++ ports/devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/devel/gettext/gettext.port.mk,v 1.1.7.1 2005/03/18 15:44:38 tg Exp $
+# $MirOS: ports/devel/gettext/gettext.port.mk,v 1.2 2006/02/01 17:48:29 tg Exp $
 # $OpenBSD: gettext.port.mk,v 1.8 2001/11/27 17:44:04 brad Exp $
 
 MODULES+=	converters/libiconv
@@ -6,6 +6,5 @@ MODULES+=	converters/libiconv
 # The RUN_DEPENDS entry is to ensure gettext is installed. This is
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS+=	intl:gettext->=0.10.38:devel/gettext
-BUILD_DEPENDS+=	:gettext->=0.10.38:devel/gettext
-RUN_DEPENDS+=	:gettext->=0.10.38:devel/gettext
+LIB_DEPENDS+=	intl::devel/gettext
+B_R_DEPENDS+=	:gettext->=0.10.38:devel/gettext
