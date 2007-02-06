71e52ffef8610d5c48641e1a4021985a86ea9617
cvs2git <admin@example.com>
2007-02-06 20:09:47 +0000

    This commit was manufactured by cvs2git to create branch 'libz'.

--- ports/lang/ghc/ghc.port.mk
+++
@@ -1,9 +0,0 @@
-# $OpenBSD: ghc.port.mk,v 1.3 2004/04/26 23:48:50 dons Exp $
-# Module for Glasgow Haskell Compiler
-
-# Not yet ported to other architectures
-# See comments in lang/ghc/Makefile for more information
-ONLY_FOR_PLATFORM=	*:*:i386 *:*:sparc *:*:amd64
-
-BUILD_DEPENDS+=	bin/ghc::lang/ghc
-RUN_DEPENDS+=	bin/ghc::lang/ghc
