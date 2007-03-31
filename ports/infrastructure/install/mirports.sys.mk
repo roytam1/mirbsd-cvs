35755521a36d73bc740b0f72f793f4d044add136
tg <>
2007-03-31 19:35:51 +0000

    catch including <bsd.own.mk> instead of <mirports.sys.mk>
    (this made archivers/star have SUDO=sudo from /etc/make.cfg whereas
    mmake had set it to empty since I'm using a not-as-root mirports setup)

--- ports/infrastructure/install/mirports.sys.mk
+++ ports/infrastructure/install/mirports.sys.mk
@@ -1,6 +1,10 @@
-# $MirOS: ports/infrastructure/install/mirports.sys.mk,v 1.2 2005/09/12 22:53:17 tg Exp $
+# $MirOS: ports/infrastructure/install/mirports.sys.mk,v 1.3 2005/12/20 19:17:39 tg Exp $
 
 .ifndef MIRPORTS_SYS_MK
+
+.ifdef BSD_OWN_MK
+.  error Never .include <bsd.own.mk> in MirPorts, always .include <mirports.sys.mk>
+.endif
 
 # Note these two are not necessarily coupled to each other
 PORTSDIR?=	@
@@ -10,9 +14,7 @@ LOCALBASE?=	@
 .  include "${LOCALBASE}/db/SetEnv.make"
 .endif
 
-.ifndef BSD_OWN_MK
-.  include <bsd.own.mk>
-.endif
+.include <bsd.own.mk>
 
 .if exists(${PORTSDIR}/infrastructure/mk/mirports.sys.mk)
 .  include "${PORTSDIR}/infrastructure/mk/mirports.sys.mk"
