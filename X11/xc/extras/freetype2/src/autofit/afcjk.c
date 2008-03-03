412d4e1e1360482b2b62d04c6f70f639db6b9d40
tg <>
2008-03-03 01:27:17 +0000

    this is actually funny:
    mgcc -o bdftopcf -O2 -Wall -Wpointer-arith -Os -fweb -frename-registers -Wformat -Wstrict-aliasing  -Wbounded -fwrapv -fno-align-functions -fno-align-labels  -falign-loops=4 -falign-jumps=4 -march=i486 -mpush-args -mpreferred-stack-boundary=2 -fhonour-copts      -L../../exports/lib   bdftopcf.o -lXfont -lfntstubs -lfreetype -lz -Wl,-O2  -lz -lm   -Wl,-rpath,/usr/X11R6/lib -Wl,-rpath-link,../../exports/lib
    ../../exports/lib/libXfont.so.1.5: warning: strcpy() is almost always misused, consider using strlcpy()
    ../../exports/lib/libXfont.so.1.5: warning: strcat() is almost always misused, consider using strlcat()
    ../../exports/lib/libXfont.so.1.5: warning: sprintf() is often misused, consider using snprintf()
    ../../exports/lib/libfreetype.so.11.1: undefined reference to `af_cjk_hints_apply'
    ../../exports/lib/libfreetype.so.11.1: undefined reference to `af_cjk_metrics_init'
    ../../exports/lib/libfreetype.so.11.1: undefined reference to `af_cjk_metrics_scale'
    ../../exports/lib/libfreetype.so.11.1: undefined reference to `af_cjk_hints_init'
    collect2: ld returned 1 exit status
    *** Error code 1
    
    the afindic.c autofitter uses some internal symbols from
    the afcjk.c autofitter, which are defined with the static
    qualifier inside there – remove that, now we can link
    against the new libfreetype

--- X11/xc/extras/freetype2/src/autofit/afcjk.c
+++ X11/xc/extras/freetype2/src/autofit/afcjk.c
@@ -45,7 +45,7 @@
   /*************************************************************************/
   /*************************************************************************/
 
-  static FT_Error
+  FT_Error
   af_cjk_metrics_init( AF_LatinMetrics  metrics,
                        FT_Face          face )
   {
@@ -91,7 +91,7 @@
   }
 
 
-  static void
+  void
   af_cjk_metrics_scale( AF_LatinMetrics  metrics,
                         AF_Scaler        scaler )
   {
@@ -596,7 +596,7 @@
   }
 
 
-  static FT_Error
+  FT_Error
   af_cjk_hints_init( AF_GlyphHints    hints,
                      AF_LatinMetrics  metrics )
   {
@@ -1350,7 +1350,7 @@
   }
 
 
-  static FT_Error
+  FT_Error
   af_cjk_hints_apply( AF_GlyphHints    hints,
                       FT_Outline*      outline,
                       AF_LatinMetrics  metrics )
