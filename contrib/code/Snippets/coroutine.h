5bcb2449d190d5d37edd108641c9b956a6370193
tg <>
2008-06-18 20:27:16 +0000

    preliminary version of a coroutines-in-C header file written by Felix “nbd” Fietkau after an idea (and self-criticism) of Simon Tatham, plus design improvements discussed with and implemented by me. Felix kindly agreed to put this under MirOS licence.
    This is the preliminary version ack’d by Felix,
    • with some namespace improvements (e.g. __CR_DUFF)
    • with a new macro __coroutine_proto for function prototypes if
      -Wstrict-prototypes is used
    
    With this the sample programme included builds with __CRAZY=Yes
    
    Changes to the final version (missing):
    • do not use __extension__({ on non-gcc (or at all)
      ‣ possible change of syntax a̲n̲d̲ semantics of __cr_new()
    • test with pcc and SUNw,cc (and possibly others)
    • get an ack from Felix for the changed versions again
    • send to Simon for review

---
+++ contrib/code/Snippets/coroutine.h
@@ -0,0 +1,240 @@
+/* $MirOS: src/share/misc/licence.template,v 1.24 2008/04/22 11:43:31 tg Rel $ */
+
+/*-
+ * $Id$ is
+ * Copyright (c) 2008
+ *	Felix Fietkau <nbd@openwrt.org>
+ *	Thorsten Glaser <tg@mirbsd.org>
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un-
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person's immediate fault when using the work as intended.
+ *-
+ * Thanks to Simon Tatham for the idea and a reference for the
+ * implementation.  For more information about this, refer to:
+ * http://www.chiark.greenend.org.uk/~sgtatham/coroutines.html
+ */
+
+/**
+ * Information on how to use this header file:
+ *
+ * To create a coroutine, use this:
+
+__coroutine_decl(<typename>, <return type> [, <arguments>]);
+
+__coroutine_proto(<typename>, <name>, <return type> [, <arguments>]);
+
+__coroutine_defn(<typename>, <name>, <return type> [, <arguments>])
+{
+	<local variables>
+	__cr_begin(<name>);
+	< your code goes here >
+	__cr_end(<name>);
+	return (<value>);
+}
+
+ * A coroutine can yield by using __cr_return(<value>);
+ * Local variables can be accessed in a coroutine with __cr_var(<name>)
+ *
+ * To access a coroutine, first create a pointer to a state variable
+ * of its <typename>, initialise it with something like
+ *	<pointervar> = __cr_new(<typename>, <name>);
+ * and call the coroutine using __cr_call(<pointervar> [, <arguments>]).
+ *
+ * Example:
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "coroutine.h"
+
+__coroutine_decl(footype, int, int);
+
+__coroutine_proto(footype, foo, int, int);
+
+__coroutine_defn(footype, foo, int, int bar)
+{
+	int a;
+
+	__cr_begin(foo);
+	__cr_var(a) = 0;
+	while (1) {
+		__cr_var(a) += bar;
+		__cr_return(__cr_var(a));
+	}
+	__cr_end(foo);
+	return (0);
+}
+
+int
+main(void)
+{
+	footype *c;
+	int i = 0;
+
+	c = __cr_new(footype, foo);
+	for (i = 1; i <= 3; i++) {
+		printf("%d: ret=%d\n", i, __cr_call(c, i));
+	}
+
+	return (0);
+}
+
+ */
+
+#ifndef __COROUTINE_H
+#define __COROUTINE_H
+
+/* configuration: malloc/free functions to use */
+#ifndef __coroutine_malloc
+#define __coroutine_malloc	malloc
+#endif
+
+#ifndef __coroutine_free
+#define __coroutine_free	free
+#endif
+
+/* configuration: implementation style to use */
+#if !defined(__COROUTINE_DUFF)
+#ifdef __GNUC__
+#define __COROUTINE_DUFF	0
+#else
+#define __COROUTINE_DUFF	1
+#endif
+#endif
+
+
+/* concatenation macro, used internally */
+#define __CR(x, y)		__coroutine_ ## x ## _ ## y
+
+
+#if __COROUTINE_DUFF
+#define __coroutine_content	void *ptr
+#else
+#define __coroutine_content	unsigned long lno
+#endif
+
+#define __coroutine_decl(_typename, _rettype, ...)			\
+	struct __CR(struct, _typename);					\
+	typedef _rettype (*__CR(ptr, _typename))(			\
+	    struct __CR(struct, _typename) **, ##__VA_ARGS__);		\
+	typedef struct __CR(struct, _typename) {			\
+		__CR(ptr, _typename) __fptr;				\
+		__coroutine_content;					\
+	} _typename
+
+#define __coroutine_initctx(_name) do {					\
+	*__cr_ectx = __coroutine_malloc(sizeof (*__cr_ictx));		\
+	(*__cr_ectx)->__fptr = &_name;					\
+} while (/* CONSTCOND */ 0)
+
+#define __coroutine_proto(_typename, _name, _rettype, ...)		\
+	_rettype _name(_typename **, ##__VA_ARGS__)
+
+#define __coroutine_defn(_typename, _name, _rettype, ...)		\
+	_rettype _name(_typename **__cr_ectx, ##__VA_ARGS__)		\
+	{								\
+		struct __CR(internal, _name) {				\
+			/* __cr_internal must be first */		\
+			_typename __cr_internal;			\
+			struct	/* ... yes, here the macro ends */
+
+#define __coroutine_checkctx(_name) do {				\
+	/* check if the struct passed matches */			\
+	if ((*__cr_ectx)->__fptr != &_name)				\
+		abort();						\
+	__cr_ictx = (struct __CR(internal, _name) *)(*__cr_ectx);	\
+} while (/* CONSTCOND */ 0)
+
+#if __COROUTINE_DUFF
+
+#define __coroutine_begin(_name)					\
+			} __cr_data;					\
+		} *__cr_ictx;						\
+									\
+	if (*__cr_ectx == NULL) {					\
+		__coroutine_initctx(_name);				\
+		(*__cr_ectx)->ptr = NULL;				\
+		goto __CR(endlbl, _name);				\
+	} else								\
+		__coroutine_checkctx(_name);				\
+	if ((*__cr_ectx)->ptr != NULL)					\
+		__extension__({ goto *((*__cr_ectx)->ptr); })
+
+#define __coroutine_return(value) __extension__({			\
+	__label__ __cr_tmplbl;						\
+									\
+	(*__cr_ectx)->ptr = &&__cr_tmplbl;				\
+	return value;	/* no parens: void function support */		\
+ __cr_tmplbl:								\
+	;								\
+})
+
+#define __coroutine_end(_name)						\
+	if (*__cr_ectx != NULL) {					\
+		__coroutine_free(*__cr_ectx);				\
+		*__cr_ectx = NULL;					\
+	}								\
+	__CR(endlbl, _name):
+
+#else /* !__COROUTINE_DUFF */
+
+#ifndef __extension__
+#define __extension__		/* nothing */
+#endif
+
+#define __coroutine_begin(_name)					\
+			} __cr_data;					\
+		} *__cr_ictx;						\
+									\
+	if (*__cr_ectx == NULL) {					\
+		__coroutine_initctx(_name);				\
+		(*__cr_ectx)->lno = 0;					\
+		goto __CR(endlbl, _name);				\
+	} else								\
+		__coroutine_checkctx(_name);				\
+	switch ((*__cr_ectx)->lno) {					\
+	case 0:
+
+#define __coroutine_return(x) do {					\
+	/* the following must all be on the same line */		\
+	(*__cr_ectx)->lno = __LINE__; return x; case __LINE__: ;	\
+} while (/* CONSTCOND */ 0)
+
+#define __coroutine_end(_name)						\
+		if (*__cr_ectx != NULL) {				\
+			__coroutine_free(*__cr_ectx);			\
+			*__cr_ectx = NULL;				\
+		}							\
+	}								\
+	__CR(endlbl, _name):
+
+#endif /* !__COROUTINE_DUFF */
+
+#define __coroutine_new(_typename, _name) __extension__({		\
+	_typename *__cr_tmp1 = NULL;					\
+	void (*__cr_tmp2)(_typename **) =				\
+	    (void (*)(_typename **))&_name;				\
+	(*__cr_tmp2)(&__cr_tmp1);					\
+	(__cr_tmp1);							\
+})
+
+#define __cr_new		__coroutine_new
+#define __cr_free		__coroutine_free
+#define __cr_begin		__coroutine_begin
+#define __cr_end		__coroutine_end
+#define __cr_call(_ctx, ...)	((_ctx)->__fptr(&(_ctx), ##__VA_ARGS__))
+#define __cr_return(v)		__coroutine_return(v)
+#define __cr_var(_name)		(__cr_ictx->__cr_data._name)
+
+#endif /* __COROUTINE_H */
