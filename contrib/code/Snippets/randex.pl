ef071bb57b34430010a8da790466ed7910766894
tg <>
2008-07-08 23:36:00 +0000

    debug version of an irssi script doing the new MirSirc™ randex protocol too dedicated to Vutral, who can’t let his leenocks go…

---
+++ contrib/code/Snippets/randex.pl
@@ -0,0 +1,107 @@
+# $MirOS: src/share/misc/licence.template,v 1.24 2008/04/22 11:43:31 tg Rel $
+#-
+# Copyright (c) 2008
+#	Thorsten Glaser <tg@mirbsd.org>
+#
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un-
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
+#
+# This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person's immediate fault when using the work as intended.
+#-
+# Irssi extension to support MirSirc's randex protocol
+
+require BSD::arc4random;
+
+use vars qw($VERSION %IRSSI);
+$VERSION = '200807082330';
+%IRSSI = (
+	authors		=> 'Thorsten Glaser',
+	contact		=> 'tg@mirbsd.de',
+	name		=> 'randex',
+	description	=> 'implement MirSirc\'s randex protocol',
+	license		=> 'MirOS',
+	url		=> 'http://cvs.mirbsd.de/ports/net/sirc/dist/dsircp',
+	changed		=> $VERSION,
+	modules		=> '',
+	commands	=> "randex"
+);
+
+my $debug = 1;
+
+sub
+dprintf
+{
+	my $text = shift;
+
+	if (!$debug) {
+		return;
+	}
+
+	Irssi::print("D: randex: ${text}");
+}
+
+sub
+cmd_randex
+{
+	my ($data, $server, $witem) = @_;
+	my $recip = undef;
+	my $towho;
+	my $s;
+
+	if (!$server || !$server->{connected}) {
+		Irssi::print("Not connected to server");
+		return;
+	}
+
+	if ($data) {
+		$recip = $server;
+		$towho = $data;
+	} elsif ($witem && ($witem->{type} eq "QUERY")) {
+		$recip = $witem->{server};
+		$towho = $witem->{name};
+	}
+	if (!defined($recip)) {
+		Irssi::print("You must specify a nick or channel!");
+		return;
+	}
+
+	$s = pack("u", BSD::arc4random::arc4random_bytes(32, "for $towho"));
+	chop($s);
+	$recip->send_raw("PRIVMSG ${towho} :\caENTROPY ${s}\ca");
+	dprintf("initiating protocol with '${towho}': 「${s}」");
+}
+
+sub
+process_entropy_request
+{
+	my ($server, $args, $nick, $address, $target) = @_;
+	my $evalue = pack("u", BSD::arc4random::arc4random_bytes(32,
+	    "from $nick $args"));
+	chop($evalue);
+	$server->ctcp_send_reply("NOTICE ${nick} :\caRANDOM ${evalue}\ca");
+	dprintf("incoming request from '${nick}': 「${args}」");
+	dprintf("outgoing response to '${nick}': 「${evalue}」");
+}
+
+sub
+process_random_response
+{
+	my ($server, $args, $nick, $address, $target) = @_;
+
+	BSD::arc4random::arc4random_pushk("by $nick $args");
+	dprintf("incoming response from '${nick}': 「${args}」");
+}
+
+Irssi::command_bind('randex', 'cmd_randex');
+Irssi::signal_add('ctcp msg entropy', \&process_entropy_request);
+Irssi::signal_add('ctcp reply random', \&process_random_response);
