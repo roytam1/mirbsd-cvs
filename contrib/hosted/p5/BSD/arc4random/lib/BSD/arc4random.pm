e96a3bb82e367521cb7013fe84bf5a2b6ea7ccca
tg <>
2008-07-13 01:46:50 +0000

    put the Perl book Dr. Robert “Pfeffer” Arnold gave me for birthday to good use and implement a tied $RANDOM variable behaving like in mksh

--- contrib/hosted/p5/BSD/arc4random/lib/BSD/arc4random.pm
+++ contrib/hosted/p5/BSD/arc4random/lib/BSD/arc4random.pm
@@ -1,4 +1,4 @@
-# $MirOS: contrib/hosted/p5/BSD/arc4random/lib/BSD/arc4random.pm,v 1.20 2008/07/12 19:21:56 tg Exp $
+# $MirOS: contrib/hosted/p5/BSD/arc4random/lib/BSD/arc4random.pm,v 1.21 2008/07/12 23:59:21 tg Exp $
 #-
 # Copyright (c) 2008
 #	Thorsten Glaser <tg@mirbsd.org>
@@ -23,17 +23,17 @@ package BSD::arc4random;
 use 5.004_63;
 use strict;
 use warnings;
-use integer;
 use threads::shared;
 
 BEGIN {
 	require Exporter;
 	require DynaLoader;
 	use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION = 1.03;
+	$VERSION = 1.10;
 	@ISA = qw(Exporter DynaLoader);
 	@EXPORT = qw();
 	@EXPORT_OK = qw(
+		$RANDOM
 		&arc4random
 		&arc4random_addrandom
 		&arc4random_bytes
@@ -45,12 +45,16 @@ BEGIN {
 	);
 }
 
-sub have_kintf() {}	# public constant function, prototyped
+use vars qw($RANDOM);		# public tied integer variable
+sub have_kintf() {}		# public constant function, prototyped
 
+
+# private thread lock
 my $arcfour_lock : shared;
 
 bootstrap BSD::arc4random $BSD::arc4random::VERSION;
 
+# public thread-safe functions
 sub
 arc4random()
 {
@@ -120,6 +124,42 @@ arc4random_bytes($;$)
 	return $rv;
 }
 
+# private implementation for a tied $RANDOM variable
+sub
+TIESCALAR
+{
+	my $class = shift;
+	my $max = shift;
+
+	if (!defined($max) || ($max = int($max)) > 0xFFFFFFFE || $max < 0) {
+		$max = 0;
+	}
+
+	return bless \$max, $class;
+}
+
+sub
+FETCH
+{
+	my $self = shift;
+
+	my $x = ($$self == 0 ? arc4random() : (arc4random() % ($$self + 1)));
+
+	return $x;
+}
+
+sub
+STORE
+{
+	my $self = shift;
+	my $value = shift;
+
+	arc4random_pushb($value);
+}
+
+# tie the public $RANDOM variable to an mksh-style implementation
+tie $RANDOM, 'BSD::arc4random', 0x7FFF;
+
 # we are nice and re-seed perl's internal PRNG as well
 srand(arc4random_pushb(pack("F*", rand(), rand(), rand(), rand())));
 
@@ -141,6 +181,7 @@ BSD::arc4random - Perl interface to the arc4 random number generator
     $v = arc4random_pushk("entropy to pass to the kernel");
   }
   $s = arc4random_bytes(16);
+  print $RANDOM;
 
 =head1 DESCRIPTION
 
@@ -189,6 +230,21 @@ they merely map to B<arc4random_addrandom> instead.
 This function returns a string containing as many random bytes as
 requested by the integral argument I<num>.
 An optional I<pbuf> argument is passed to the system first.
+
+=head2 PACKAGE VARIABLES
+
+=item B<$RANDOM>
+
+The B<$RANDOM> returns a random value in the range S<[0; 32767]> on
+each read attempt and pushes any value it is assigned to the kernel.
+It is tied at module load time.
+
+=item tie I<variable>, 'BSD::arc4random'[, I<max>]
+
+You can tie any scalar variable to this package; the I<max> argument
+is the maximum number returned; if undefined, 0 or S<E<62>= 0xFFFFFFFF>,
+no bound is used, and values in the range S<[0; 2**32-1]> are returned.
+They will behave like B<$RANDOM>.
 
 =head1 AUTHOR
 
