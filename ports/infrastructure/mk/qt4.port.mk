24d43dc1add80385c6b292d042d6e658bbcf1c59
bsiegert <>
2008-09-13 14:38:12 +0000

    Correct LIB_DEPENDS (I hope) for qt4-mac, except if MODQT_NO_DEPENDS=Yes

--- ports/infrastructure/mk/qt4.port.mk
+++ ports/infrastructure/mk/qt4.port.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: ports/infrastructure/mk/qt4.port.mk,v 1.1 2008/08/10 09:16:48 bsiegert Exp $
 
 USE_CXX=		Yes
 
@@ -8,3 +8,6 @@ QTDIR=			${LOCALBASE}/qt4
 MODQT_MOC=		${QTDIR}/bin/moc
 MODQT_UIC=		${QTDIR}/bin/uic
 
+.if ${MODQT_NO_DEPENDS:L} != "yes"
+LIB_DEPEDS+=		../qt4/lib/QtCore.la::aqua/qt4
+.endif
