21af0daef9a4b318df339eb59092dbaeb8e31876
tg <>
2008-10-20 11:37:38 +0000

    * allow arc4random_pushb(3) on GNU/Cygwin32 * improve code while here

--- contrib/code/Snippets/arc4random.c
+++ contrib/code/Snippets/arc4random.c
@@ -1,4 +1,4 @@
-/* $MirOS: contrib/code/Snippets/arc4random.c,v 1.2 2007/09/09 22:14:04 tg Exp $ */
+static const char __rcsid[] = "$MirOS: contrib/code/Snippets/arc4random.c,v 1.3 2008/03/04 22:53:14 tg Exp $";
 
 /*-
  * Arc4 random number generator for OpenBSD.
@@ -29,6 +29,28 @@
  * This is arc4random(3) using urandom.
  */
 
+/*-
+ * Copyright (c) 2008
+ *	Thorsten Glaser <tg@mirbsd.de>
+ * This is arc4random(3) made more portable,
+ * as well as arc4random_pushb(3) for Cygwin.
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un-
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person's immediate fault when using the work as intended.
+ */
+
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -43,6 +65,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifdef __CYGWIN__
+#include <stdbool.h>
+
+#define WIN32_WINNT 0x400
+#define _WIN32_WINNT 0x400
+#include <windows.h>
+#include <wincrypt.h>
+#endif
+
 struct arc4_stream {
 	uint8_t i;
 	uint8_t j;
@@ -54,10 +85,17 @@ static struct arc4_stream rs;
 static pid_t arc4_stir_pid;
 
 static uint8_t arc4_getbyte(struct arc4_stream *);
+static void stir_finish(struct arc4_stream *, int);
+#ifdef __CYGWIN__
+static bool w32_cryptoapi_getrand(uint8_t *, size_t);
+#endif
 
 u_int32_t arc4random(void);
 void arc4random_addrandom(u_char *, int);
 void arc4random_stir(void);
+#ifdef __CYGWIN__
+uint32_t arc4random_pushb(const void *, size_t);
+#endif
 
 static void
 arc4_init(struct arc4_stream *as)
@@ -90,7 +128,7 @@ arc4_addrandom(struct arc4_stream *as, u_char *dat, int datlen)
 static void
 arc4_stir(struct arc4_stream *as)
 {
-	int     n, fd;
+	int fd;
 	struct {
 		struct timeval tv;
 		u_int rnd[(128 - sizeof(struct timeval)) / sizeof(u_int)];
@@ -98,6 +136,12 @@ arc4_stir(struct arc4_stream *as)
 	size_t sz = 0;
 
 	gettimeofday(&rdat.tv, NULL);
+	memcpy(rdat.rnd, __rcsid, MIN(sizeof (__rcsid), sizeof (rdat.rnd)));
+
+#ifdef __CYGWIN__
+	if (w32_cryptoapi_getrand((char *)rdat.rnd, sizeof (rdat.rnd)))
+		goto stir_okay;
+#endif
 
 	/* /dev/urandom is a multithread interface, sysctl is not. */
 	/* Try to use /dev/urandom before sysctl. */
@@ -134,19 +178,37 @@ arc4_stir(struct arc4_stream *as)
 #endif
 	}
 
-	arc4_stir_pid = getpid();
+ stir_okay:
+	fd = arc4_getbyte(as);
+
 	/*
 	 * Time to give up. If no entropy could be found then we will just
 	 * use gettimeofday.
 	 */
 	arc4_addrandom(as, (void *)&rdat, sizeof(rdat));
 
+	stir_finish(as, fd);
+}
+
+static void
+stir_finish(struct arc4_stream *as, int av)
+{
+	size_t n;
+
+	arc4_stir_pid = getpid();
+
 	/*
 	 * Discard early keystream, as per recommendations in:
 	 * http://www.wisdom.weizmann.ac.il/~itsik/RC4/Papers/Rc4_ksa.ps
 	 * We discard 256 words. A long word is 4 bytes.
+	 * We also discard a randomly fuzzed amount.
 	 */
-	for (n = 0; n < 256 * 4; n ++)
+	n = 256 * 4 + (arc4_getbyte(as) & 0x0F);
+	while (av) {
+		n += (av & 0x0F);
+		av >>= 4;
+	}
+	while (n--)
 		arc4_getbyte(as);
 }
 
@@ -200,3 +262,57 @@ arc4random(void)
 		arc4random_stir();
 	return arc4_getword(&rs);
 }
+
+#ifdef __CYGWIN__
+static bool
+w32_cryptoapi_getrand(uint8_t *buf, size_t len)
+{
+	static bool has_provider = false;
+	static HCRYPTPROV p;
+
+	if (!has_provider) {
+		if (!CryptAcquireContext(&p, NULL, NULL, PROV_RSA_FULL, 0)) {
+			if ((HRESULT)GetLastError() != NTE_BAD_KEYSET)
+				return (false);
+			if (!CryptAcquireContext(&p, NULL, NULL, PROV_RSA_FULL,
+			    CRYPT_NEWKEYSET))
+				return (false);
+		}
+		has_provider = true;
+	}
+	return (CryptGenRandom(p, len, buf) ? true : false);
+}
+
+uint32_t
+arc4random_pushb(const void *src, size_t len)
+{
+	size_t rlen;
+	union {
+		uint8_t buf[256];
+		struct timeval tv;
+		uint32_t xbuf;
+	} idat;
+	const uint8_t *cbuf = (const uint8_t *)src;
+	uint32_t res = 1;
+
+	if (!rs_initialized) {
+		arc4_init(&rs);
+		rs_initialized = 1;
+	}
+
+	gettimeofday(&idat.tv, NULL);
+	for (rlen = 0; rlen < len; ++rlen)
+		idat.buf[rlen % sizeof (idat)] ^= cbuf[rlen];
+	rlen = MIN(sizeof (idat), MAX(sizeof (struct timeval), len));
+
+	if (w32_cryptoapi_getrand(&idat.buf[0], rlen))
+		res = 0;
+	arc4_addrandom(&rs, &idat.buf[0], rlen);
+	if (res)
+		res = idat.xbuf;
+	else
+		/* we got entropy from the kernel, so consider us stirred */
+		stir_finish(&rs, idat.buf[5]);
+	return (res ^ arc4_getword(&rs));
+}
+#endif
