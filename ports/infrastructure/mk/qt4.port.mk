26ba139624b9a5df7c0259d0206c50e1010c4a2c
bsiegert <>
2009-02-24 17:52:32 +0000

    fix name of dependency

--- ports/infrastructure/mk/qt4.port.mk
+++ ports/infrastructure/mk/qt4.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/qt4.port.mk,v 1.2 2008/09/13 14:38:12 bsiegert Exp $
+# $MirOS: ports/infrastructure/mk/qt4.port.mk,v 1.3 2009/01/03 20:37:45 bsiegert Exp $
 
 USE_CXX=		Yes
 
@@ -9,5 +9,5 @@ MODQT_MOC=		${QTDIR}/bin/moc
 MODQT_UIC=		${QTDIR}/bin/uic
 
 .if ${MODQT_NO_DEPENDS:L} != "yes"
-LIB_DEPENDS+=		../qt4/lib/QtCore.la::aqua/qt4
+LIB_DEPENDS+=		../qt4/lib/QtCore.la::aqua/qt4-mac
 .endif
