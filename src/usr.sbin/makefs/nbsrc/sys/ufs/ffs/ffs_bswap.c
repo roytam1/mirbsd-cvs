f4ec6bcf3c4e0fd97810ffeb0d185c91ed40b98b
tg <>
2010-03-06 20:36:44 +0000

    sync with TNF

--- src/usr.sbin/makefs/nbsrc/sys/ufs/ffs/ffs_bswap.c
+++ src/usr.sbin/makefs/nbsrc/sys/ufs/ffs/ffs_bswap.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ffs_bswap.c,v 1.32 2005/12/11 12:25:25 christos Exp $	*/
+/*	$NetBSD: ffs_bswap.c,v 1.34 2009/10/19 18:41:17 bouyer Exp $	*/
 
 /*
  * Copyright (c) 1998 Manuel Bouyer.
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Manuel Bouyer.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -35,7 +30,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: ffs_bswap.c,v 1.32 2005/12/11 12:25:25 christos Exp $");
+__KERNEL_RCSID(0, "$NetBSD: ffs_bswap.c,v 1.34 2009/10/19 18:41:17 bouyer Exp $");
 
 #include <sys/param.h>
 #if defined(_KERNEL)
@@ -58,7 +53,7 @@ __KERNEL_RCSID(0, "$NetBSD: ffs_bswap.c,v 1.32 2005/12/11 12:25:25 christos Exp 
 void
 ffs_sb_swap(struct fs *o, struct fs *n)
 {
-	int i;
+	size_t i;
 	u_int32_t *o32, *n32;
 
 	/*
@@ -164,7 +159,7 @@ ffs_dinode2_swap(struct ufs2_dinode *o, struct ufs2_dinode *n)
 void
 ffs_csum_swap(struct csum *o, struct csum *n, int size)
 {
-	int i;
+	size_t i;
 	u_int32_t *oint, *nint;
 
 	oint = (u_int32_t*)o;
