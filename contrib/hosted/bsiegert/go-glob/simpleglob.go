8b93ac80e6b4c67b0506b33c719fc2d41a44279b
bsiegert <>
2010-10-11 18:29:16 +0000

    Add a minimal (simple) Glob implementation

---
+++ contrib/hosted/bsiegert/go-glob/simpleglob.go
@@ -0,0 +1,74 @@
+//	$MirOS: contrib/hosted/bsiegert/go-glob/glob.go,v 1.4 2010/10/05 17:54:13 bsiegert Exp $
+
+package simpleglob
+
+import (
+	"container/vector"
+	"path"
+	"os"
+	"strings"
+)
+
+// Glob returns the names of all files matching pattern or nil
+// if there is no matching file. The syntax used by pattern is like in
+// path.Match. The pattern can contain a pathname.
+func Glob(pattern string) []string {
+	var m, matches vector.StringVector
+
+	if !ContainsMagic(pattern) {
+		if _, err := os.Stat(pattern); err == nil {
+			return []string{pattern}
+		}
+		return nil
+	}
+		
+	dir, file := path.Split(pattern)
+	if dir == "" {
+		dir = "."
+	} else {
+		dir = dir[0:len(dir)-1] // chop off tailing '/'
+	}
+
+	if ContainsMagic(dir) {
+		for _, d := range Glob(dir) {
+			m = Glob(path.Join(d, file))
+			matches.AppendVector(&m)
+		}
+	} else {
+		print("globInDir ", dir, " ", file, "\n") //DEBUG
+
+		fi, err := os.Stat(dir)
+		if err != nil || !fi.IsDirectory() {
+			return nil
+		}
+		print("globInDir going to open ", dir, "\n") //DEBUG
+		d, err := os.Open(dir, os.O_RDONLY, 0666)
+		if err != nil {
+			return nil
+		}
+		defer d.Close()
+
+		print("globInDir going to readdirnames ", dir, "\n") //DEBUG
+		names, err := d.Readdirnames(-1)
+		if err != nil {
+			return nil
+		}
+
+		for _, n := range names {
+			didmatch, err := path.Match(file, n)
+			if err != nil {
+				return nil
+			}
+			if didmatch {
+				matches.Push(path.Join(dir, n))
+			}
+		}
+	}
+	return matches
+}
+
+// ContainsMagic returns true if path contains any of the magic characters
+// recognized by path.Match.
+func ContainsMagic(path string) bool {
+	return strings.IndexAny(path, "*?[") != -1
+}
--- contrib/hosted/bsiegert/go-glob/test-glob.go
+++ contrib/hosted/bsiegert/go-glob/test-glob.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"./glob"
+	"./simpleglob"
 	"fmt"
 	"os"
 )
 
-func Err(filename string, err os.Error) bool {
-	fmt.Printf("Err called for %v: %v\n", filename, err)
-	return false
-}
 
 func main() {
 	for _, a := range os.Args[1:] {
-		matches, ok := glob.Glob(a, 0, Err)
-		if !ok {
+		matches := simpleglob.Glob(a)
+		if matches == nil {
 			fmt.Printf("Glob on %v not ok\n", a)
 		} else {
 			fmt.Printf("%v matches:\n", a)
