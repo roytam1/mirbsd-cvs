5c01b711e2f28fc79adcb7011425a4898449a70b
tg <>
2011-06-07 20:46:01 +0000

    fold in unmodified xsel.h from xsel-1.2.0

--- X11/extras/xsel/xsel.c
+++ X11/extras/xsel/xsel.c
@@ -34,7 +34,91 @@
 #include <X11/Intrinsic.h>
 #include <X11/Xatom.h>
 
-#include "xsel.h"
+#define AUTHOR "Conrad Parker <conrad@vergenet.net>"
+
+/* Default debug level (ship at 0) */
+#define DEBUG_LEVEL 0
+
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
+#define empty_string(s) (s==NULL||s[0]=='\0')
+#define free_string(s) { free(s); s=NULL; }
+
+/* Maximum line length for error messages */
+#define MAXLINE 4096
+
+/* Maximum filename length */
+#define MAXFNAME 1024
+
+/* Maximum incremental selection size. (Ripped from Xt) */
+#define MAX_SELECTION_INCR(dpy) (((65536 < XMaxRequestSize(dpy)) ? \
+        (65536 << 2)  : (XMaxRequestSize(dpy) << 2))-100)
+
+/*
+ * Debug levels (for print_debug()):
+ *
+ *   0  -  Fatal errors (default/unmaskable)
+ *   1  -  Non-fatal warning (essential debugging info)
+ *   2  -  Informative (generally useful debugging info)
+ *   3  -  Obscure (more detailed debugging info)
+ *   4  -  Trace (sequential trace of progress)
+ */
+
+#define D_FATAL 0
+#define D_WARN  1
+#define D_INFO  2
+#define D_OBSC  3
+#define D_TRACE 4
+
+/* An instance of a MULTIPLE SelectionRequest being served */
+typedef struct _MultTrack MultTrack;
+
+struct _MultTrack {
+  MultTrack * mparent;
+  Display * display;
+  Window requestor;
+  Atom property;
+  Atom selection;
+  Time time;
+  Atom * atoms;
+  unsigned long length;
+  unsigned long index;
+  unsigned char * sel;
+};
+
+/* Selection serving states */
+typedef enum {
+  S_NULL=0,
+  S_INCR_1,
+  S_INCR_2
+} IncrState;
+
+/* An instance of a selection being served */
+typedef struct _IncrTrack IncrTrack;
+
+struct _IncrTrack {
+  MultTrack * mparent;
+  IncrTrack * prev, * next;
+  IncrState state;
+  Display * display;
+  Window requestor;
+  Atom property;
+  Atom selection;
+  Time time;
+  Atom target;
+  int format;
+  unsigned char * data;
+  int nelements; /* total */
+  int offset, chunk, max_elements; /* all in terms of nelements */
+};
+
+/* Status of request handling */
+typedef int HandleResult;
+#define HANDLE_OK         0
+#define HANDLE_ERR        (1<<0)
+#define HANDLE_INCOMPLETE (1<<1)
+#define DID_DELETE        (1<<2)
 
 
 /* The name we were invoked as (argv[0]) */
