c48f392360587ff424f07c2a872f44d9d25e7f3e
tg <>
2011-06-21 20:27:22 +0000

    trim some more, it helps

--- contrib/hosted/tg/historic/ctr.php
+++ contrib/hosted/tg/historic/ctr.php
@@ -10,12 +10,12 @@
 /*-
  * $MirOS: contrib/hosted/tg/historic/ctr.php,v 1.3 2010/11/10 16:14:56 tg Exp $
  *-
- * Copyright (c) 2006, 2010
- *	Thorsten Glaser <tg@mirbsd.de>
+ * Copyright © 2006, 2010, 2011
+ *	Thorsten Glaser <tg@mirbsd.org>
  *
  * Provided that these terms and disclaimer and all copyright notices
  * are retained or reproduced in an accompanying document, permission
- * is granted to deal in this work without restriction, including un-
+ * is granted to deal in this work without restriction, including un‐
  * limited rights to use, publicly perform, distribute, sell, modify,
  * merge, give away, or sublicence.
  *
@@ -23,14 +23,14 @@
  * display the following acknowledgement:
  *	This product includes material provided by Thorsten Glaser.
  *
- * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
  * the utmost extent permitted by applicable law, neither express nor
  * implied; without malicious intent or gross negligence. In no event
  * may a licensor, author or contributor be held liable for indirect,
  * direct, other damage, loss, or other issues arising in any way out
  * of dealing in the work, even if advised of the possibility of such
  * damage or existence of a defect, except proven that it results out
- * of said person's immediate fault when using the work as intended.
+ * of said person’s immediate fault when using the work as intended.
  *-
  * Use this as follows (probably better with CDATA, but what the hey):
  *	<script type="text/javascript" language="JavaScript"><!--
@@ -80,6 +80,15 @@ $arg_id = $_REQUEST["id"];
 $arg_debug = $_REQUEST["debug"];
 /* trim HTML anchor if present */
 $hofs = strpos($arg_id, "#");
+if ($hofs !== false)
+	$arg_id = substr($arg_id, 0, $hofs);
+$hofs = strpos($arg_id, "?");
+if ($hofs !== false)
+	$arg_id = substr($arg_id, 0, $hofs);
+$hofs = strpos($arg_id, " ");
+if ($hofs !== false)
+	$arg_id = substr($arg_id, 0, $hofs);
+$hofs = strpos($arg_id, "	");
 if ($hofs !== false)
 	$arg_id = substr($arg_id, 0, $hofs);
 if (strlen($arg_id) < 1)
