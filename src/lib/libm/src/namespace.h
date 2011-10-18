eeb35f3776585c729fe9a6cb7e4809ce19384579
tg <>
2011-10-18 21:10:29 +0000

    stop playing stupid netbsd namespace games

--- src/lib/libm/src/namespace.h
+++ src/lib/libm/src/namespace.h
@@ -1,5 +1,7 @@
+/* $MirOS$ */
 /* $NetBSD: namespace.h,v 1.4 2011/07/26 16:10:16 joerg Exp $ */
 
+#if 0 /* stupid netbsd games */
 #define atan2 _atan2
 #define atan2f _atan2f
 #define hypot _hypot
@@ -9,6 +11,7 @@
 #define expf _expf
 #define log _log
 #define logf _logf
+#endif
 
 #if 0 /* not yet - need to review use in machdep code first */
 #define sin _sin
@@ -18,12 +21,14 @@
 #define finite _finite
 #define finitef _finitef
 #endif /* notyet */
+#if 0 /* stupid netbsd games */
 #define sinh _sinh
 #define sinhf _sinhf
 #define cosh _cosh
 #define coshf _coshf
 #define asin _asin
 #define asinf _asinf
+#endif
 
 #define casin _casin
 #define casinf _casinf
