14e191cd6292f41addd81c98046195e5454e115e
tg <>
2011-12-04 23:56:15 +0000

    this commit made my dedicated server reboot spontaneously, but the Pentium 120 with a Hercules graphics card (anno 1981) survives it:
    allow building and running the testsuite if the source or
    build directory contain spaces (not unusual on Windows®)

--- src/bin/mksh/check.pl
+++ src/bin/mksh/check.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $MirOS: src/bin/mksh/check.pl,v 1.1 2005/05/23 03:06:05 tg Exp $
+# $MirOS: src/bin/mksh/check.pl,v 1.2 2005/05/23 14:48:21 tg Exp $
 # $OpenBSD: th,v 1.11 2004/11/29 06:20:02 jsg Exp $
 #-
 # Test harness for pdksh tests.
@@ -494,6 +494,7 @@ run_test
 	if ($program_kludge) {
 	    @argv = split(' ', $test_prog);
 	} else {
+	    $test_prog =~ s/ /\\ /g;
 	    @argv = ($test_prog);
 	}
 	if (defined $test{'arguments'}) {
