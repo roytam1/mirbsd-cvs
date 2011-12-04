e05d1acf62f5347f6aa0285a9273ff2744dec838
tg <>
2011-12-04 23:58:21 +0000

    correct GPC diff from gmane.comp.compilers.gpc (Russell Whitaker, Frank Heckenbach)

--- gcc/gcc/gcc.c
+++ gcc/gcc/gcc.c
@@ -1,4 +1,4 @@
-/* $MirOS: gcc/gcc/gcc.c,v 1.4 2005/04/17 03:17:47 tg Exp $ */
+/* $MirOS: gcc/gcc/gcc.c,v 1.5 2005/05/14 16:21:58 tg Exp $ */
 
 /* Compiler driver program that can handle many languages.
    Copyright (C) 1987, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
@@ -771,8 +771,8 @@ static const char *cc1_options =
 "%{pg:%{fomit-frame-pointer:%e-pg and -fomit-frame-pointer are incompatible}}\
  %1 %{!Q:-quiet} -dumpbase %B %{d*} %{m*} %{a*}\
  %{c|S:%{o*:-auxbase-strip %*}%{!o*:-auxbase %b}}%{!c:%{!S:-auxbase %b}}\
- %{g*} %{O*} %{W*&pedantic*} %{w} %{std*&ansi&trigraphs}\
- %{v:-version} %{pg:-p} %{p} %{f*} %{undef}\
+ %{g*} %{O*} %{f*&W*&pedantic*&w} %{std*&ansi&trigraphs}\
+ %{v:-version} %{pg:-p} %{p} %{undef}\
  %{Qn:-fno-ident} %{--help:--help}\
  %{--target-help:--target-help}\
  %{!fsyntax-only:%{S:%W{o*}%{!o*:-o %b.s}}}\
