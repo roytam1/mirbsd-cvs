7678cfb4d08eda7e50eeb49866eb3be513d59230
cvs2git <admin@example.com>
2011-12-05 00:01:58 +0000

    This commit was manufactured by cvs2git to create tag 'mksh-R26b'.

--- ports/www/opera/files/opera.sh
+++
@@ -1,7 +0,0 @@
-#!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.5 2006/06/20 18:20:37 tg Exp $
-
-export OPERA_DIR=@PREFIX@/share/opera
-export LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
-
-exec @PREFIX@/libexec/opera "$@"
