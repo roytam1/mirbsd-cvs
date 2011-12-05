a9ebbf03fa93333f15366b2b0ab7d216fac961f6
tg <>
2011-12-05 00:05:49 +0000

    fix the already long known cases of brokenness (or at least superfluous warnings) when ${BSDSRCDIR} and/or ${BSDOBJDIR} don’t exist

--- src/share/mk/bsd.obj.mk
+++ src/share/mk/bsd.obj.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.obj.mk,v 1.5 2005/05/06 16:57:59 tg Exp $
+# $MirOS: src/share/mk/bsd.obj.mk,v 1.6 2006/07/30 23:21:57 tg Exp $
 # $OpenBSD: bsd.obj.mk,v 1.12 2003/10/28 17:09:33 espie Exp $
 # $NetBSD: bsd.obj.mk,v 1.9 1996/04/10 21:08:05 thorpej Exp $
 
@@ -27,11 +27,11 @@ __objdir=	${__baseobjdir}
 .    endif
 
 .    if defined(USR_OBJMACHINE)
-__usrobjdir!=	readlink -nf ${BSDOBJDIR}.${MACHINE} || \
-		    echo ${BSDOBJDIR:Q}.${MACHINE:Q}
+__usrobjdir!=	readlink -nf ${BSDOBJDIR}.${MACHINE} 2>&- || \
+		    print -nr -- ${BSDOBJDIR:Q}.${MACHINE:Q}
 __usrobjdirpf=
 .    else
-__usrobjdir!=	readlink -nf ${BSDOBJDIR} || echo ${BSDOBJDIR:Q}
+__usrobjdir!=	readlink -nf ${BSDOBJDIR} 2>&- || print -nr -- ${BSDOBJDIR:Q}
 .      if defined(OBJMACHINE)
 __usrobjdirpf=	.${MACHINE}
 .      else
@@ -42,10 +42,14 @@ __usrobjdirpf=
 _SUBDIRUSE:
 
 obj! _SUBDIRUSE
-	@cd ${.CURDIR}; here=$$(readlink -nf .); \
-	bsdsrcdir=$$(readlink -nf ${BSDSRCDIR}); \
-	subdir=$${here#$${bsdsrcdir}/}; \
-	if [[ $$here != $$subdir ]]; then \
+	@cd ${.CURDIR}; \
+	tryobjdir=0; \
+	if bsdsrcdir=$$(readlink -nf ${BSDSRCDIR} 2>&-); then \
+		here=$$(readlink -nf .); \
+		subdir=$${here#$${bsdsrcdir}/}; \
+		[[ $$here = $$subdir ]] || tryobjdir=1; \
+	fi; \
+	if [[ $$tryobjdir = 1 ]]; then \
 		dest=${__usrobjdir}/$$subdir${__usrobjdirpf}; \
 		print -r "$$here/${__objdir} -> $$dest"; \
 		if [[ ! -L ${__objdir} \
