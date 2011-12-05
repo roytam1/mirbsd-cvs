c5a1e3dd5daad6b0f53c0057843cc93823952ced
tg <>
2011-12-05 00:04:12 +0000

    * -Wall clean * implement tar -xO mode for extracting to stdout, GNU tar compatible
    for freewrt
    
    "schön" wbx@

--- src/bin/pax/options.h
+++ src/bin/pax/options.h
@@ -1,4 +1,4 @@
-/**	$MirOS$ */
+/**	$MirOS: src/bin/pax/options.h,v 1.2 2006/06/19 19:22:08 tg Exp $ */
 /*	$OpenBSD: options.h,v 1.4 2003/06/13 17:51:14 millert Exp $	*/
 /*	$NetBSD: options.h,v 1.3 1995/03/21 09:07:32 cgd Exp $	*/
 
@@ -126,3 +126,5 @@
 #define	ANON_MAXVAL	0x003F
 extern int anonarch;
 void anonarch_init(void);
+
+extern int to_stdout;
