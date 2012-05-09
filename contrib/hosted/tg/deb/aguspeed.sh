3e0e14cfff095c970a13cf7cc17346f8bd6bd889
tg <>
2012-05-09 08:58:36 +0000

    sync

--- contrib/hosted/tg/deb/aguspeed.sh
+++ contrib/hosted/tg/deb/aguspeed.sh
@@ -1,5 +1,5 @@
 #!/bin/mksh
-# $MirOS: contrib/hosted/tg/deb/aguspeed.sh,v 1.1 2011/05/25 17:46:54 tg Exp $
+# $MirOS: contrib/hosted/tg/deb/aguspeed.sh,v 1.2 2011/06/09 20:30:24 tg Exp $
 # $Id$
 #-
 # From Planet Debian. Not complicated enough for copyright.
@@ -8,6 +8,13 @@ unset LANGUAGE
 export LC_ALL=C
 
 (( USER_ID )) && exec sudo mksh "$0" "$@"
+
+if [[ $1 = -f ]]; then
+	fflag=1
+	shift
+else
+	fflag=0
+fi
 
 if [[ -n $1 && -s /etc/apt/sources.list.$1 ]]; then
 	cat "/etc/apt/sources.list.$1" >/etc/apt/sources.list
@@ -19,7 +26,7 @@ apt-get update || rv=$?
 dpkg --clear-avail || rv=${rv:-$?}
 # this emits a harmless warning on squeeze
 dpkg --forget-old-unavail || rv=${rv:-$?}
-if sa=$(whence -p sync-available); then
+(( fflag )) || if sa=$(whence -p sync-available); then
 	# call with mksh due to *buntu bug
 	mksh "$sa" || rv=${rv:-$?}
 fi
