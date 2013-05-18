258936cefc7cc8be7e2b56702927d0ded6213f98
tg <>
2013-05-18 15:19:46 +0000

    oeps, bump

--- src/usr.bin/ssh/version.h
+++ src/usr.bin/ssh/version.h
@@ -1,10 +1,10 @@
-/* $MirOS: src/usr.bin/ssh/version.h,v 1.22 2006/09/24 17:48:35 tg Exp $ */
+/* $MirOS: src/usr.bin/ssh/version.h,v 1.23 2006/10/02 23:24:15 tg Exp $ */
 /* $NetBSD: version.h,v 1.28 2003/04/03 06:21:37 itojun Exp $	*/
 /* $OpenBSD: version.h,v 1.47 2006/08/30 00:14:37 djm Exp $ */
 
 #define __OPENSSH_VERSION	"OpenSSH_4.4"
 
-#define __MIRBSDSSH_VERSION	"MirBSD_Secure_Shell-09uA2"
+#define __MIRBSDSSH_VERSION	"MirBSD_Secure_Shell-09uA3"
 
 /*
  * it is important to retain OpenSSH version identification part, it is
