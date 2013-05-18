59050b1f3238a58f744960eb48865df6882294ea
tg <>
2013-05-18 15:06:03 +0000

    fix thinko introduced while cross-building a native compiler bump version to #9-release while here

--- gcc/gcc/version.c
+++ gcc/gcc/version.c
@@ -1,4 +1,4 @@
-/* $MirOS: gcc/gcc/version.c,v 1.31 2006/06/15 19:18:46 tg Exp $ */
+/* $MirOS: gcc/gcc/version.c,v 1.32 2006/06/17 17:17:29 tg Exp $ */
 
 #include "version.h"
 
@@ -7,7 +7,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.4.6 (propolice; gpc; MirOS 0912)";
+const char version_string[] = "3.4.6 (propolice; gpc; MirOS 0980)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
