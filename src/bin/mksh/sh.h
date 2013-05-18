e521b00ccd7c1ea6febdac6f68000c7babe55fb1
tg <>
2013-05-18 16:15:55 +0000

    my change to edit.c:x_free_words introduced a bug because the afreechk() macro was not side-effect safe → make it so
    oops, but this was definitively not obvious
    I think I know now why people tend to write macros in ALL CAPS

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -8,7 +8,7 @@
 /*	$OpenBSD: c_test.h,v 1.4 2004/12/20 11:34:26 otto Exp $	*/
 /*	$OpenBSD: tty.h,v 1.5 2004/12/20 11:34:26 otto Exp $	*/
 
-#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.205 2008/04/16 21:56:03 tg Exp $"
+#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.206 2008/04/19 17:21:54 tg Exp $"
 #define MKSH_VERSION "R33 2008/04/16"
 
 #if HAVE_SYS_PARAM_H
@@ -1207,8 +1207,9 @@ void *alloc(size_t, Area *);
 void *aresize(void *, size_t, Area *);
 void afree(void *, Area *);
 #define afreechk(s)	do {		\
-	if (s)				\
-		afree(s, ATEMP);	\
+	void *afree_t = (s);		\
+	if (afree_t)			\
+		afree(afree_t, ATEMP);	\
 } while (0)
 #define afreechv(v,s)	do {		\
 	if (v)				\
