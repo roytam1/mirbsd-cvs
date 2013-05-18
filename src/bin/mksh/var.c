adaf7e3a56f87846c1ee4a9acac6586d4cd6e305
tg <>
2013-05-18 16:42:45 +0000

    do not lie

--- src/bin/mksh/var.c
+++ src/bin/mksh/var.c
@@ -2,7 +2,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/var.c,v 1.64 2008/12/04 18:11:08 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/var.c,v 1.65 2008/12/13 17:02:18 tg Exp $");
 
 /*
  * Variables
@@ -104,7 +104,7 @@ initvar(void)
 	int i;
 	struct tbl *tp;
 
-	ktinit(&specials, APERM, 32); /* must be 2^n (currently 17 specials) */
+	ktinit(&specials, APERM, 16); /* must be 2^n (currently 11 specials) */
 	for (i = 0; names[i].name; i++) {
 		tp = ktenter(&specials, names[i].name, hash(names[i].name));
 		tp->flag = DEFINED|ISSET;
