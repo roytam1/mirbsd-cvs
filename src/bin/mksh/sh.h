346376da9a6e181f4c2593fbc4e5596283592808
tg <>
2013-05-18 17:24:33 +0000

    SUSv4 says trap foo UNKNOWN is not a syntax error, merely exit ≠0. Inspired by 673dab8698b0399c967216c02262eaf95361a75c.

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -154,9 +154,9 @@
 #endif
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.424 2011/01/22 20:33:14 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.425 2011/01/30 01:35:35 tg Exp $");
 #endif
-#define MKSH_VERSION "R39 2011/01/22"
+#define MKSH_VERSION "R39 2011/01/29"
 
 #ifndef MKSH_INCLUDES_ONLY
 
