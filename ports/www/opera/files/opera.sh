8068018223e6fb43b1874821ab2d7270320cf979
tg <>
2013-05-18 16:29:31 +0000

    Create a new package, fedora-libs, from OpenBSD’s fedora_base and fedora_motif, containing Fedora Core 4 libraries, by suggestion of the MidnightBSD developers; convert Opera and AcroRead to use it, tentatively; this fixes plugins in Opera, for instance, and possi- bly opens the door for the Opera 9.5x upgrade – which MidnightBSD doesn’t have yet either. While here sprinkle some uname checks for mnbsd, fix the emulated ldd, and re-enable plugins/java for Opera
    NOTE: This is EXPERIMENTAL.

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,5 +1,5 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.10 2007/04/30 12:47:16 tg Exp $
+# $MirOS: ports/www/opera/files/opera.sh,v 1.11 2007/05/22 19:56:58 tg Exp $
 
 cfg=$HOME/.opera/opera6.ini
 [[ -e $cfg ]] && if ! fgrep -q "Synchronous DNS Lookup=1" $cfg; then
@@ -8,9 +8,7 @@ fi
 
 export OPERA_SCRIPT_PATH=$(readlink -nf $0) OPERA_NUM_XSHM=0
 export OPERA_BINARYDIR=@PREFIX@/libexec/ OPERA_DIR=@PREFIX@/share/opera
-LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
-LD_PRELOAD=/dbl/libnss_compat.so.2:/dbl/libnss_dns.so.2:/dbl/libnss_files.so.2:${LD_PRELOAD}
+LD_LIBRARY_PATH=/lib:@PREFIX@/libexec:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH%%:}
-export OPERA_LD_PRELOAD=${LD_PRELOAD%%:} LD_PRELOAD=${LD_PRELOAD%%:}
 
 exec @PREFIX@/libexec/opera "$@"
