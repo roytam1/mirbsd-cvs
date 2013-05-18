95735de1a666847d51781e646b277ce826c3771e
tg <>
2013-05-18 17:57:07 +0000

    provide an HTML version (even hypertexted!) of sendmail “cf/README”

--- src/scripts/genhtman.sh
+++ src/scripts/genhtman.sh
@@ -1,5 +1,23 @@
 #!/bin/mksh
-# $MirOS: src/scripts/genhtman.sh,v 1.16 2011/12/03 18:21:12 tg Exp $
+# $MirOS: src/share/misc/licence.template,v 1.28 2008/11/14 15:33:44 tg Rel $
+#-
+# Copyright © 2005, 2007, 2011, 2012, 2013
+#	Thorsten Glaser <tg@mirbsd.org>
+#
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un‐
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
+#
+# This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person’s immediate fault when using the work as intended.
 #-
 # Build the HTML format manual pages. This is part of the MirOS con-
 # tribuional and base system release process.
@@ -63,6 +81,54 @@ else
 	cd $BSDRELDIR/base/usr/share/doc/html; pax -rw * $hr/manDOCS/
 	cd $hr/manDOCS; find . -type f -a ! -name '*.htm' -a ! -name '*.html' \
 	    -a ! -name '*.jpg' -print0 | xargs -0 rm -f --
+	{
+		output_header sendmail.cf DOCS
+		print -n '<pre>'
+		set -A convs -- -e 's/&/\&amp;/g' \
+		    -e 's/</\&lt;/g' -e 's/>/\&gt;/g' \
+		    -e 's#\([0-9A-z][-.,0-9A-z:]*\)(\([1-9]\))#<a href="../man\2/\1.htm">&</a>#g'
+		set -A jumps
+		<$BSDSRCDIR/gnu/usr.sbin/sendmail/cf/README \
+		    tr '\n' '' | \
+		    sed 's/+-*+|//g' | \
+		    tr '' '\n' | \
+		    fgrep '' | \
+		    sed -e 's/^ *//g' -e 's/ *|*$//g' |&
+		njumps=0
+		while IFS= read -pr x; do
+			jumps[j = njumps++]=$x
+			i=-1
+			while (( ++i < j )); do
+				[[ -n ${jumps[i]} ]] || continue
+				[[ $x = *"${jumps[i]}"* ]] || continue
+				jumps[njumps++]=${jumps[i]}
+				unset jumps[i]
+			done
+		done
+		set -A jumps -- "${jumps[@]}"
+		njumps=${#jumps[*]}
+		i=-1
+		while (( ++i < njumps )); do
+			set -A convs+ -- -e "s!${jumps[i]}!${i}!g"
+		done
+		i=-1
+		while (( ++i < njumps )); do
+			set -A convs+ -- -e "s!${i}!<a href=\"#${jumps[i]//[ \?,-]/_}\">${jumps[i]}</a>!g"
+		done
+		set -A convs+ -- \
+		    -e 's#in op\.me)#in <a href="../manSMM/08.sendmailop.htm">op.me</a>)#' \
+		    -e 's#doc/op/op\.\*#doc/op/op.me#' \
+		    -e 's#in op\.me)#in doc/op/op.me)#' \
+		    -e 's#doc/op/op\.me#<a href="../manSMM/08.sendmailop.htm">&</a>#g' \
+		    -e 's#doc/op/op\..me,ps,txt.#<a href="../manSMM/08.sendmailop.htm">&</a>#g'
+		<$BSDSRCDIR/gnu/usr.sbin/sendmail/cf/README \
+		    sed "${convs[@]}" | \
+		    tr '\n' '' | \
+		    sed -e 's-+| <a href="#\([^<]*\)</a> |+--+| <span id="\1</span> |+-g' | \
+		    tr '' '\n'
+		print '</pre>'
+		output_footer
+	} >$hr/manDOCS/sendmail.cf.htm
 	output_htaccess >$hr/.htaccess
 	output_htaccess >$hr/manDOCS/.htaccess
 	{
