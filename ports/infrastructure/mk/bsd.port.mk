02ef435fb2a70596ab52eb65e670e6b9070e7949
tg <>
2013-05-18 15:19:05 +0000

    * infrastructure/mk/bsd.port.mk: never compile C++ code with   -fomit-frame-pointer as this seems to break exceptions   reported by: libtorrent/rtorrent wiki * devel/libsigc++: speed up build; be explicit about building   of shared or static libraries; regen distinfo; fix indentation * net/libtorrent, net/rtorrent: update to latest UNSTABLE ver-   sion (because the STABLE version has, according to upstream,   known bugs); enable IPv6; use "autogen" configuration style,   because "autoconf" doesn't work any more because upstream   wishes to use libtool 2.0-beta which has an interface incom-   patible to libtool 1.4, 1.5 and mirlibtool; speed up build;   be explicit about minimum curl version; add default session   dir example (commented out) to rtorrent.rc; apply fix for the   slow hashing problem (from upstream); indent properly

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.130 2006/09/17 20:17:33 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.131 2006/09/22 00:09:18 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -442,6 +442,8 @@ CFLAGS+=		${CDIAGFLAGS}
 CXXFLAGS+=		${CXXDIAGFLAGS}
 .  endif
 .endif
+# cf. http://libtorrent.rakshasa.no/wiki/LibTorrentKnownIssues#Badcodeproducedwith-fomit-frame-pointer
+CXXFLAGS+=		-fno-omit-frame-pointer
 LDFLAGS+=		-Wl,--library-after=${LOCALBASE}/lib ${LDSTATIC}
 
 NO_CXX?=		No	# inhibit use of C++ ports
