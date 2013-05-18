9af5b65d5e7dfefaddce0ffd0d8ad9510c0a1241
tg <>
2013-05-18 16:50:00 +0000

    unbreak ports which call pkg-config outside do-configure: too

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++ ports/devel/pkgconfig/pkgconfig.port.mk
@@ -1,7 +1,8 @@
-# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.4 2007/07/05 22:18:11 tg Exp $
+# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.5 2008/12/26 15:42:05 tg Exp $
 #
 # module file for ports that use pkgconfig
 
 BUILD_DEPENDS+=		:pkgconfig->=0.15.0-2:devel/pkgconfig
 PKG_CONFIG_PATH?=	/usr/libdata/pkgconfig:${LOCALBASE}/libdata/pkgconfig:${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig
 CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
+MAKE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
