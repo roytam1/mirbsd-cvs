c9f34350e6243fbc1d77ee8ef89d10f4facdf682
tg <>
2013-05-18 16:29:30 +0000

    new: NO_LIBTOOLISE_PLIST, to be used to prevent .la files from being processed on “mmake package”
    XXX TODO: also make this prevent “mmake plist” removing lib symlinks

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.206 2008/08/26 17:29:00 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.207 2008/08/26 18:38:13 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -613,7 +613,9 @@ _SED_SUBST+=		-e 's,$${FLAVOURS},${FLAVOUR_EXT},g' -e 's,$$\\,$$,g'
 # and append it to the PLIST substitution pipeline
 SED_PLIST+=		|${_SED_SUBST}
 
+.if !defined(NO_LIBTOOLISE_PLIST) || (${NO_LIBTOOLISE_PLIST:L} == "no")
 SED_PLIST+=		| (cd ${WRKINST}${PREFIX}; LOCALBASE=${LOCALBASE} perl -W ${PORTSDIR}/infrastructure/scripts/unlibtoolise || rm -f $@.tmp)
+.endif
 
 # find out the most appropriate PLIST source
 .if !defined(PLIST) \
