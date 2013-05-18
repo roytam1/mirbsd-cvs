082f0aef6c52f6f41a8553bee7394f7da9fdc8c0
cvs2git <admin@example.com>
2013-05-18 15:58:21 +0000

    This commit was manufactured by cvs2git to create branch 'tg-crypto- improvements'.

--- gcc/gcc/p/p-version.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically by the Makefile.
-   DO NOT CHANGE THIS FILE MANUALLY! */
-
-#define GPC_MAJOR "2"
-#define GPC_MINOR "1"
-#define GPC_VERSION_STRING "20060325"
-#define GPC_RELEASE_STRING GPC_VERSION_STRING
