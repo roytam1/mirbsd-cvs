ca61bbcbafc51b576f40ebdea2548a3ed4a3179a
tg <>
2013-05-18 16:33:46 +0000

    this will most certainly be mksh R36

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -100,9 +100,9 @@
 #define __SCCSID(x)	__IDSTRING(sccsid,x)
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.243 2008/10/20 19:29:25 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.244 2008/10/24 19:54:23 tg Exp $");
 #endif
-#define MKSH_VERSION "R35 2008/10/24"
+#define MKSH_VERSION "R36 2008/10/24"
 
 #ifndef MKSH_INCLUDES_ONLY
 
