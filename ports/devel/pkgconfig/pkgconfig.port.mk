3a9e650fd8d9b776c5775a5bb4854bef1037fabc
tg <>
2013-05-18 16:41:39 +0000

    • prefer /usr/libdata/pkgconfig over everything else • prefer ${LOCALBASE}/libdata/pkgconfig over ${LOCALBASE}/lib/pkgconfig,   with plans to move everything soonish • prefer -I${LOCALBASE}/include over -I${X11BASE}/include over   -I/usr/local/include and do it only once and only if different • search only in /usr/libdata/pkgconfig by default (nōn-MirPorts use)
    agreed bsiegert@ on the phone

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++ ports/devel/pkgconfig/pkgconfig.port.mk
@@ -1,7 +1,7 @@
-# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.3 2007/05/08 22:43:41 tg Exp $
+# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.4 2007/07/05 22:18:11 tg Exp $
 #
 # module file for ports that use pkgconfig
 
 BUILD_DEPENDS+=		:pkgconfig->=0.15.0-2:devel/pkgconfig
-PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig:/usr/libdata/pkgconfig
+PKG_CONFIG_PATH?=	/usr/libdata/pkgconfig:${LOCALBASE}/libdata/pkgconfig:${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig
 CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
