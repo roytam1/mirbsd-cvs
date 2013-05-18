38a301849be249df9a7a48a9924daef9c96242e9
tg <>
2013-05-18 14:45:00 +0000

    the next one... even easier

---
+++ src/lib/libc/i18n/wctob.c
@@ -0,0 +1,29 @@
+/* $MirOS: src/lib/libc/i18n/btowc.c,v 1.1 2005/09/22 21:39:21 tg Exp $ */
+
+/*-
+ * Copyright (c) 2005
+ *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
+ *
+ * Licensee is hereby permitted to deal in this work without restric-
+ * tion, including unlimited rights to use, publicly perform, modify,
+ * merge, distribute, sell, give away or sublicence, provided all co-
+ * pyright notices above, these terms and the disclaimer are retained
+ * in all redistributions or reproduced in accompanying documentation
+ * or other materials provided with binary redistributions.
+ *
+ * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
+ * any kind, expressed or implied, to the maximum extent permitted by
+ * applicable law, but with the warranty of being written without ma-
+ * licious intent or gross negligence; in no event shall licensor, an
+ * author or contributor be held liable for any damage, direct, indi-
+ * rect or other, however caused, arising in any way out of the usage
+ * of this work, even if advised of the possibility of such damage.
+ */
+
+#include <wchar.h>
+
+int
+wctob(wint_t c)
+{
+	return ((c <= (locale_is_utf8 ? 0x7E : 0xFF)) ? c : EOF);
+}
