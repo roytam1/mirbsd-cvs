5d695d914a1ede8d5cfbf52196a34aea361e22ab
tg <>
2013-05-18 17:03:50 +0000

    new function unbksl doing "backslash expansion" independent of c_print(); also make a separate subsection about it in the manpage

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -134,7 +134,7 @@
 #endif
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.339 2009/09/06 17:55:55 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.340 2009/09/07 17:24:49 tg Exp $");
 #endif
 #define MKSH_VERSION "R39 2009/09/07"
 
@@ -1587,6 +1587,7 @@ void set_current_wd(char *);
 char *strdup_(const char *, Area *);
 char *strndup_(const char *, size_t, Area *);
 #endif
+int unbksl(const char **);
 /* shf.c */
 struct shf *shf_open(const char *, int, int, int);
 struct shf *shf_fdopen(int, int, struct shf *);
