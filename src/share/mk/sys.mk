c3957e6fdf60572b395f82bdccf602f0a0e4dfaf
tg <>
2013-05-18 16:09:06 +0000

    bump

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.119 2007/08/01 22:50:11 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.120 2007/10/08 21:10:05 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -29,7 +29,7 @@ OSNAME!=	uname -s
 OSname=		${OSNAME:L}
 OStriplet?=	${MACHINE_ARCH}-ecce-${OSname}${OSrev}
 _MIRMAKE_EXE=	/usr/bin/make
-_MIRMAKE_VER=	20071008
+_MIRMAKE_VER=	20080309
 
 .SUFFIXES:	.out .a .ln .o .s .S .c .m .cc .cxx .cpp .y .l .i .h .sh .m4
 .LIBS:		.a
