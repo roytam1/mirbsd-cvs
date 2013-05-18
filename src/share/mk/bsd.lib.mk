17440386999cb4d1f2d176ef10f9d8c9f03fa079
tg <>
2013-05-18 15:47:16 +0000

    optional different source dir

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.54 2007/04/28 00:12:46 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.55 2007/05/17 17:21:44 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -311,8 +311,10 @@ ${.CURDIR}/tags: ${SRCS}
 .endif
 
 .if defined(HDRS) && !target(includes)
+HDRSRC?=${.CURDIR}
+
 includes:
-	@cd ${.CURDIR}; for i in ${HDRS}; do \
+	@cd ${HDRSRC}; for i in ${HDRS}; do \
 		if cmp -s "$$i" ${DESTDIR:Q}/usr/include/"$$i"; then \
 			print Header ${DESTDIR:Q}/usr/include/"$$i" \
 			    is up to date.; \
