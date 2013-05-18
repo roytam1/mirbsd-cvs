825f01f8842ded14f9258ab4d58660d602aa9986
tg <>
2013-05-18 15:25:20 +0000

    fastmerge

--- src/gnu/usr.bin/lynx/userdefs.h
+++ src/gnu/usr.bin/lynx/userdefs.h
@@ -1,4 +1,4 @@
-/* $MirOS: src/gnu/usr.bin/lynx/userdefs.h,v 1.10 2006/09/20 22:03:15 tg Exp $ */
+/* $MirOS: src/gnu/usr.bin/lynx/userdefs.h,v 1.11 2006/10/02 23:12:15 tg Exp $ */
 
 /*
  * Lynx - Hypertext navigation system
@@ -1149,11 +1149,11 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.6pre.5-MirOS"
+#define LYNX_VERSION "2.8.7dev.2-MirOS"
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Mon, 02 Oct 2006 12:56:53 -0700"
+#define LYNX_DATE "Mon, 06 Nov 2006 17:14:13 -0800"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 
