9c460427956be0195e874252f8c7f9627117f462
tg <>
2013-05-18 17:08:22 +0000

    Apply http://marc.info/?m=124508133203041 from Debian #541735
    Usage scenario: Root CA Cert is signed by algo Y but issues with algo X.
    Now algo Y is unknown, unsupported, or disabled due to security, patent,
    or FIPS compliance reasons. Since we have the cert locally anyway, there
    should be no need to verify its self-signature (IMHO, except as an addi-
    tional integrity check – but… be conservative/liberal…)

--- src/lib/libssl/src/crypto/x509/x509_vfy.c
+++ src/lib/libssl/src/crypto/x509/x509_vfy.c
@@ -70,6 +70,8 @@
 #include <openssl/x509v3.h>
 #include <openssl/objects.h>
 
+__RCSID("$MirOS$");
+
 static int null_callback(int ok,X509_STORE_CTX *e);
 static int check_issued(X509_STORE_CTX *ctx, X509 *x, X509 *issuer);
 static X509 *find_issuer(X509_STORE_CTX *ctx, STACK_OF(X509) *sk, X509 *x);
@@ -809,7 +811,11 @@ static int internal_verify(X509_STORE_CTX *ctx)
 	while (n >= 0)
 		{
 		ctx->error_depth=n;
-		if (!xs->valid)
+
+		/* Skip signature check for self signed certificates. It
+		 * doesn't add any security and just wastes time.
+		 */
+		if (!xs->valid && xs != xi)
 			{
 			if ((pkey=X509_get_pubkey(xi)) == NULL)
 				{
@@ -819,13 +825,6 @@ static int internal_verify(X509_STORE_CTX *ctx)
 				if (!ok) goto end;
 				}
 			else if (X509_verify(xs,pkey) <= 0)
-				/* XXX  For the final trusted self-signed cert,
-				 * this is a waste of time.  That check should
-				 * optional so that e.g. 'openssl x509' can be
-				 * used to detect invalid self-signatures, but
-				 * we don't verify again and again in SSL
-				 * handshakes and the like once the cert has
-				 * been declared trusted. */
 				{
 				ctx->error=X509_V_ERR_CERT_SIGNATURE_FAILURE;
 				ctx->current_cert=xs;
