4c94dad84974c19beec42cc437dcd4baea47fb11
tg <>
2013-05-18 15:40:27 +0000

    simplify

--- src/sys/lib/libkern/srandom.c
+++ src/sys/lib/libkern/srandom.c
@@ -1,47 +1,39 @@
-/* $MirOS: src/share/misc/licence.template,v 1.7 2006/04/09 22:08:49 tg Rel $ */
+/* $MirOS: src/share/misc/licence.template,v 1.20 2006/12/11 21:04:56 tg Rel $ */
 
 /*-
- * Copyright (c) 2006
+ * Copyright (c) 2007
  *	Thorsten Glaser <tg@mirbsd.de>
  *
- * Licensee is hereby permitted to deal in this work without restric-
- * tion, including unlimited rights to use, publicly perform, modify,
- * merge, distribute, sell, give away or sublicence, provided all co-
- * pyright notices above, these terms and the disclaimer are retained
- * in all redistributions or reproduced in accompanying documentation
- * or other materials provided with binary redistributions.
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un-
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
  *
- * Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
- * express, or implied, to the maximum extent permitted by applicable
- * law, without malicious intent or gross negligence; in no event may
- * licensor, an author or contributor be held liable for any indirect
- * or other damage, or direct damage except proven a consequence of a
- * direct error of said person and intended use of this work, loss or
- * other issues arising in any way out of its use, even if advised of
- * the possibility of such damage or existence of a nontrivial bug.
- *-
- * Reinitialise the 31-bit PRNG but retain a little state information
+ * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person's immediate fault when using the work as intended.
  */
 
-#include <sys/types.h>
 #include <lib/libkern/libkern.h>
 
 extern u_long _randseed;
 
 void
-srandom(u_long newseed)
+srandom(u_long val)
 {
-	u_long t = _randseed;
+	u_long tmp = _randseed;
 
-	_randseed ^= newseed;
-	while (_randseed & ~0x7FFFFFFF)
-		_randseed = (_randseed >> 31) + (_randseed & 0x7FFFFFFF);
-
-	t ^= (random() << 1) | (random() & 1);
-	_randseed = newseed & 0x7FFFFFFF;
-
-	while (t) {
-		random();
-		t >>= 1;
+	_randseed = val & 0x7FFFFFFF;
+	tmp += val >> 30;
+	while (tmp) {
+		if (tmp & 1)
+			random();
+		tmp >>= 1;
 	}
 }
