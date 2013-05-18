5d63c158dbe090619c490b4305b29d6b16dbee17
tg <>
2013-05-18 16:29:22 +0000

    no we don't make a file called whatif-depends

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.205 2008/06/15 14:52:00 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.206 2008/08/26 17:29:00 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -1588,7 +1588,6 @@ whatif-${_DEP}-depends: .PHONY
 			eval $$toset ${MAKE} whatif-depends; \
 		done; \
 	}
-	@${_MAKE_COOKIE} $@
 .    endfor
 .  endif
 .endfor
