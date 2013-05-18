6aee34e78e5e6b2fd30b40b856f90311c262c2f9
tg <>
2013-05-18 16:58:47 +0000

    another duplicate defn avoidance

--- src/bin/mksh/funcs.c
+++ src/bin/mksh/funcs.c
@@ -25,7 +25,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.115 2009/06/11 12:42:18 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.116 2009/07/25 20:26:32 tg Exp $");
 
 #if HAVE_KILLPG
 /*
@@ -38,7 +38,7 @@ __RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.115 2009/06/11 12:42:18 tg Exp $");
 #define mksh_kill	kill
 #endif
 
-#ifndef RLIM_INFINITY
+#if !defined(RLIM_INFINITY) && !defined(MKSH_NO_LIMITS)
 #define MKSH_NO_LIMITS
 #endif
 
