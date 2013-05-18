0d5c343efd926ebec72bdb2494572dbef1d07331
tg <>
2013-05-18 14:47:16 +0000

    make WRKDIST= optional for CVS_DISTMODS case

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.72 2005/12/16 11:14:50 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.73 2005/12/16 11:16:12 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -346,6 +346,9 @@ ERRORS+=		"No flavours for this port."
 
 SED_PLIST+=		| (cd ${WRKINST}${PREFIX}; LOCALBASE=${LOCALBASE} perl -W ${PORTSDIR}/infrastructure/scripts/unlibtoolise || rm -f $@.tmp)
 
+.if defined(CVS_DISTMODS) && !empty(CVS_DISTMODS)
+WRKDIST?=		${WRKDIR}/${CVS_DISTMODS}
+.endif
 .if defined(DIST_NAME) && defined(DIST_DATE)
 PKGNAME?=		${DIST_NAME}-${DIST_DATE}-0
 WRKDIST?=		${WRKDIR}/${DIST_NAME}
