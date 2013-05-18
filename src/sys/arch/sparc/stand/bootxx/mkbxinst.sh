0db412956e187c4256edffabaa752af59dc601fb
tg <>
2013-05-18 16:04:33 +0000

    a less shell-in-Makefile-embedded approach

--- src/sys/arch/sparc/stand/bootxx/mkbxinst.sh
+++ src/sys/arch/sparc/stand/bootxx/mkbxinst.sh
@@ -1,5 +1,66 @@
-#!/usr/bin/env mksh
-# $MirOS: src/sys/arch/sparc/stand/bootxx/bxinst.sh,v 1.9 2007/10/16 22:44:43 tg Exp $
+#!/bin/mksh
+# $MirOS: src/sys/arch/sparc/stand/bootxx/bxinst.sh,v 1.10 2007/10/20 20:05:55 tg Exp $
+#-
+# Copyright (c) 2007
+#	Thorsten Glaser <tg@mirbsd.de>
+#
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un-
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
+#
+# Advertising materials mentioning features or use of this work must
+# display the following acknowledgement:
+#	This product includes material provided by Thorsten Glaser.
+#
+# This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person's immediate fault when using the work as intended.
+#-
+# Create a self-installing bootxx for sparc.
+# Arguments: $1 = a.out (sparc OpenBoot) bootxx, linked
+# Output: shell script to stdout
+
+rcsid='$MirOS$'
+
+function die {
+	rv=$1; shift
+	print -u2 -- "$@"
+	exit $rv
+}
+
+[[ -s $1 ]] || die 1 Cannot read input file "'$1'"
+
+T=$(mktemp /tmp/pine.smime.XXXXXXXXXX) || die 1 Cannot create temporary file
+trap 'rm -f $T ; exit 0' 0
+trap 'rm -f $T ; exit 1' 1 2 3 5 13 15
+
+nm --target=a.out-sunos-big $1 |&
+while read -p adr typ sym; do
+	[[ $sym = @(_block_start|_start) ]] || continue
+	eval typeset -i10 sym$sym=0x\$adr
+done
+
+typeset -Uui10 ofs tblsz
+(( ofs = sym_block_start - sym_start + 32 ))
+strip -F a.out-sunos-big -s -o $T $1
+tblsz=$(dd if=$T bs=1 skip=$ofs count=4 2>/dev/null | \
+    hexdump -ve '"0x" 4/1 "%02X"')
+part1=$(dd if=$T bs=1 skip=8 count=$((ofs - 8)) 2>/dev/null | \
+    hexdump -ve '1/1 "\\0%o"')
+part2=$(dd if=$T bs=1 skip=$((ofs + tblsz * 4 + 8)) 2>/dev/null | \
+    hexdump -ve '1/1 "\\0%o"')
+
+print '#!/usr/bin/env mksh'
+print "# $rcsid"
+print "# \$miros:${rcsid#*:}"
+cat <<'EOF'
 #-
 # Copyright (c) 2007
 #	Thorsten Glaser <tg@mirbsd.de>
@@ -39,9 +100,11 @@ function out_int32 {
 	print -n "\\0${ba#8#}\\0${bb#8#}\\0${bc#8#}\\0${bd#8#}"
 }
 
-integer blktblsz=@@TBLSZ@@
+EOF
+print typeset -i blktblsz=$tblsz
+cat <<'EOF'
 set -A blktblent
-integer blktblnum=0 firstblock lastblock i=0 sscale=0
+typeset -i blktblnum=0 firstblock lastblock i=0 sscale=0
 
 while getopts ":S:" ch; do
 	case $ch {
@@ -80,7 +143,9 @@ done
 print -u2 .
 
 # Part 1
-print -n '\01\03\01\07\060\0200\0\07@@PARTONE@@'
+EOF
+print -r "print -n '\01\03\01\07\060\0200\0\07$part1'"
+cat <<'EOF'
 
 # The Block Table
 out_int32 $blktblnum
@@ -91,5 +156,7 @@ while (( i < blktblsz )); do
 done
 
 # Part 2
-print -n '@@PARTTWO@@'
+EOF
+print -r "print -n '$part2'"
+print exit 0
 exit 0
