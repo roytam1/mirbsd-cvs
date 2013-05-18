f53967c34ffb04ba1c3fbb87c127e7c86ceb7f08
tg <>
2013-05-18 17:32:24 +0000

    document why this is separate

--- src/kern/c/explicit_bzero.c
+++ src/kern/c/explicit_bzero.c
@@ -6,7 +6,7 @@
 
 #include <libckern.h>
 
-__RCSID("$MirOS$");
+__RCSID("$MirOS: src/kern/c/explicit_bzero.c,v 1.1 2011/07/17 17:21:02 tg Exp $");
 
 /*
  * explicit_bzero - don't let the compiler optimize away bzero
@@ -14,5 +14,13 @@ __RCSID("$MirOS$");
 void
 explicit_bzero(void *p, size_t n)
 {
+	/*
+	 * we forward to the "correct" bzero implementation,
+	 * instead of just making this a symbol alias in the
+	 * arch subdirectory's copy, because e.g. the kernel
+	 * on i386 (locore.S) has per-CPU optimised ones; in
+	 * userspace we want to keep the symbol in libmbfun,
+	 * to not clutter libc symbol space
+	 */
 	bzero(p, n);
 }
