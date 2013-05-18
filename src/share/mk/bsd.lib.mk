68852f1f223fd01cfb5285e123a22e46c9845e25
tg <>
2013-05-18 16:29:09 +0000

    preparing in /usr/src/include/../lib/libncurses AWK=/usr/bin/awk /bin/mksh /usr/src/lib/libncurses/src/include/MKkey_defs.sh  /usr/src/lib/libncurses/src/include/Caps >curses.keys cat /usr/src/lib/libncurses/src/include/curses.head curses.keys  /usr/src/lib/libncurses/src/include/curses.wide  /usr/src/lib/libncurses/src/include/curses.tail >curses.h /usr/bin/awk -f /usr/src/lib/libncurses/src/include/MKterm.h.awk /usr/src/lib/libncurses/src/include/Caps >term.h preparing in /usr/src/include/../lib/librpcsvc "/usr/share/mk/bsd.lib.mk", line 204: Malformed conditional (${SHLIB_VERSION} != "-") "/usr/share/mk/bsd.lib.mk", line 329: if-less endif "/usr/share/mk/bsd.lib.mk", line 329: Need an operator Fatal errors encountered -- cannot continue *** Error code 1
    Stop in /usr/src/include (line 50 of Makefile).
    *** Error code 1

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.73 2008/08/08 12:40:44 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.74 2008/08/08 12:42:49 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -201,7 +201,7 @@ lib${LIB}_pic.a:: ${SOBJS}
 .endif
 
 ${SHLIB_SONAME}: ${CRTI} ${CRTBEGIN} ${SOBJS} ${DPADD} ${CRTEND} ${CRTN}
-.if ${SHLIB_VERSION} != "-"
+.if defined(SHLIB_VERSION) && (${SHLIB_VERSION} != "-")
 	@print -r building shared ${LIB} library \(version ${SHLIB_VERSION}\)
 .else
 	@print -r building shared library ${SHLIB_SONAME}
