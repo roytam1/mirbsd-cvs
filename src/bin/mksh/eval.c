99f01d25b13f9b4302269eb2f09ecccac5447169
tg <>
2013-05-18 17:55:26 +0000

    as a more general solution to fd leak detection, just do CLOEXEC manually (this also allows us to see “real” leaks)

--- src/bin/mksh/eval.c
+++ src/bin/mksh/eval.c
@@ -1340,23 +1340,10 @@ comsub(Expand *xp, const char *cp, int fn MKSH_A_UNUSED)
 		unlink(tf->tffn);
 		afree(tf, ATEMP);
 	} else {
-#ifdef DEBUG_LEAKS
-#define ofd1 e->savefd[1]
-#else
-		int ofd1;
-#endif
-		int pv[2];
+		int ofd1, pv[2];
 
 		openpipe(pv);
 		shf = shf_fdopen(pv[0], SHF_RD, NULL);
-#ifdef DEBUG_LEAKS
-		if (!e->savefd) {
-			e->savefd = alloc2(NUFILE, sizeof(short), ATEMP);
-			memset(e->savefd, 0, NUFILE * sizeof(short));
-		}
-		/* hack to get it closed in child */
-		e->savefd[pv[0]] = pv[0];
-#endif
 		ofd1 = savefd(1);
 		if (pv[1] != 1) {
 			ksh_dup2(pv[1], 1, false);
