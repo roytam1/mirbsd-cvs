7906bbbb3de98c4c731e674472fd1949521daa5b
cvs2git <admin@example.com>
2013-05-18 16:44:22 +0000

    This commit was manufactured by cvs2git to create branch 'tg-new-cd9660'.

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++
@@ -1,7 +0,0 @@
-# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.4 2007/07/05 22:18:11 tg Exp $
-#
-# module file for ports that use pkgconfig
-
-BUILD_DEPENDS+=		:pkgconfig->=0.15.0-2:devel/pkgconfig
-PKG_CONFIG_PATH?=	/usr/libdata/pkgconfig:${LOCALBASE}/libdata/pkgconfig:${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig
-CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
