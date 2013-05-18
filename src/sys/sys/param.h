f26dc5c93771000a10fc77240b0c98a95c75b844
tg <>
2013-05-18 15:32:48 +0000

    fixing the integer overflow problem is worth me a bump

--- src/sys/sys/param.h
+++ src/sys/sys/param.h
@@ -1,4 +1,4 @@
-/**	$MirOS: src/sys/sys/param.h,v 1.54 2006/11/17 03:20:29 tg Exp $ */
+/**	$MirOS: src/sys/sys/param.h,v 1.55 2006/12/11 20:48:35 tg Exp $ */
 /*	$OpenBSD: param.h,v 1.54 2004/02/27 18:06:55 deraadt Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
@@ -48,7 +48,7 @@
 #define	BSD	199306		/* System version (year & month). */
 #define BSD4_3	1
 #define BSD4_4	1
-#define	MirBSD	0x09A9		/* minor 10-7F=prerelease; 80=release
+#define	MirBSD	0x09AA		/* minor 10-7F=prerelease; 80=release
 				 * minor 81-9F=stable; A0-FF=unlocked
 				 */
 #ifndef _LOCORE
