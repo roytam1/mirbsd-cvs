d8f5cc0c835a9055b85781ac6115f4507d82e0f7
tg <>
2013-05-18 17:03:24 +0000

    no -Wl,--no-undefined for LTMIRMAKE – unbreaks ports/devel/lrmi

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.81 2009/08/30 18:04:09 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.82 2009/08/30 22:06:40 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -83,7 +83,8 @@ SHLIB_FLAGS?=	-Wl,--image-base,$$((RANDOM % 0x1000 / 4 * 0x40000 + 0x40000000))
 .  else
 SHLIB_FLAGS?=	${PICFLAG}
 .  endif
-.  if (${SHLIB_TYPE:U} == "DLL") && (${RTLD_TYPE} != "dyld")
+.  if (${SHLIB_TYPE:U} == "DLL") && (${RTLD_TYPE} != "dyld") && \
+    (${LTMIRMAKE:L} != "yes")
 # GNU or BSD, DLL
 SHLIB_FLAGS+=	-Wl,--no-undefined
 .    if ${LIB} != "c"
