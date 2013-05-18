3b70c3a376cd67ce6fc54c802ddb9270f94c0fb4
tg <>
2013-05-18 16:40:44 +0000

    standard path to mksh is /usr/dbin/mksh on MirOS-current (sometimes I curse myself for giving in to that idea…)

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.141 2008/12/10 21:38:57 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.142 2008/12/10 21:46:27 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -29,7 +29,7 @@ OSNAME!=	uname -s
 OSname=		${OSNAME:L}
 OStriplet?=	${MACHINE_ARCH}-ecce-${OSname}${OSrev}
 _MIRMAKE_EXE=	/usr/bin/make
-_MIRMAKE_VER=	20081012
+_MIRMAKE_VER=	20081212
 
 .SUFFIXES:	.out .a .ln .o .lo .s .S .c .m .cc .C .cxx .cpp .F .f .y .l .i .h .sh .m4
 .LIBS:		.a
@@ -80,7 +80,7 @@ LINT?=		lint
 LINTFLAGS?=	-chapbx
 
 # MirOS make can use MirBSD ksh extensions per definitionem
-SHELL=		/bin/mksh
+SHELL=		/usr/dbin/mksh
 MAKE?=		make
 
 YACC?=		yacc
