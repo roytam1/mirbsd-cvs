4d513280308ccc79cff6a4570847d607f9ee4a76
tg <>
2013-05-18 15:28:23 +0000

    * untested update to 9.10 * bundle "Minimalist Native" theme (XXX can it be selected out of the box?) * bundle .lu third-party language pack (the only language pack   available for 9.10-Linux; the 9.10-Windows official packs won't   work with the Linux binary; more to come...)
    remaining issues:
    * /etc/opera6rc can't be used, thus pkg/MESSAGE stays :(
    * no Java support (someone should make 1.3-linux work)

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,8 +1,11 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.6 2006/06/23 14:27:15 tg Exp $
+# $MirOS: ports/www/opera/files/opera.sh,v 1.7 2006/06/25 06:26:52 tg Exp $
 
-export OPERA_DIR=@PREFIX@/share/opera
-export LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
-export LD_PRELOAD=/dbl/libnss_compat.so.2:/dbl/libnss_dns.so.2:/dbl/libnss_files.so.2:${LD_PRELOAD}
+export OPERA_SCRIPT_PATH=$(readlink -nf $0)
+export OPERA_BINARYDIR=@PREFIX@/libexec/ OPERA_DIR=@PREFIX@/share/opera
+LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
+LD_PRELOAD=/dbl/libnss_compat.so.2:/dbl/libnss_dns.so.2:/dbl/libnss_files.so.2:${LD_PRELOAD}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH%%:}
+export OPERA_LD_PRELOAD=${LD_PRELOAD%%:} LD_PRELOAD=${LD_PRELOAD%%:}
 
 exec @PREFIX@/libexec/opera "$@"
