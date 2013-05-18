5114cbdee7cbe16d362fbf15d8a22c0d0da2452a
tg <>
2013-05-18 17:27:34 +0000

    no // style comments in my source code!

--- src/bin/mksh/tree.c
+++ src/bin/mksh/tree.c
@@ -22,7 +22,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/tree.c,v 1.40 2011/03/13 01:20:24 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/tree.c,v 1.41 2011/03/17 21:58:40 tg Exp $");
 
 #define INDENT	8
 
@@ -931,7 +931,7 @@ dumptree(struct shf *shf, struct op *t)
  dumpleftmidrightandout:
 		shf_putc('\n', shf);
 		dumptree(shf, t->left);
-// middumprightandout:
+/* middumprightandout: (unused) */
 		shf_fprintf(shf, "/%s:", name);
  dumprightandout:
 		t = t->right;
