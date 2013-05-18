581bfef2b043d67e23920107d7854fe031a1d2d0
tg <>
2013-05-18 16:14:46 +0000

    spend expat a fake-pkgview

--- ports/textproc/expat/expat.port.mk
+++ ports/textproc/expat/expat.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/textproc/expat/expat.port.mk,v 1.1 2006/05/29 21:23:29 bsiegert Exp $
+# $MirOS: ports/textproc/expat/expat.port.mk,v 1.2 2006/05/29 21:32:24 tg Exp $
 # $OpenBSD: iconv.port.mk,v 1.4 2001/11/27 17:44:04 brad Exp $
 
 # This is equivalent to USE_MOTIF.
@@ -24,13 +24,14 @@ USE_EXPAT=	port
 .endif
 
 .if ${USE_EXPAT:L} == "port"
-LIB_DEPENDS+=	expat::textproc/expat
-EXPAT_PREFIX=	${LOCALBASE}
+LIB_DEPENDS+=	expat:expat->=2.0.0-1:textproc/expat
+EXPAT_PREFIX=	${LOCALBASE}/pkgview/expat
+LDFLAGS+=	-Wl,-rpath,${EXPAT_PREFIX:Q}/lib -L${EXPAT_PREFIX:Q}/lib
 .elif ${USE_EXPAT:L} == "base"
 EXPAT_PREFIX=	/usr
 .elif ${USE_EXPAT:L} == "x11"
 EXPAT_PREFIX=	${X11BASE}
-USE_X11=	Yes
+USE_X11=	Yes		# XXX too late in a MODULES file
 .else
 .  error USE_EXPAT=${USE_EXPAT:L} invalid
 .endif
