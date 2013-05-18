253a2c89d1e0762f0163054bba3c9fbafc754e6d
cvs2git <admin@example.com>
2013-05-18 17:09:28 +0000

    This commit was manufactured by cvs2git to create tag 'kwalletcli-2_00'.

--- src/gnu/usr.bin/binutils/ld/emulparams/elf_mbsd.sh
+++
@@ -1,7 +0,0 @@
-# $MirOS$
-
-. ${srcdir}/emulparams/elf_obsd.sh
-
-# Not. That's the BFD output format. MirOS BSD uses pretty
-# standard ELF, so no need to do that.
-#OUTPUT_FORMAT="${OUTPUT_FORMAT}-mirbsd"
