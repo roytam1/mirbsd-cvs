e14518ed67ea812daf9cada19e75b9c53ec08026
tg <>
2013-05-18 15:02:23 +0000

    header files are interface definitions and as such not copyrightable

--- contrib/code/libhaible/wchar.h
+++ contrib/code/libhaible/wchar.h
@@ -1,29 +1,4 @@
-/* $MirOS: contrib/code/libhaible/wchar.h,v 1.14 2006/05/30 23:25:39 tg Exp $ */
-
-/*-
- * Copyright (c) 2005, 2006
- *	Thorsten Glaser <tg@mirbsd.de>
- *
- * Licensee is hereby permitted to deal in this work without restric-
- * tion, including unlimited rights to use, publicly perform, modify,
- * merge, distribute, sell, give away or sublicence, provided all co-
- * pyright notices above, these terms and the disclaimer are retained
- * in all redistributions or reproduced in accompanying documentation
- * or other materials provided with binary redistributions.
- *
- * All advertising materials mentioning features or use of this soft-
- * ware must display the following acknowledgement:
- *	This product includes material provided by Thorsten Glaser.
- *
- * Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
- * express, or implied, to the maximum extent permitted by applicable
- * law, without malicious intent or gross negligence; in no event may
- * licensor, an author or contributor be held liable for any indirect
- * or other damage, or direct damage except proven a consequence of a
- * direct error of said person and intended use of this work, loss or
- * other issues arising in any way out of its use, even if advised of
- * the possibility of such damage or existence of a nontrivial bug.
- */
+/* $MirOS: contrib/code/libhaible/wchar.h,v 1.15 2006/05/31 00:56:12 tg Exp $ */
 
 #ifndef	_WCHAR_H_
 #define	_WCHAR_H_
