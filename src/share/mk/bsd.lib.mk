845b3aea2b1ea9800411772e49401f156cb6ef95
tg <>
2013-05-18 16:34:05 +0000

    fix dependency information for the LTMIRMAKE case (shlibs)
    XXX bsd.prog.mk does not have similar issues as we do not
    XXX use libfool to build .lo files for executables (now)

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.75 2008/08/25 19:00:43 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.76 2008/10/05 20:14:30 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -292,7 +292,11 @@ cleandir: _SUBDIRUSE clean
 
 .if defined(SRCS)
 afterdepend: .depend
+.  if ${LTMIRMAKE:L} == "yes"
+	@print ',g/^\([^\.]*\).o[ ]*:/s//\1.o \1.lo:/\nwq' | ed -s .depend
+.  else
 	@print ',g/^\([^\.]*\).o[ ]*:/s//\1.o \1.so:/\nwq' | ed -s .depend
+.  endif
 .endif
 
 .if !target(install)
