c0d2db4bd03290b0757e0572354887fee952e13f
cvs2git <admin@example.com>
2013-05-18 16:29:05 +0000

    This commit was manufactured by cvs2git to create tag 'joe-3_1jupp10'.

--- ports/devel/libreadline/libreadline.port.mk
+++
@@ -1,6 +0,0 @@
-# $MirOS$
-
-READLINE_PREFIX=	${LOCALBASE}/pkgview/libreadline
-CPPFLAGS+=		-I${READLINE_PREFIX:Q}/include
-LDFLAGS+=		-Wl,-rpath,${READLINE_PREFIX:Q}/lib \
-			-L${READLINE_PREFIX:Q}/lib
