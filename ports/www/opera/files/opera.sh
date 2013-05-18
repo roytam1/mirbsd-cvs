c4966588c49526fdd4a2d23a321d5c381f26e69d
tg <>
2013-05-18 15:44:46 +0000

    update opera to 9.20, still can't get plugins to work, have to disable new XSHM

--- ports/www/opera/files/opera.sh
+++ ports/www/opera/files/opera.sh
@@ -1,5 +1,5 @@
 #!@SHELL@
-# $MirOS: ports/www/opera/files/opera.sh,v 1.8 2006/12/20 21:39:32 tg Exp $
+# $MirOS: ports/www/opera/files/opera.sh,v 1.9 2006/12/21 13:41:34 tg Exp $
 
 if ! fgrep "Synchronous DNS Lookup=1" $HOME/.opera/opera6.ini \
     >/dev/null 2>&1; then
@@ -8,7 +8,7 @@ if ! fgrep "Synchronous DNS Lookup=1" $HOME/.opera/opera6.ini \
 	sleep 3
 fi
 
-export OPERA_SCRIPT_PATH=$(readlink -nf $0)
+export OPERA_SCRIPT_PATH=$(readlink -nf $0) OPERA_NUM_XSHM=0
 export OPERA_BINARYDIR=@PREFIX@/libexec/ OPERA_DIR=@PREFIX@/share/opera
 LD_LIBRARY_PATH=/dbl:@PREFIX@/libexec:${LD_LIBRARY_PATH}
 LD_PRELOAD=/dbl/libnss_compat.so.2:/dbl/libnss_dns.so.2:/dbl/libnss_files.so.2:${LD_PRELOAD}
