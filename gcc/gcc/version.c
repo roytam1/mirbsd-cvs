5071c12028c5d941ef3fe9eae8729fa4a27f53aa
tg <>
2013-05-18 15:22:38 +0000

    seems as if the <ctype.h> change hit us quite hard; we can't cover as netbsd any more, and in addition to that, the IEEE_1003.1-2001 locale is upstream broken so we can't use it even if we probably fulfil it already, which means two ABI changes. bump OS patchlevel here too, but don't bump in base yet, I'll do that once HEAD is sort of more stable again…

--- gcc/gcc/version.c
+++ gcc/gcc/version.c
@@ -1,4 +1,4 @@
-/* $MirOS: gcc/gcc/version.c,v 1.35 2006/09/24 17:48:35 tg Exp $ */
+/* $MirOS: gcc/gcc/version.c,v 1.36 2006/09/29 22:02:44 tg Exp $ */
 
 #include "version.h"
 
@@ -7,7 +7,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.4.6 (propolice; gpc; MirOS 09A1)";
+const char version_string[] = "3.4.6 (propolice; gpc; MirOS 09A6)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
