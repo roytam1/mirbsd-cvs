de068a21103976caa3b98f8e0e6f9623286646c6
tg <>
2013-05-18 16:01:21 +0000

    • fix memory leaks found by coverity   from netbsd via oksh   we had the NULL pointer deref already fixed • avoid a bogus not-setting the return value of edit.c:x_file_glob()   introduced by the above change in oksh • escape ? as well (but not ] because that’s wrong)   reminded by cbiere@netbsd via oksh • Unsetting a non-existent variable is not an error. See   http://www.opengroup.org/onlinepubs/009695399/utilities/unset.html   report from Arkadiusz Miskiewicz; fixed based on   http://cvs.pld-linux.org diff via oksh but modified slightly • Be more smart waiting for input for non-interactive scripts.  Fix   based on a diff from debian:  see their bug#296446 (via oksh)   modified slightly   this also fixes cnuke@’s “mksh busy loop” problem, for which I never   received a bug report, but the Debian bug page contains a set of two   scripts to reproduce this before (and no longer after) this commit • some KNF • bump version

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -8,8 +8,8 @@
 /*	$OpenBSD: c_test.h,v 1.4 2004/12/20 11:34:26 otto Exp $	*/
 /*	$OpenBSD: tty.h,v 1.5 2004/12/20 11:34:26 otto Exp $	*/
 
-#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.173 2007/08/19 23:12:22 tg Exp $"
-#define MKSH_VERSION "R31 2007/08/19"
+#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.174 2007/08/20 14:06:10 tg Exp $"
+#define MKSH_VERSION "R31 2007/09/09"
 
 #if HAVE_SYS_PARAM_H
 #include <sys/param.h>
