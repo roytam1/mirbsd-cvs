8d955ba16b1adcb5355504dacaffa9af26018ca1
tg <>
2013-05-18 15:44:34 +0000

    merge sendmail 8.14.1 via obsd

--- src/gnu/usr.sbin/sendmail/sendmail/map.c
+++ src/gnu/usr.sbin/sendmail/sendmail/map.c
@@ -14,8 +14,8 @@
 
 #include <sendmail.h>
 
-SM_RCSID("$MirOS: src/gnu/usr.sbin/sendmail/sendmail/map.c,v 1.2 2006/10/28 21:13:47 tg Exp $")
-SM_RCSID("@(#)$Sendmail: map.c,v 8.693 2006/12/19 00:58:56 ca Exp $")
+SM_RCSID("$MirOS: src/gnu/usr.sbin/sendmail/sendmail/map.c,v 1.3 2007/02/05 17:13:45 tg Exp $")
+SM_RCSID("@(#)$Sendmail: map.c,v 8.696 2007/04/03 21:33:14 ca Exp $")
 
 #if LDAPMAP
 # include <sm/ldap.h>
@@ -4706,7 +4706,7 @@ ldapmap_set_defaults(spec)
 /*
 **  Support for the CCSO Nameserver (ph/qi).
 **  This code is intended to replace the so-called "ph mailer".
-**  Contributed by Mark D. Roth <roth@uiuc.edu>.  Contact him for support.
+**  Contributed by Mark D. Roth.  Contact him for support.
 */
 
 /* what version of the ph map code we're running */
