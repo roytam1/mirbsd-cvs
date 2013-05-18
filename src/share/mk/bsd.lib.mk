924fa0ce1c629ca7d5c3351851f1879e3b6bf7c2
tg <>
2013-05-18 17:03:29 +0000

    also do not add --no-undefined (-z defs) if BSD_LIB_MK__nodefs is defined, for cases where we *must* manually override this

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.82 2009/08/30 22:06:40 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.83 2009/09/12 09:47:34 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -84,7 +84,7 @@ SHLIB_FLAGS?=	-Wl,--image-base,$$((RANDOM % 0x1000 / 4 * 0x40000 + 0x40000000))
 SHLIB_FLAGS?=	${PICFLAG}
 .  endif
 .  if (${SHLIB_TYPE:U} == "DLL") && (${RTLD_TYPE} != "dyld") && \
-    (${LTMIRMAKE:L} != "yes")
+    (${LTMIRMAKE:L} != "yes") && !defined(BSD_LIB_MK__nodefs)
 # GNU or BSD, DLL
 SHLIB_FLAGS+=	-Wl,--no-undefined
 .    if ${LIB} != "c"
