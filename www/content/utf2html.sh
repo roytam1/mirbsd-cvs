e1686adf1232568752f10fc369e68e23ab9b498d
tg <>
2013-05-18 15:27:18 +0000

    now that we have iconv(1) in /usr/bin, use it

--- www/content/utf2html.sh
+++ www/content/utf2html.sh
@@ -1,5 +1,5 @@
 #!/bin/mksh
-# $MirOS: src/share/misc/licence.template,v 1.6 2006/01/24 22:24:02 tg Rel $
+# $MirOS: www/content/utf2html.sh,v 1.1 2006/03/23 23:30:34 tg Exp $
 #-
 # Copyright (c) 2006
 #	Thorsten Glaser <tg@mirbsd.de>
@@ -7,27 +7,27 @@
 # Licensee is hereby permitted to deal in this work without restric-
 # tion, including unlimited rights to use, publicly perform, modify,
 # merge, distribute, sell, give away or sublicence, provided all co-
-# pyright notices above, these terms and the disclaimer are retained
-# in all redistributions or reproduced in accompanying documentation
-# or other materials provided with binary redistributions.
+# pyright notices above, these terms and below disclaimer are retai-
+# ned in all redistributions, or reproduced in accompanying documen-
+# tation or other materials provided with binary redistributions.
 #
-# All advertising materials mentioning features or use of this soft-
-# ware must display the following acknowledgement:
+# Advertising materials mentioning features or use of this work must
+# display the following acknowledgement:
 #	This product includes material provided by Thorsten Glaser.
 #
 # Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
 # express, or implied, to the maximum extent permitted by applicable
 # law, without malicious intent or gross negligence; in no event may
-# licensor, an author or contributor be held liable for any indirect
-# or other damage, or direct damage except proven a consequence of a
-# direct error of said person and intended use of this work, loss or
-# other issues arising in any way out of its use, even if advised of
-# the possibility of such damage or existence of a nontrivial bug.
+# a licensor, author or contributor be held liable for any indirect,
+# direct (unless proven consequence of a direct fault of said person
+# and intended use of this work), other damage, loss, or other issu-
+# es, arising in any way out of dealing with the work, even if advi-
+# sed of the possibility of such damage or existence of a defect.
 #-
 # bsiegert@ helper
 
 cat "$@" \
-    | ${ICONV:-myiconv} -f utf-8 -t ucs-2le \
+    | iconv -f utf-8 -t ucs-2le \
     | hexdump -ve '/2 "%04x" "\n"' \
     | while read x; do
 	let i=16#$x
