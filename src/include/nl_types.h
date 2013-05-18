6f3d560983d76e5515c3690d8340edc1e97cb33d
tg <>
2013-05-18 15:02:46 +0000

    add format_arg attribute to catgets

--- src/include/nl_types.h
+++ src/include/nl_types.h
@@ -1,4 +1,4 @@
-/*	$MirOS: src/include/nl_types.h,v 1.3 2006/05/30 20:11:59 tg Exp $	*/
+/*	$MirOS: src/include/nl_types.h,v 1.4 2006/05/30 20:28:15 tg Exp $	*/
 /*	$OpenBSD: nl_types.h,v 1.7 2002/02/16 21:27:17 millert Exp $	*/
 /*	$NetBSD: nl_types.h,v 1.6 1996/05/13 23:11:15 jtc Exp $	*/
 
@@ -51,7 +51,8 @@ typedef int nl_item;
 
 __BEGIN_DECLS
 extern nl_catd 	catopen(const char *, int);
-extern char    *catgets(nl_catd, int, int, const char *);
+extern char    *catgets(nl_catd, int, int, const char *)
+    __attribute__((format_arg(3)));
 extern int	catclose(nl_catd);
 __END_DECLS
 
