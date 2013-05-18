21e0747383df0c9c39270638f7900fe292dd3857
tg <>
2013-05-18 17:15:39 +0000

    switch to 2-clause licence; from NetBSD®

--- src/lib/libutil/fparseln.c
+++ src/lib/libutil/fparseln.c
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Christos Zoulas.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -43,7 +38,7 @@
 #include "util.h"
 #endif
 
-__RCSID("$MirOS$");
+__RCSID("$MirOS: src/lib/libutil/fparseln.c,v 1.2 2009/07/23 19:32:21 tg Exp $");
 
 static int isescaped(const char *, const char *, int);
 
