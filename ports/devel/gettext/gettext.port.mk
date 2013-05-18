2fd8de1fc571392c36f2d9ded5abdba22e51c20d
tg <>
2013-05-18 15:27:51 +0000

    better workaround for the issue that lib dependencies' middle part is not correctly checked pre-build: add them to BUILD_DEPENDS if they have a middle part
    XXX what about them being RUN_DEPENDS? especially on not shared
    XXX arches, where this isn't done automatically... probably, if
    XXX the library package (.cgz) contains additional stuff, the
    XXX porter who devises the dependency should add an explicit
    XXX run-time dependency

--- ports/devel/gettext/gettext.port.mk
+++ ports/devel/gettext/gettext.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/devel/gettext/gettext.port.mk,v 1.2 2006/02/01 17:48:29 tg Exp $
+# $MirOS: ports/devel/gettext/gettext.port.mk,v 1.3 2006/11/22 18:08:20 tg Exp $
 # $OpenBSD: gettext.port.mk,v 1.8 2001/11/27 17:44:04 brad Exp $
 
 MODULES+=	converters/libiconv
@@ -6,5 +6,4 @@ MODULES+=	converters/libiconv
 # The RUN_DEPENDS entry is to ensure gettext is installed. This is
 # necessary so that we have locale.alias installed on static archs.
 # Typically installed in PREFIX/share/locale.
-LIB_DEPENDS+=	intl::devel/gettext
-B_R_DEPENDS+=	:gettext->=0.10.38:devel/gettext
+LIB_DEPENDS+=	intl:gettext->=0.10.38:devel/gettext
