689ba1cc8f4a987bd84f0ecfef96d627527f08fb
tg <>
2013-05-18 15:01:26 +0000

    * #define the respective highest allowed character for the   'C' and 'UTF-8' locales - the 'C' one is 0xFF at the moment   but will go down to 0x7F later, once we switch to UTF-8 * include missing <sys/cdefs.h> like with libhaible

--- src/lib/libc/include/mir18n.h
+++ src/lib/libc/include/mir18n.h
@@ -1,7 +1,16 @@
-/* $MirOS$ */
+/* $MirOS: src/lib/libc/include/mir18n.h,v 1.1 2006/05/21 12:12:30 tg Exp $ */
 
 #ifndef _LIBC_MIR18N_H
 #define _LIBC_MIR18N_H
+
+/* highest allowed character in 'C' locale */
+#define MIR18N_SB_MAX	0xFF
+/* highest allowed character in 'UTF-8' locale */
+#define MIR18N_MB_MAX	0xFFFD
+
+#ifndef __BEGIN_DECLS
+#include <sys/cdefs.h>
+#endif
 
 #ifndef __bool_true_false_are_defined
 #include <stdbool.h>
