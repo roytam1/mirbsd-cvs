2bf8fff4b7fbdc2afd06f03bbb7f6418945f6b7f
tg <>
2013-05-18 17:53:51 +0000

    after ^KR now can jump to end of insertion with ^QP

--- contrib/code/jupp/ublock.c
+++ contrib/code/jupp/ublock.c
@@ -29,6 +29,7 @@
 #include "uedit.h"
 #include "utils.h"
 #include "vs.h"
+#include "poshist.h"
 #include "ushell.h"
 #include "utf8.h"
 #include "charmap.h"
@@ -917,8 +918,15 @@ int doinsf(BW *bw, unsigned char *s, void *object, int *notify)
 		if (error) {
 			msgnw(bw->parent, msgs[-error]), brm(tmp);
 			ret = -1;
-		} else
+		} else {
+			P *pafter;
+
+			pafter = pdup(bw->cursor);
+			pgetc(pafter);
 			binsb(bw->cursor, tmp);
+			prgetc(pafter);
+			aftermove(bw->parent, pafter);
+		}
 		vsrm(s);
 		bw->cursor->xcol = piscol(bw->cursor);
 		return ret;
