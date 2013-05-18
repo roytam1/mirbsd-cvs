1f4b1c50b0795438c8fba5c75fdc6e7583e2a635
tg <>
2013-05-18 17:49:02 +0000

    * for the "foo.sh -> foo" implicit rule, chmod +x $@ in the object dir * bump

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.162 2011/07/17 22:34:27 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.163 2012/01/14 22:28:43 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -21,7 +21,7 @@ OStype=		MirBSD
 # Sync these with <sys/param.h>
 unix=		We run ${OStype}.
 OSrev=		10		# MirOS version (major)
-OSrpl=		176		# MirOS patchlevel
+OSrpl=		177		# MirOS patchlevel
 OScompat=	3.5		# OpenBSD compatibility revision
 .if !defined(OSNAME) || empty(OSNAME)
 OSNAME!=	uname -s
@@ -237,6 +237,7 @@ CTAGS?=		/usr/bin/ctags
 .sh:
 	rm -f $@
 	cp ${.IMPSRC} $@
+	chmod +x $@
 
 # Debugging output
 .if defined(___DISPLAY_MAKEVARS)
--- src/sys/sys/param.h
+++ src/sys/sys/param.h
@@ -1,4 +1,4 @@
-/**	$MirOS: src/sys/sys/param.h,v 1.92 2011/07/17 22:34:27 tg Exp $ */
+/**	$MirOS: src/sys/sys/param.h,v 1.93 2012/01/14 22:28:44 tg Exp $ */
 /*	$OpenBSD: param.h,v 1.54 2004/02/27 18:06:55 deraadt Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
@@ -48,7 +48,7 @@
 #define	BSD	199306		/* System version (year & month). */
 #define BSD4_3	1
 #define BSD4_4	1
-#define	MirBSD	0x0AB0		/* minor 10-7F=prerelease; 80=release
+#define	MirBSD	0x0AB1		/* minor 10-7F=prerelease; 80=release
 				 * minor 81-9F=stable; A0-FF=unlocked
 				 */
 #ifndef _LOCORE
