a37bb15dac62be2f64ac67d63fd5924969b3de85
tg <>
2013-05-18 15:22:52 +0000

    wrap this entire file in between .if !exists(/usr/lib/libpng.a)

--- ports/graphics/png/png.port.mk
+++ ports/graphics/png/png.port.mk
@@ -1,4 +1,6 @@
-# $MirOS: ports/graphics/png/png.port.mk,v 1.2 2006/03/06 21:58:36 bsiegert Exp $
+# $MirOS: ports/graphics/png/png.port.mk,v 1.3 2006/06/25 01:56:00 tg Exp $
 
+.if !exists(/usr/lib/libpng.a)
 LIB_DEPENDS+=		png::graphics/png
 CPPFLAGS+=		-I${LOCALBASE}/include/libpng
+.endif
