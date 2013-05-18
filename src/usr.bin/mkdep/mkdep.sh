b09a7272dceb6a1d887ed0f843a77969cbf634d7
tg <>
2013-05-18 15:51:18 +0000

    fix for the CC=compiler+arg case

--- src/usr.bin/mkdep/mkdep.sh
+++ src/usr.bin/mkdep/mkdep.sh
@@ -1,28 +1,27 @@
 #!/bin/mksh
-# $MirOS: src/usr.bin/mkdep/mkdep.sh,v 1.5 2005/12/17 05:46:28 tg Exp $
+# $MirOS: src/share/misc/licence.template,v 1.20 2006/12/11 21:04:56 tg Rel $
 #-
-# Copyright (c) 2004, 2005
-#	Thorsten "mirabile" Glaser <tg@66h.42h.de>
+# Copyright (c) 2004, 2005, 2007
+#	Thorsten Glaser <tg@mirbsd.de>
 #
-# Licensee is hereby permitted to deal in this work without restric-
-# tion, including unlimited rights to use, publicly perform, modify,
-# merge, distribute, sell, give away or sublicence, provided all co-
-# pyright notices above, these terms and the disclaimer are retained
-# in all redistributions or reproduced in accompanying documentation
-# or other materials provided with binary redistributions.
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un-
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
 #
 # Advertising materials mentioning features or use of this work must
 # display the following acknowledgement:
 #	This product includes material provided by Thorsten Glaser.
 #
-# Licensor offers the work "AS IS" and WITHOUT WARRANTY of any kind,
-# express, or implied, to the maximum extent permitted by applicable
-# law, without malicious intent or gross negligence; in no event may
-# licensor, an author or contributor be held liable for any indirect
-# or other damage, or direct damage except proven a consequence of a
-# direct error of said person and intended use of this work, loss or
-# other issues arising in any way out of its use, even if advised of
-# the possibility of such damage or existence of a defect.
+# This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person's immediate fault when using the work as intended.
 #-
 # This is loosely based upon material provided by: the University of
 # California, Berkeley, and its contributors.
@@ -35,8 +34,10 @@ function die
 }
 
 # select a suitable C compiler
-CC=$(whence -p ${CC:-cc} 2>/dev/null) || CC=$(whence -p cc)
-[[ -x $CC ]] || die no compiler found.
+cc=${CC:-cc}
+[[ -x $cc || -x ${cc%% *} ]] || cc=$(whence -p $cc) || cc=$(whence -p cc)
+[[ -x $cc || -x ${cc%% *} ]] || die no compiler found.
+CC=$cc; export CC
 
 # default values
 df=.depend
