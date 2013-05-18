1aea7cbd059e36fb806f3ae9ec422d455a66783e
cvs2git <admin@example.com>
2013-05-18 15:28:58 +0000

    This commit was manufactured by cvs2git to create branch 'bsiegert- glib2-2_12'.

--- src/gnu/usr.bin/binutils/ld/emulparams/elf_mbsd.sh
+++
@@ -1,9 +0,0 @@
-# $MirOS: src/gnu/usr.bin/binutils/ld/emulparams/elf_mbsd.sh,v 1.1.7.1 2005/03/06 16:58:49 tg Exp $
-
-. ${srcdir}/emulparams/elf_obsd.sh
-
-# Not. That's the BFD output format. MirOS BSD uses pretty
-# standard ELF, so no need to do that.
-#OUTPUT_FORMAT="${OUTPUT_FORMAT}-mirbsd"
-
-INITIAL_NOTE_SECTIONS='.note.miros.ident'
