24cf75464187577b4ccc455611b3a871706d990d
tg <>
2013-05-18 15:54:40 +0000

    move /usr/share/pkgconfig/ -> /usr/libdata/pkgconfig/ by suggestion of bsiegert@

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++ ports/devel/pkgconfig/pkgconfig.port.mk
@@ -1,7 +1,7 @@
-# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.2 2007/05/08 22:38:58 tg Exp $
+# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.3 2007/05/08 22:43:41 tg Exp $
 #
 # module file for ports that use pkgconfig
 
 BUILD_DEPENDS+=		:pkgconfig->=0.15.0-2:devel/pkgconfig
-PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig:/usr/share/pkgconfig
+PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig:/usr/libdata/pkgconfig
 CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
