550217aecdbe7df291577be7766fb8efcc17a148
cvs2git <admin@example.com>
2013-05-18 15:26:39 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.33.2'.

--- src/distrib/common/runlist.sh
+++
@@ -1,8 +0,0 @@
-# $MirOS: src/distrib/miniroot/runlist.sh,v 1.3 2006/07/24 00:24:01 tg Exp $
-
-SHELLCMD="mksh -e"
-if [[ $1 = -d ]]; then
-	SHELLCMD=cat
-	shift
-fi
-cat "$@" | awk -f $(dirname $0)/list2sh.awk | ${SHELLCMD}
--- src/etc/etc.i386/make.cfg.md
+++
@@ -1,5 +0,0 @@
-# $MirOS: src/etc/etc.i386/make.cfg.md,v 1.3 2005/10/21 20:00:12 tg Exp $
-#
-# machdep part of ${MAKE}.cfg for i386
-
-_DEFCOPTS:=	-march=i486 -mpush-args -mpreferred-stack-boundary=2
