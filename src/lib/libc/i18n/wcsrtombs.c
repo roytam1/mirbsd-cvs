b3af7f1b0bcef6e3002df20c4a5319e75f90d5a5
tg <>
2013-05-18 15:03:08 +0000

    * rewrite from scratch (different algo, found in the restroom ;)   to fix regression test errors, is now fully restarting, properly * heavily optimise (even shrinks code size)

--- src/lib/libc/i18n/wcsrtombs.c
+++ src/lib/libc/i18n/wcsrtombs.c
@@ -1,4 +1,4 @@
-/* $MirOS: contrib/code/libhaible/wcsrtombs.c,v 1.1 2006/05/31 00:25:52 tg Exp $ */
+/* $MirOS: src/lib/libc/i18n/wcsrtombs.c,v 1.1 2006/06/01 22:17:21 tg Exp $ */
 
 /*-
  * Copyright (c) 2006
@@ -30,87 +30,85 @@
 
 #include "mir18n.h"
 
-__RCSID("$MirOS: contrib/code/libhaible/wcsrtombs.c,v 1.1 2006/05/31 00:25:52 tg Exp $");
+__RCSID("$MirOS: src/lib/libc/i18n/wcsrtombs.c,v 1.1 2006/06/01 22:17:21 tg Exp $");
 
 size_t
 wcsrtombs(char *__restrict__ dst, const wchar_t **__restrict__ src,
     size_t len, mbstate_t *__restrict__ ps)
 {
-	static mbstate_t internal_mbstate = { 0, 0 };
+	static mbstate_t internal_mbstate = { 0, 0 };	/* if ps == NULL */
 	const wchar_t *s = *src;
 	unsigned char *d = (unsigned char *)dst;
-	size_t numb = 0;
-	wint_t w;
+	wint_t wc, wc_max;
+	unsigned count;
+
+	/* make sure we can at least write one output byte */
+	if ((dst != NULL) && (len == 0))
+		return (0);
 
 	if (__predict_false(ps == NULL))
 		ps = &internal_mbstate;
 
-	/* first process any hanging bytes */
-	while (__locale_is_utf8 && __predict_false(ps->count)) {
-		if (d != NULL) {
-			if (len-- == 0)
-				return (numb);
-			d[numb] = ((ps->value >> (6 * --ps->count)) & 0x3F)
-			    | 0x80;
-		}
-		++numb;
+	/* highest allowed wide character to convert */
+	wc_max = __locale_is_utf8 ? MIR18N_MB_MAX : MIR18N_SB_CVT;
+
+	/* in the 'C' locale, the mbstate information is ignored */
+	if ((count = __locale_is_utf8 ? ps->count : 0)) {
+		wc = ps->value;
+		/* process remnants */
+		goto process_state;
 	}
 
-	/* now process the string */
- loop:
-	w = *s++;
-	if (!__locale_is_utf8 || (w < 0x80)) {
-		if (!__locale_is_utf8 && __predict_false(w > MIR18N_SB_CVT)) {
- ilseq:
-			errno = EILSEQ;
-			return ((size_t)(-1));
-		}
-		if (d != NULL) {
-			if (len-- == 0) {
-				*src = --s;
-				return (numb);
-			}
-			d[numb] = w;
-		}
-		if (w == 0) {
-			*src = NULL;
-			return (numb);
-		}
-	} else if (w < 0x0800) {
-		ps->count = 1;
-		ps->value = w;
-		if (d != NULL) {
-			if (len-- == 0) {
-				*src = --s;
-				return (numb);
-			}
-			d[numb] = (w >> 6) | 0xC0;
-		}
-	} else if (__predict_false(w > MIR18N_MB_MAX)) {
-		goto ilseq;
+ process_firstbyte:
+	/* count is zero here; devour an input wide character */
+	wc = *s++;
+	/* create the first output byte and state information from it */
+	if (__predict_false(wc > wc_max)) {
+		errno = EILSEQ;
+		return ((size_t)(-1));
+	} else if (__predict_true((wc < 0x80) || !__locale_is_utf8)) {
+		if (dst != NULL)
+			*d = wc;
+	} else if (wc < 0x0800) {
+		if (dst != NULL)
+			*d = (wc >> 6) | 0xC0;
+		count = 1;
 	} else {
-		ps->count = 2;
-		ps->value = w;
-		if (d != NULL) {
-			if (len-- == 0) {
-				*src = --s;
-				return (numb);
-			}
-			d[numb] = (w >> 12) | 0xE0;
-		}
+		if (dst != NULL)
+			*d = (wc >> 12) | 0xE0;
+		count = 2;
 	}
-	++numb;
+	/* account the output byte, except if it's the terminating NUL */
+	if (wc > 0)
+		++d;
+	/* at this point, we have written an output byte */
+	--len;
 
-	while (__predict_false(ps->count)) {
-		if (d != NULL) {
-			if (len-- == 0) {
-				*src = s;
-				return (numb);
-			}
-			d[numb] = ((w >> (6 * --ps->count)) & 0x3F) | 0x80;
-		}
-		++numb;
+ process_state:
+	/* entering with len >= 0, count + wc containing state info */
+	while (__predict_false((count > 0) && ((len > 0) || (dst == NULL)))) {
+		--count;
+		if (dst != NULL)
+			*d = ((wc >> (6 * count)) & 0x3F) | 0x80;
+		++d;
+		--len;
 	}
 
-	goto loop;
+	/* here: either len == 0 or count == 0 (or both!) */
+	if (__predict_false(wc == 0))
+		/* last character was a terminating NUL, count == 0 */
+		s = NULL;
+	else if (__predict_true(count == 0))
+		/* so, if there's still output space left or we ignore it */
+		if ((len > 0) || (dst == NULL))
+			/* loop on to the next full input wide character */
+			goto process_firstbyte;
+
+	if (dst != NULL) {
+		*src = s;
+		/* save state information for restarting */
+		ps->count = count;
+		ps->value = wc;
+	}
+	return ((char *)d - dst);
 }
