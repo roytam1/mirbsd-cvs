e54ffffefbf6f871eac9431cecc98f9c327b0d99
tg <>
2013-05-18 15:21:50 +0000

    only build this .if !exists(/usr/lib/libncursesw.a)

--- ports/devel/libncursesw/libncursesw.port.mk
+++ ports/devel/libncursesw/libncursesw.port.mk
@@ -1,5 +1,7 @@
-# $MirOS$
+# $MirOS: ports/devel/libncursesw/libncursesw.port.mk,v 1.1 2006/10/27 15:53:09 tg Exp $
 
+.if !exists(/usr/lib/libncursesw.a)
 LIB_DEPENDS+=			ncursesw::devel/libncursesw
 CPPFLAGS+=			-I${LOCALBASE}/include/ncursesw
+.endif
 CPPFLAGS+=			-D_XOPEN_SOURCE_EXTENDED
