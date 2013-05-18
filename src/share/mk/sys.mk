dad751cb3e9c407172a7d1a195d7659e838bb21e
tg <>
2013-05-18 14:46:26 +0000

    this will be MirOS 08u3B

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.38 2005/11/17 18:30:03 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.39 2005/11/19 12:34:57 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -21,7 +21,7 @@ OStype=		MirBSD
 # Sync these with <sys/param.h>
 unix=		We run ${OStype}.
 OSrev=		8		# MirOS version (major)
-OSrpl=		58		# MirOS patchlevel
+OSrpl=		59		# MirOS patchlevel
 OScompat=	3.5		# OpenBSD compatibility revision
 .if !defined(OSNAME) || empty(OSNAME)
 OSNAME!=	uname -s
--- src/sys/sys/param.h
+++ src/sys/sys/param.h
@@ -1,4 +1,4 @@
-/**	$MirOS: src/sys/sys/param.h,v 1.25 2005/11/21 19:31:55 tg Exp $ */
+/**	$MirOS: src/sys/sys/param.h,v 1.26 2005/11/21 19:51:17 tg Exp $ */
 /*	$OpenBSD: param.h,v 1.54 2004/02/27 18:06:55 deraadt Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
@@ -48,7 +48,7 @@
 #define	BSD	199306		/* System version (year & month). */
 #define BSD4_3	1
 #define BSD4_4	1
-#define	MirBSD	0x083A		/* minor 10-7F=prerelease; 80=release
+#define	MirBSD	0x083B		/* minor 10-7F=prerelease; 80=release
 				 * minor 81-9F=stable; A0-FF=unlocked
 				 */
 #ifndef _LOCORE
