ca0ac82096b399bae0f7bb402c7aa0a74517063a
tg <>
2013-05-18 15:54:14 +0000

    • On Darwin / Mac OSX, ld fails if -x is used with   | /usr/bin/ld: /Volumes/FreeWRT/freewrt/tools_build/w-mirmake-20070626-3/mirmake/build/libmirmake/libmirmake.a(getopt_long.o) malformed object (section (__TEXT,__textcoal_nt) no symbol at start of coalesced section)   or something similar. Drop the -x, use -X which seems to work.   (Note: on Solaris, -x also seems to make problems. Thanks joerg@dragonfly) • The Darwin ld(1) doesn't support -g either.

--- src/share/mk/bsd.lib.mk
+++ src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.lib.mk,v 1.67 2007/06/26 19:05:51 tg Exp $
+# $MirOS: src/share/mk/bsd.lib.mk,v 1.68 2007/06/28 16:07:28 tg Exp $
 # $OpenBSD: bsd.lib.mk,v 1.43 2004/09/20 18:52:38 espie Exp $
 # $NetBSD: bsd.lib.mk,v 1.67 1996/01/17 20:39:26 mycroft Exp $
 # @(#)bsd.lib.mk	5.26 (Berkeley) 5/2/91
@@ -144,6 +144,9 @@ CFLAGS+=	-g1 -fno-omit-frame-pointer
 CXXFLAGS+=	-g1
 .  endif
 DEBUG?=		-g
+_DISCARD=	-X
+_SODISCARD=	-X
+.elif ${RTLD_TYPE} == "dyld"
 _DISCARD=	-X
 _SODISCARD=	-X
 .else
