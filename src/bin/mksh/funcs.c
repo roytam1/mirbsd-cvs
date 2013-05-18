f57838c89c293b6ca666bf86ac7ee49f4985f4ca
tg <>
2013-05-18 17:52:46 +0000

    __CRAZY=Yes clean wants switch(enum) to be complete

--- src/bin/mksh/funcs.c
+++ src/bin/mksh/funcs.c
@@ -38,7 +38,7 @@
 #endif
 #endif
 
-__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.232 2012/12/04 01:18:28 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.233 2012/12/08 18:30:30 tg Exp $");
 
 #if HAVE_KILLPG
 /*
@@ -2918,6 +2918,9 @@ test_eval(Test_env *te, Test_op op, const char *opnd1, const char *opnd2,
 			te->flags |= TEF_ERROR;
 			return (1);
 		}
+		break;
+	default:
+		/* for completeness of switch */
 		break;
 	}
 #endif
