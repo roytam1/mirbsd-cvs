019c24f27114e1d9dd378c33a88dbbaabaff48bc
tg <>
2013-05-18 16:21:18 +0000

    SUNw,cc is one of the compilers whose version reporting works better when compiling a demo programme at the same time

--- src/bin/mksh/Build.sh
+++ src/bin/mksh/Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.325 2008/05/20 18:47:06 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.326 2008/06/21 19:20:14 tg Exp $'
 #-
 # Environment used: CC CFLAGS CPPFLAGS LDFLAGS LIBS NOWARN NROFF TARGET_OS
 # CPPFLAGS recognised:	MKSH_SMALL MKSH_ASSUME_UTF8 MKSH_NOPWNAM MKSH_NOVI
@@ -559,7 +559,7 @@ sdcc)
 	EOF
 	;;
 sunpro)
-	vv '|' "$CC -v"
+	vv '|' "$CC -V scn.c"
 	;;
 tcc)
 	vv '|' "$CC -v"
