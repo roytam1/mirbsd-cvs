f43c6a799f4de932cf5c6ef157c6260aa02cb424
tg <>
2013-05-18 15:38:10 +0000

    merge -rHEAD as of now into the const-correctness branch

--- src/bin/mksh/Build.sh
+++ src/bin/mksh/Build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $MirOS: src/bin/mksh/Build.sh,v 1.110 2007/01/12 03:30:40 tg Exp $
+# $MirOS: src/bin/mksh/Build.sh,v 1.154 2007/03/03 21:36:06 tg Exp $
 #-
-# Environment: CC, CFLAGS, CPP, CPPFLAGS, LDFLAGS, LIBS, NOWARN, NROFF
-# With -x (cross compile): TARGET_OS (default: uname -s)
+# Env: CC, CFLAGS, CPP, CPPFLAGS, LDFLAGS, LIBS, NOWARN, NROFF, TARGET_OS
+# CPPFLAGS recognised: MKSH_SMALL MKSH_ASSUME_UTF8 MKSH_NEED_MKNOD MKSH_NOPWNAM
 
 v()
 {
@@ -15,6 +15,12 @@ vq()
 	eval "$@"
 }
 
+if test -d /usr/xpg4/bin/. >/dev/null 2>&1; then
+	# Solaris: some of the tools have weird behaviour, use portable ones
+	PATH=/usr/xpg4/bin:$PATH
+	export PATH
+fi
+
 if test -t 1; then
 	bi=`printf '\033[1m'`
 	ui=`printf '\033[4m'`
@@ -25,9 +31,14 @@ else
 	ao=
 fi
 
+allu=QWERTYUIOPASDFGHJKLZXCVBNM
+alll=qwertyuiopasdfghjklzxcvbnm
+alln=0123456789
+alls=______________________________________________________________
+
 upper()
 {
-	echo "$@" | tr qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM
+	echo :"$@" | sed 's/^://' | tr $alll $allu
 }
 
 # pipe .c | ac_test[n] label [!] checkif[!]0 [setlabelifcheckis[!]0] useroutput
@@ -85,6 +96,38 @@ ac_test()
 	eval CPPFLAGS=\"\$CPPFLAGS -DHAVE_$fu=\$HAVE_$fu\"
 }
 
+# ac_flags add varname flags [text]
+ac_flags()
+{
+	fa=$1
+	vn=$2
+	f=$3
+	ft=$4
+	test x"$ft" = x"" && ft="if $f can be used"
+	save_CFLAGS=$CFLAGS
+	CFLAGS="$CFLAGS $f"
+	ac_testn can_$vn '' "$ft" <<-'EOF'
+		int main(void) { return (0); }
+	EOF
+	eval fv=\$HAVE_CAN_`upper $vn`
+	test 11 = $fa$fv || CFLAGS=$save_CFLAGS
+}
+
+# ac_header header [prereq ...]
+ac_header()
+{
+	hf=$1; shift
+	hv=`echo "$hf" | tr -d '\r\n' | tr -c $alll$allu$alln $alls`
+	for i
+	do
+		echo "#include <$i>" >>x
+	done
+	echo "#include <$hf>" >>x
+	echo 'int main(void) { return (0); }' >>x
+	ac_test "$hv" "" "<$hf>" <x
+	rm -f x
+}
+
 addsrcs()
 {
 	eval i=\$$1
@@ -94,27 +137,28 @@ addsrcs()
 	esac
 }
 
+
 if test -d mksh; then
 	echo "$0: Error: ./mksh is a directory!" >&2
 	exit 1
 fi
 
-: ${CFLAGS='-O2 -fno-strict-aliasing -Wall'}
-: ${CC=gcc} ${NROFF=nroff}
+: ${CC=gcc} ${CPP=false} ${NROFF=nroff}
 curdir=`pwd` srcdir=`dirname "$0"` check_categories=pdksh
-echo | $NROFF -v 2>&1 | grep GNU >/dev/null 2>&1 && NROFF="$NROFF -c"
 
 e=echo
 h=1
 r=0
-x=0
-LDSTATIC=-static
+s=def
 
 for i
 do
 	case $i in
 	-d)
-		LDSTATIC=@@
+		s=dyn
+		;;
+	-nd)
+		s=sta
 		;;
 	-q)
 		e=:
@@ -122,10 +166,6 @@ do
 		;;
 	-r)
 		r=1
-		;;
-	-x)
-		x=1
-		LDSTATIC=@@
 		;;
 	*)
 		echo "$0: Unknown option '$i'!" >&2
@@ -137,52 +177,91 @@ done
 SRCS="alloc.c edit.c eval.c exec.c expr.c funcs.c histrap.c"
 SRCS="$SRCS jobs.c lex.c main.c misc.c shf.c syn.c tree.c var.c"
 
-test $x = 0 && TARGET_OS=`uname -s 2>/dev/null || uname`
+test $r = 0 && echo | $NROFF -v 2>&1 | grep GNU >/dev/null 2>&1 && \
+    NROFF="$NROFF -c"
+
+
+test x"$TARGET_OS" = x"" && TARGET_OS=`uname -s 2>/dev/null || uname`
+warn=
 case $TARGET_OS in
 CYGWIN*)
-	test x"@@" = x"$LDSTATIC" || LDSTATIC=@
+	test def = $s && s=pam
 	;;
 Darwin)
-	test x"@@" = x"$LDSTATIC" || LDSTATIC=@
 	CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64"
+	test def = $s && s=pam
+	;;
+DragonFly)
+	;;
+FreeBSD)
+	;;
+GNU)
+	warn=' but should work'
+	CPPFLAGS="$CPPFLAGS -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64"
+	;;
+GNU/kFreeBSD)
+	warn=' but should work'
+	CPPFLAGS="$CPPFLAGS -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64"
 	;;
 Interix)
 	CPPFLAGS="$CPPFLAGS -D_ALL_SOURCE"
 	;;
 Linux)
-	CPPFLAGS="$CPPFLAGS -D_POSIX_C_SOURCE=2 -D_BSD_SOURCE -D_GNU_SOURCE"
-	CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64"
-	test x"@@" = x"$LDSTATIC" || LDSTATIC=@
+	CPPFLAGS="$CPPFLAGS -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64"
+	test def = $s && s=pam
+	: ${HAVE_REVOKE=0}
+	;;
+MirBSD)
+	;;
+NetBSD)
+	;;
+OpenBSD)
 	;;
 SunOS)
 	CPPFLAGS="$CPPFLAGS -D_BSD_SOURCE -D__EXTENSIONS__"
 	CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64"
-	test x"@@" = x"$LDSTATIC" || LDSTATIC=@
+	test def = $s && s=pam
 	r=1
+	;;
+*)
+	warn='; it may or may not work'
 	;;
 esac
 
+if test -n "$warn"; then
+	echo "Warning: mksh has not yet been ported to or tested on your" >&2
+	echo "operating system '$TARGET_OS'$warn. If you can provide" >&2
+	echo "a shell account to the developer, this may improve; please" >&2
+	echo "drop us a success or failure notice or even send in diffs." >&2
+fi
+
 CPPFLAGS="$CPPFLAGS -I'$curdir'"
+
+
+#
+# Begin of mirtoconf checks
+#
+rm -f scn.c a.out a.exe x
 $e ${ao}Scanning for functions... please ignore any errors.
 
-ac_testn compiler_works '' 'if the compiler works' <<-'EOF'
-	int main(void) { return (0); }
-EOF
-test 1 = $HAVE_COMPILER_WORKS || exit 1
-
+#
+# Compiler: works as-is, with -Wno-error and -Werror
+#
 test x"$NOWARN" = x"" && NOWARN=-Wno-error
-ac_testn can_wnoerror '' "if '$NOWARN' can be used" <<-'EOF'
-	int main(void) { return (0); }
-EOF
-test 1 = $HAVE_CAN_WNOERROR || NOWARN=
-
 save_NOWARN=$NOWARN
-NOWARN=-Werror
-ac_testn can_werror '' "if -Werror can be used" <<-'EOF'
-	int main(void) { return (0); }
-EOF
-test 1 = $HAVE_CAN_WERROR || NOWARN=
+NOWARN=
+ac_flags 0 compiler_works '' 'if the compiler works'
+test 1 = $HAVE_CAN_COMPILER_WORKS || exit 1
+ac_flags 0 wnoerror "$save_NOWARN"
+test 1 = $HAVE_CAN_WNOERROR || save_NOWARN=
+ac_flags 0 werror "-Werror"
 
+# The following tests are run with -Werror if possible
+test 1 = $HAVE_CAN_WERROR && NOWARN=-Werror
+
+#
+# Compiler: check for stuff that only generates warnings
+#
 ac_test attribute '' 'if we have __attribute__((...)) at all' <<-'EOF'
 	#include <stdlib.h>
 	void fnord(void) __attribute__((noreturn));
@@ -206,8 +285,25 @@ ac_test attribute_used attribute 0 'for __attribute__((used))' <<-'EOF'
 	int main(void) { return (0); }
 EOF
 
+# End of tests run with -Werror
 NOWARN=$save_NOWARN
 
+#
+# Compiler: extra flags (-O2 -f* -W* etc.)
+#
+i=`echo :"$CFLAGS" | sed 's/^://' | tr -c -d $alll$allu$alln-`
+test x"$i" = x"" && ac_flags 1 otwo "-O2"
+ac_flags 1 fnostrictaliasing "-fno-strict-aliasing"
+ac_flags 1 fwholepgm "-fwhole-program --combine"
+ac_flags 1 fwrapv "-fwrapv"
+# I'd use -std=c99 but this wrecks havoc on glibc and cygwin based
+# systems (at least) because their system headers are so broken...
+ac_flags 1 stdg99 "-std=gnu99" 'if -std=gnu99 (ISO C99) can be used'
+ac_flags 1 wall "-Wall"
+
+#
+# mksh: flavours (full/small mksh, omit certain stuff)
+#
 ac_testn mksh_full '' "if we're building without MKSH_SMALL" <<-'EOF'
 	#ifdef MKSH_SMALL
 	#error OK, we are building an extra small mksh.
@@ -215,39 +311,90 @@ ac_testn mksh_full '' "if we're building without MKSH_SMALL" <<-'EOF'
 	int main(void) { return (0); }
 EOF
 
+ac_testn mksh_defutf8 '' "if we assume UTF-8 is enabled" <<-'EOF'
+	#ifndef MKSH_ASSUME_UTF8
+	#error Nope, we shall check with setlocale() and nl_langinfo(CODESET)
+	#endif
+	int main(void) { return (0); }
+EOF
+
+ac_testn mksh_need_mknod '!' mksh_full 1 'if we still want c_mknod()' <<-'EOF'
+	#ifndef MKSH_NEED_MKNOD
+	#error Nope, the user really wants it teensy.
+	#endif
+	int main(void) { return (0); }
+EOF
+
+ac_testn mksh_nopam mksh_full 1 'if the user wants to omit getpwnam()' <<-'EOF'
+	#ifndef MKSH_NOPWNAM
+	#error No, the user wants to pull in getpwnam.
+	#endif
+	int main(void) { return (0); }
+EOF
+
 if test 0 = $HAVE_MKSH_FULL; then
-	save_CFLAGS=$CFLAGS
-	CFLAGS="$CFLAGS -fno-inline"
-	ac_testn can_fnoinline '' 'if -fno-inline can be used' <<-'EOF'
-		int main(void) { return (0); }
-	EOF
-	test 1 = $HAVE_CAN_FNOINLINE || CFLAGS=$save_CFLAGS
+	ac_flags 1 fnoinline "-fno-inline"
 
 	: ${HAVE_SETLOCALE_CTYPE=0}
 	check_categories=$check_categories,smksh
 fi
 
-# I'd use -std=c99 but this wrecks havoc on glibc and cygwin based
-# systems (at least) because their system headers are so broken...
-save_CFLAGS=$CFLAGS
-CFLAGS="$CFLAGS -std=gnu99"
-ac_testn can_stdg99 '' 'if -std=gnu99 (ISO C99) can be used' <<-'EOF'
-	int main(void) { return (0); }
-EOF
-test 1 = $HAVE_CAN_STDG99 || CFLAGS=$save_CFLAGS
+#
+# Environment: headers
+#
+ac_header sys/param.h
+ac_header sys/mkdev.h
+ac_header sys/sysmacros.h
+ac_header libgen.h
+ac_header paths.h
+ac_header stdbool.h
+ac_header grp.h sys/types.h
+ac_header ulimit.h
+ac_header values.h
 
-save_CFLAGS=$CFLAGS
-CFLAGS="$CFLAGS -fwhole-program --combine"
-ac_testn can_fwholepgm '' 'if -fwhole-program --combine can be used' <<-'EOF'
-	int main(void) { return (0); }
+#
+# Environment: types
+#
+ac_test rlim_t <<-'EOF'
+	#include <sys/types.h>
+	#include <sys/time.h>
+	#include <sys/resource.h>
+	#include <unistd.h>
+	int main(void) { return ((int)(rlim_t)0); }
 EOF
-test 1 = $HAVE_CAN_FWHOLEPGM || CFLAGS=$save_CFLAGS
 
-ac_test sys_param_h '' '<sys/param.h>' <<'EOF'
-	#include <sys/param.h>
-	int main(void) { return (0); }
+# only testn: added later below
+ac_testn sig_t <<-'EOF'
+	#include <sys/types.h>
+	#include <signal.h>
+	int main(void) { return ((int)(sig_t)0); }
 EOF
 
+ac_testn sighandler_t '!' sig_t 0 <<-'EOF'
+	#include <sys/types.h>
+	#include <signal.h>
+	int main(void) { return ((int)(sighandler_t)0); }
+EOF
+if test 1 = $HAVE_SIGHANDLER_T; then
+	CPPFLAGS="$CPPFLAGS -Dsig_t=sighandler_t"
+	HAVE_SIG_T=1
+fi
+
+ac_testn __sighandler_t '!' sig_t 0 <<-'EOF'
+	#include <sys/types.h>
+	#include <signal.h>
+	int main(void) { return ((int)(__sighandler_t)0); }
+EOF
+if test 1 = $HAVE___SIGHANDLER_T; then
+	CPPFLAGS="$CPPFLAGS -Dsig_t=__sighandler_t"
+	HAVE_SIG_T=1
+fi
+
+CPPFLAGS="$CPPFLAGS -DHAVE_SIG_T=$HAVE_SIG_T"
+
+#
+# Environment: signals
+#
 ac_test mksh_signame '' 'our own list of signal names' <<-'EOF'
 	#include <stdlib.h>	/* for NULL */
 	#define MKSH_SIGNAMES_CHECK
@@ -271,22 +418,31 @@ else
 	NEED_MKSH_SIGNAME=0
 fi
 
-ac_test sys_siglist '' 'the sys_siglist[] array' <<-'EOF'
+# only testn: added later below
+ac_testn sys_siglist '' 'the sys_siglist[] array' <<-'EOF'
 	extern const char *const sys_siglist[];
 	int main(void) { return (sys_siglist[0][0]); }
 EOF
 
-ac_test _sys_siglist '!' sys_siglist 0 'the _sys_siglist[] array' <<-'EOF'
+ac_testn _sys_siglist '!' sys_siglist 0 'the _sys_siglist[] array' <<-'EOF'
 	extern const char *const _sys_siglist[];
 	int main(void) { return (_sys_siglist[0][0]); }
 EOF
+if test 1 = $HAVE__SYS_SIGLIST; then
+	CPPFLAGS="$CPPFLAGS -Dsys_siglist=_sys_siglist"
+	HAVE_SYS_SIGLIST=1
+fi
+CPPFLAGS="$CPPFLAGS -DHAVE_SYS_SIGLIST=$HAVE_SYS_SIGLIST"
 
-ac_test strsignal '!' _sys_siglist 0 <<-'EOF'
+ac_test strsignal '!' sys_siglist 0 <<-'EOF'
 	#include <string.h>
 	#include <signal.h>
 	int main(void) { return (strsignal(1)[0]); }
 EOF
 
+#
+# Environment: library functions
+#
 ac_test arc4random <<-'EOF'
 	#include <stdlib.h>
 	int main(void) { return (arc4random()); }
@@ -297,7 +453,14 @@ ac_test arc4random_push arc4random 0 <<-'EOF'
 	int main(void) { arc4random_push(1); return (0); }
 EOF
 
-ac_test setlocale_ctype '' 'setlocale(LC_CTYPE, "")' <<-'EOF'
+ac_test flock_ex '' 'flock and LOCK_EX' <<-'EOF'
+	#include <sys/types.h>
+	#include <sys/file.h>
+	#include <fcntl.h>
+	int main(void) { return (flock(0, LOCK_EX)); }
+EOF
+
+ac_test setlocale_ctype '!' mksh_defutf8 0 'setlocale(LC_CTYPE, "")' <<-'EOF'
 	#include <locale.h>
 	#include <stddef.h>
 	int main(void) { return ((ptrdiff_t)(void *)setlocale(LC_CTYPE, "")); }
@@ -309,7 +472,12 @@ ac_test langinfo_codeset setlocale_ctype 0 'nl_langinfo(CODESET)' <<-'EOF'
 	int main(void) { return ((ptrdiff_t)(void *)nl_langinfo(CODESET)); }
 EOF
 
-ac_test setmode mksh_full 1 <<-'EOF'
+ac_test revoke mksh_full 0 <<-'EOF'
+	#include <unistd.h>
+	int main(int ac, char *av[]) { return (ac + revoke(av[0])); }
+EOF
+
+ac_test setmode mksh_need_mknod 1 <<-'EOF'
 	#if defined(__MSVCRT__) || defined(__CYGWIN__)
 	#error Win32 setmode() is different from what we need
 	#endif
@@ -325,7 +493,9 @@ EOF
 
 ac_test setgroups setresugid 0 <<-'EOF'
 	#include <sys/types.h>
+	#if HAVE_GRP_H
 	#include <grp.h>
+	#endif
 	#include <unistd.h>
 	int main(void) { gid_t gid = 0; return (setgroups(0, &gid)); }
 EOF
@@ -343,34 +513,62 @@ ac_test strlcpy <<-'EOF'
 	int main(int ac, char *av[]) { return (strlcpy(*av, av[1], ac)); }
 EOF
 
+#
+# other checks
+#
+ac_test multi_idstring '' 'if we can use __RCSID(x) multiple times' <<-'EOF'
+	#include "sh.h"
+	__RCSID("one");
+	__RCSID("two");
+	int main(void) { return (0); }
+EOF
+
+ac_test persistent_history mksh_full 0 'if to use persistent history' <<-'EOF'
+	#if !HAVE_FLOCK_EX || defined(MKSH_SMALL)
+	#error No, some prerequisites are missing.
+	#endif
+	int main(void) { return (0); }
+EOF
+test 1 = $HAVE_PERSISTENT_HISTORY || \
+    check_categories=$check_categories,no-histfile
+
+#
+# Compiler: Praeprocessor (only if needed)
+#
 if test 1 = $NEED_MKSH_SIGNAME; then
-	$e "... checking how to run the C Preprocessor"
-	rm -f x
-	( ( echo '#if (23 * 2 - 2) == (fnord + 2)'
-	    echo mksh_rules: fnord
-	    echo '#endif'
-	  ) | v "$CC -E - $CPPFLAGS -Dfnord=42 >x" ) 2>&$h | sed 's/^/] /'
-	if grep '^mksh_rules:.*42' x >/dev/null 2>&1; then
-		CPP="$CC -E -"
-	else
-		rm -f x
+	$e ... checking how to run the C Preprocessor
+	save_CPP=$CPP
+	for i in "$save_CPP" "$CC -E -" "cpp" "/usr/libexec/cpp" "/lib/cpp"; do
+		CPP=$i
+		test x"$CPP" = x"false" && continue
 		( ( echo '#if (23 * 2 - 2) == (fnord + 2)'
 		    echo mksh_rules: fnord
 		    echo '#endif'
 		  ) | v "$CPP $CPPFLAGS -Dfnord=42 >x" ) 2>&$h | sed 's/^/] /'
-		grep '^mksh_rules:.*42' x >/dev/null 2>&1 || CPP=no
-	fi
-	rm -f x
+		grep '^mksh_rules:.*42' x >/dev/null 2>&1 || CPP=false
+		rm -f x
+		test x"$CPP" = x"false" || break
+	done
 	$e "$bi==> checking how to run the C Preprocessor...$ao $ui$CPP$ao"
-	test x"$CPP" = x"no" && exit 1
+	test x"$CPP" = x"false" && exit 1
 fi
 
+#
+# End of mirtoconf checks
+#
 $e ... done.
+
+# Some operating systems have ancient versions of ed(1) writing
+# the character count to standard output; cope for that
+echo wq >x
+ed x <x 2>/dev/null | grep 3 >/dev/null 2>&1 && \
+    check_categories=$check_categories,oldish-ed
+rm -f x
 
 if test 1 = $NEED_MKSH_SIGNAME; then
 	$e Generating list of signal names...
 	sigseen=:
-	NSIG=`( echo '#include <signal.h>'; echo "mksh_cfg: NSIG" ) | \
+	NSIG=`( echo '#include <signal.h>'; echo mksh_cfg: NSIG ) | \
 	    vq "$CPP $CPPFLAGS" | grep mksh_cfg: | \
 	    sed 's/^mksh_cfg: \([0-9x]*\).*$/\1/'`
 	NSIG=`printf %d "$NSIG" 2>/dev/null`
@@ -380,7 +578,7 @@ if test 1 = $NEED_MKSH_SIGNAME; then
 	    grep '[	 ]SIG[A-Z0-9]*[	 ]' | \
 	    sed 's/^\(.*[	 ]SIG\)\([A-Z0-9]*\)\([	 ].*\)$/\2/' | \
 	    while read name; do
-		( echo '#include <signal.h>'; echo "mksh_cfg: SIG$name" ) | \
+		( echo '#include <signal.h>'; echo mksh_cfg: SIG$name ) | \
 		    vq "$CPP $CPPFLAGS" | grep mksh_cfg: | \
 		    sed 's/^mksh_cfg: \([0-9x]*\).*$/\1:'$name/
 	done | grep -v '^:' | while IFS=: read nr name; do
@@ -403,14 +601,12 @@ addsrcs HAVE_STRCASESTR strcasestr.c
 addsrcs HAVE_STRLCPY strlfun.c
 CPPFLAGS="$CPPFLAGS -DHAVE_CONFIG_H -DCONFIG_H_FILENAME=\\\"sh.h\\\""
 
-test x"@@" = x"$LDSTATIC" && LDSTATIC=
-test x"@" = x"$LDSTATIC" && if test 0 = $HAVE_MKSH_FULL; then
-	LDSTATIC=-static
-else
-	LDSTATIC=
-fi
+case $s:$HAVE_MKSH_NOPAM in
+def:*|sta:*|pam:1)
+	LDFLAGS="$LDFLAGS -static" ;;
+esac
 (v "cd '$srcdir' && exec $CC $CFLAGS $CPPFLAGS" \
-    "$LDFLAGS $LDSTATIC -o '$curdir/mksh' $SRCS $LIBS") || exit 1
+    "$LDFLAGS -o '$curdir/mksh' $SRCS $LIBS") || exit 1
 result=mksh
 test -f mksh.exe && result=mksh.exe
 test -f $result || exit 1
@@ -429,7 +625,7 @@ test -f /usr/ucb/$i && i=/usr/ucb/$i
 $e
 $e Installing the shell:
 $e "# $i -c -s -o root -g bin -m 555 mksh /bin/mksh"
-$e "# grep -qx /bin/mksh /etc/shells || echo /bin/mksh >>/etc/shells"
+$e "# grep -x /bin/mksh /etc/shells >/dev/null || echo /bin/mksh >>/etc/shells"
 $e "# $i -c -o root -g bin -m 444 dot.mkshrc /usr/share/doc/mksh/examples/"
 $e
 $e Installing the manual:
