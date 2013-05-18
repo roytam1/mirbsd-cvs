b543ea82dfad94bd74924c6962911ebba086238e
tg <>
2013-05-18 15:45:57 +0000

    • add new directory to base: /usr/share/pkgconfig • ports/devel/pkgconfig module: use it • src/lib/libssl: add a pregenerated "openssl.pc" file; install it • while here, enable the zlib compression in openssl, link against libz,   bump minor (not needed for libz link due to DT_RPATH, but because c_zlib.c   exports some new functions) • sync lists

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++ ports/devel/pkgconfig/pkgconfig.port.mk
@@ -1,7 +1,7 @@
-# $MirOS$
+# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.1 2005/12/28 00:40:19 bsiegert Exp $
 #
 # module file for ports that use pkgconfig
 
 BUILD_DEPENDS+=		:pkgconfig-*:devel/pkgconfig
-PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig
+PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig:/usr/share/pkgconfig
 CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
