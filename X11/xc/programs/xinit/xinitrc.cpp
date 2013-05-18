180feb7d29a7f858ec7f23b2edba72b518e5beef
tg <>
2013-05-18 15:06:15 +0000

    don't use mksh features here spotted by bsiegert@ on showcase

--- X11/xc/programs/xinit/xinitrc.cpp
+++ X11/xc/programs/xinit/xinitrc.cpp
@@ -1,5 +1,5 @@
-XCOMM!/bin/mksh
-XCOMM $MirOS: X11/xc/programs/xinit/xinitrc.cpp,v 1.5 2006/02/02 19:38:08 tg Exp $
+XCOMM!/bin/sh
+XCOMM $MirOS: X11/xc/programs/xinit/xinitrc.cpp,v 1.6 2006/04/05 21:57:59 tg Exp $
 XCOMM $Xorg: xinitrc.cpp,v 1.3 2000/08/17 19:54:30 cpqbld Exp $
 
 userresources=$HOME/.Xresources
@@ -9,19 +9,19 @@ sysmodmap=XINITDIR/.Xmodmap
 
 XCOMM merge in defaults and keymaps
 
-if [[ -f $sysresources ]]; then
+if [ -f $sysresources ]; then
     xrdb -merge $sysresources
 fi
 
-if [[ -f $sysmodmap ]]; then
+if [ -f $sysmodmap ]; then
     xmodmap $sysmodmap
 fi
 
-if [[ -f $userresources ]]; then
+if [ -f $userresources ]; then
     xrdb -merge $userresources
 fi
 
-if [[ -f $usermodmap ]]; then
+if [ -f $usermodmap ]; then
     xmodmap $usermodmap
 fi
 
