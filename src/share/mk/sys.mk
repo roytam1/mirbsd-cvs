de43c2d54d9f8f85e13a15f59daa7301f235afc2
tg <>
2013-05-18 16:23:47 +0000

    bump mirmake vsn, even if we don't do a portable mirmake release (yet)

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.123 2008/04/07 05:18:17 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.124 2008/04/10 13:55:56 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -29,7 +29,7 @@ OSNAME!=	uname -s
 OSname=		${OSNAME:L}
 OStriplet?=	${MACHINE_ARCH}-ecce-${OSname}${OSrev}
 _MIRMAKE_EXE=	/usr/bin/make
-_MIRMAKE_VER=	20080410
+_MIRMAKE_VER=	20080711
 
 .SUFFIXES:	.out .a .ln .o .s .S .c .m .cc .C .cxx .cpp .y .l .i .h .sh .m4
 .LIBS:		.a
