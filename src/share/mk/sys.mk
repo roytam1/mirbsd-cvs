cfe59ba8b062a690d8e28e1ca99316db1397e470
tg <>
2013-05-18 15:02:49 +0000

    oops, an M in my tree, belongs to commitid 100447F717560592A14

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.57 2006/05/25 13:00:29 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.58 2006/05/27 19:21:35 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -21,7 +21,7 @@ OStype=		MirBSD
 # Sync these with <sys/param.h>
 unix=		We run ${OStype}.
 OSrev=		8		# MirOS version (major)
-OSrpl=		167		# MirOS patchlevel
+OSrpl=		168		# MirOS patchlevel
 OScompat=	3.5		# OpenBSD compatibility revision
 .if !defined(OSNAME) || empty(OSNAME)
 OSNAME!=	uname -s
