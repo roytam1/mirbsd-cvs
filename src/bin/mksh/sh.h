c0e8efa095c8d09d7275144f8331a2aecb97bf86
tg <>
2013-05-18 16:50:41 +0000

    sync; fold oksh-seterror test into errexit-*; use print not printf

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -102,9 +102,9 @@
 #define __SCCSID(x)	__IDSTRING(sccsid,x)
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.281 2009/03/22 17:31:17 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.282 2009/03/22 17:47:38 tg Exp $");
 #endif
-#define MKSH_VERSION "R36 2009/03/17"
+#define MKSH_VERSION "R36 2009/03/22"
 
 #ifndef MKSH_INCLUDES_ONLY
 
