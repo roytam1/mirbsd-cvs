09a43ac8d7740284d7bc78a8369b09de7c76dff3
tg <>
2013-05-18 17:31:54 +0000

    finish merging from R40-stable; set version number to yesterday though

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -151,9 +151,9 @@
 #endif
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.485 2011/07/16 17:07:34 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.486 2011/07/16 17:08:21 tg Exp $");
 #endif
-#define MKSH_VERSION "R40 2011/07/07"
+#define MKSH_VERSION "R40 2011/07/15"
 
 #ifndef MKSH_INCLUDES_ONLY
 
