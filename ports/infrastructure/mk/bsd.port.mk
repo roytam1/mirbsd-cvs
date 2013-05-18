a86cc9a5d670c93ba7aec4f10edb4a1c5643ff1c
tg <>
2013-05-18 15:09:37 +0000

    use sudo for cleaning the build area
    there are some misbehaving ports (e.g. Python, AquaTerm)
    who write to the build area (i.e. outside the fake tree)
    during install time, and it doesn't look like they get
    fixed any time soon (doesn't look easy either)
    
    requested by bsiegert@

--- ports/infrastructure/mk/bsd.port.mk
+++ ports/infrastructure/mk/bsd.port.mk
@@ -1,4 +1,4 @@
-# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.118 2006/07/11 14:26:00 tg Exp $
+# $MirOS: ports/infrastructure/mk/bsd.port.mk,v 1.119 2006/07/11 14:26:38 tg Exp $
 # $OpenBSD: bsd.port.mk,v 1.677 2005/01/06 19:30:34 espie Exp $
 # $FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 # $NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
@@ -2265,8 +2265,8 @@ clean:
 		${SUDO} rm -rf $$i; \
 	done
 .    else
-	@[ ! -L ${WRKDIR} ] || rm -rf $$(readlink ${WRKDIR})
-	@rm -rf ${WRKDIR}
+	@[ ! -L ${WRKDIR} ] || ${SUDO} rm -rf $$(readlink ${WRKDIR})
+	@${SUDO} rm -rf ${WRKDIR}
 .    endif
 .  endif
 .  if ${_clean:L:Mdist}
