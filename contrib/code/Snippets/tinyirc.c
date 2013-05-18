aa83eabc0ac00f5f2f90725e95ab023db089bcaa
tg <>
2013-05-18 14:59:45 +0000

    __CRAZY=Yes clean

--- contrib/code/Snippets/tinyirc.c
+++ contrib/code/Snippets/tinyirc.c
@@ -1,4 +1,4 @@
-/* $MirOS: contrib/code/Snippets/tinyirc.c,v 1.3 2006/05/21 10:48:26 tg Exp $ */
+/* $MirOS: contrib/code/Snippets/tinyirc.c,v 1.4 2006/05/21 10:53:15 tg Exp $ */
 
 /* Configuration options */
 static const char DEFAULTSERVER[] = "irc.mirbsd.org";
@@ -77,7 +77,7 @@ static const char DEFAULTSERVER[] = "irc.mirbsd.org";
 #define	__RCSID(x)	static const char __rcsid[] __attribute__((used)) = (x)
 #endif
 
-__RCSID("$MirOS: contrib/code/Snippets/tinyirc.c,v 1.3 2006/05/21 10:48:26 tg Exp $");
+__RCSID("$MirOS: contrib/code/Snippets/tinyirc.c,v 1.4 2006/05/21 10:53:15 tg Exp $");
 
 struct dlist {
     char name[64];
@@ -87,9 +87,10 @@ struct dlist *object = NULL, *objlist = NULL, *newobj;
 u_short IRCPORT = DEFAULTPORT;
 #define LINELEN 512
 int sockfd, sok = 1, stdinfd, stdoutfd, histline, dumb = 0, CO, LI, column;
-char *CM, *CS, *CE, *linein, lineout[LINELEN], *history[HISTLEN], localhost[64],
-*SO, *SE, *tok_in[256], *tok_out[256], *tmp, serverdata[512], termcap[1024],
-*DC, *ptr, *term, *fromhost, IRCNAME[10], inputbuf[512], beenden = 0;
+char *linein, lineout[LINELEN], *history[HISTLEN], localhost[64],
+*tok_in[256], *tok_out[256], *tmp, serverdata[512], termcap[1024],
+*ptr, *term, *fromhost, IRCNAME[10], inputbuf[512], beenden = 0;
+const char *CM, *CS, *CE, *SO, *SE, *DC;
 char bp[4096];
 int cursd = 0, curli = 0, curx = 0;
 fd_set readfs;
@@ -98,6 +99,15 @@ struct tm *timenow;
 static time_t idletimer, datenow, wasdate;
 struct passwd *userinfo;
 
+char s_co[] = "co";
+char s_li[] = "li";
+char s_cm[] = "cm";
+char s_CM[] = "CM";
+char s_so[] = "so";
+char s_se[] = "se";
+char s_cs[] = "cs";
+char s_ce[] = "ce";
+char s_dc[] = "dc";
 struct termios _tty;
 tcflag_t _res_iflg, _res_lflg;
 #define raw() (_tty.c_lflag &= ~(ICANON | ECHO | ISIG), \
@@ -107,16 +117,43 @@ tcflag_t _res_iflg, _res_lflg;
 #define resetty() (_tty.c_iflag = _res_iflg, _tty.c_lflag = _res_lflg,\
 	(void) tcsetattr(stdinfd, TCSADRAIN, &_tty))
 
-int putchar_x(c)
-int c;
-{
-    return putchar(c);
-}
+#define	tputs_x(s) (tputs(s,0,putchar))
 
-#define	tputs_x(s) (tputs(s,0,putchar_x))
+int my_stricmp(const char *, const char *);
+struct dlist *additem(char *, struct dlist *);
+struct dlist *finditem(char *, struct dlist *);
+struct dlist *removeitem(char *, struct dlist *);
+int makeconnect(const char *);
+int sendline(void);
+void updatestatus(void);
+static int nop(void);
+static int doerror(void);
+static int doinvite(void);
+static int dojoin(void);
+static int dokick(void);
+static int dokill(void);
+static int domode(void);
+static int donick(void);
+static int donotice(void);
+static int dopart(void);
+static int dopong(void);
+static int doprivmsg(void);
+static int doquit(void);
+static int dosquit(void);
+static int dotime(void);
+static int dotopic(void);
+int donumeric(int);
+int wordwrapout(char *, size_t);
+int parsedata(void);
+int serverinput(void);
+void parseinput(void);
+void histupdate(void);
+void printpartial(int);
+void userinput(void);
+__dead void cleanup(int);
+int main(int, char *[]);
 
-int my_stricmp(str1, str2)
-char *str1, *str2;
+int my_stricmp(const char *str1, const char *str2)
 {
     int compare;
 
@@ -135,37 +172,33 @@ char *str1, *str2;
     return (*str1 - *str2);
 }
 
-struct dlist *additem(item, ptr)
-char *item;
-struct dlist *ptr;
+struct dlist *additem(char *item, struct dlist *ptrx)
 {
     newobj = (struct dlist *) malloc(sizeof(struct dlist));
     strlcpy(newobj->name, item, 64);
-    newobj->next = ptr;
+    newobj->next = ptrx;
     return newobj;
 }
-struct dlist *finditem(item, ptr)
-char *item;
-struct dlist *ptr;
+
+struct dlist *finditem(char *item, struct dlist *ptrx)
 {
-    while (ptr != NULL)
-	if (my_stricmp(item, ptr->name) == 0)
+    while (ptrx != NULL)
+	if (my_stricmp(item, ptrx->name) == 0)
 	    break;
 	else
-	    ptr = ptr->next;
-    return ptr;
+	    ptrx = ptrx->next;
+    return ptrx;
 }
-struct dlist *removeitem(item, ptr)
-char *item;
-struct dlist *ptr;
+
+struct dlist *removeitem(char *item, struct dlist *ptrx)
 {
-    struct dlist *prev = NULL, *start = ptr;
-    while (ptr != NULL) 
-	if (my_stricmp(item, ptr->name) == 0) {
-	    newobj = ptr->next;
-	    if (object == ptr)
+    struct dlist *prev = NULL, *start = ptrx;
+    while (ptrx != NULL)
+	if (my_stricmp(item, ptrx->name) == 0) {
+	    newobj = ptrx->next;
+	    if (object == ptrx)
 		object = NULL;
-	    free (ptr);
+	    free (ptrx);
 	    if (prev == NULL)
 		return newobj;
 	    else {
@@ -173,13 +206,13 @@ struct dlist *ptr;
 		return start;
 	    }
 	} else {
-	    prev = ptr;
-	    ptr = ptr->next;
+	    prev = ptrx;
+	    ptrx = ptrx->next;
 	}
     return start;
 }
-int makeconnect(hostname)
-char *hostname;
+
+int makeconnect(const char *hostname)
 {
     struct sockaddr_in sa;
     struct hostent *hp;
@@ -201,14 +234,14 @@ char *hostname;
     return s;
 }
 
-int sendline()
+int sendline(void)
 {
     if (write(sockfd, lineout, strlen(lineout)) < 1)
 	return 0;
     return 1;
 }
 
-void updatestatus()
+void updatestatus(void)
 {
     int n;
     if (!dumb) {
@@ -227,22 +260,25 @@ void updatestatus()
     }
 }
 
-static int nop()
+static int nop(void)
 {
     return 1;
 }
-static int doerror()
+
+static int doerror(void)
 {
     column = printf("*** ERROR:");
     return 2;
 }
-static int doinvite()
+
+static int doinvite(void)
 {
     printf("*** %s (%s) invites you to join %s.",
 	   tok_in[0], fromhost, &tok_in[2][1]);
     return 0;
 }
-static int dojoin()
+
+static int dojoin(void)
 {
     if (strcmp(tok_in[0], IRCNAME) == 0) {
 	object = objlist = additem(tok_in[2], objlist);
@@ -252,7 +288,8 @@ static int dojoin()
 	printf("*** %s (%s) joined %s", tok_in[0], fromhost, tok_in[2]);
     return 0;
 }
-static int dokick()
+
+static int dokick(void)
 {
     printf("*** %s was kicked from %s by %s (%s)",
 	   tok_in[3], tok_in[2], tok_in[0], tok_in[4]);
@@ -266,17 +303,20 @@ static int dokick()
     }
     return 0;
 }
-static int dokill()
+
+static int dokill(void)
 {
     printf("*** %s killed by %s (%s)", tok_in[3], tok_in[0], tok_in[4]);
     return 0;
 }
-static int domode()
+
+static int domode(void)
 {
     printf("*** %s changed %s to:", tok_in[0], tok_in[2]);
     return 3;
 }
-static int donick()
+
+static int donick(void)
 {
     if (strcmp(tok_in[0], IRCNAME) == 0) {
 	wasdate = 0;
@@ -285,15 +325,17 @@ static int donick()
     printf("*** %s is now known as %s", tok_in[0], tok_in[2]);
     return 0;
 }
-static int donotice()
+
+static int donotice(void)
 {
     if (*tok_in[2] != '#')
 	column = printf("-%s-", tok_in[0]);
-    else 
+    else
 	column = printf("-%s:%s-", tok_in[0], tok_in[2]);
     return 3;
 }
-static int dopart()
+
+static int dopart(void)
 {
     printf("*** %s (%s) left %s", tok_in[0], fromhost,
 	   tok_in[2]);
@@ -307,12 +349,14 @@ static int dopart()
     }
     return 0;
 }
-static int dopong()
+
+static int dopong(void)
 {
     column = printf("*** Got PONG from %s:", tok_in[0]);
     return 3;
 }
-static int doprivmsg()
+
+static int doprivmsg(void)
 {
     if (*tok_in[2] != '#')
 	column = printf("*%s*", tok_in[0]);
@@ -322,28 +366,31 @@ static int doprivmsg()
 	column = printf("<%s>", tok_in[0]);
     return 3;
 }
-static int doquit()
+
+static int doquit(void)
 {
     printf("*** %s (%s) Quit (%s)", tok_in[0], fromhost, tok_in[2]);
     return 0;
 }
-static int dosquit()
+
+static int dosquit(void)
 {
     return 1;
 }
-static int dotime()
+
+static int dotime(void)
 {
     return 1;
 }
-static int dotopic()
+
+static int dotopic(void)
 {
     printf("*** %s set %s topic to \"%s\"", tok_in[0], tok_in[2],
 	   tok_in[3]);
     return 0;
 }
 
-int donumeric(num)
-int num;
+int donumeric(int num)
 {
     if (num == 352) {		/* rpl_whoreply */
 	column = printf("%-14s %-10s %-3s %s@%s :", tok_in[3], tok_in[7],
@@ -383,7 +430,7 @@ int num;
 #define	DO_QUOTE	31
 #define	DO_W		46
 #define	DO_WHOIS	49
-static char *cmdlist[LISTSIZE] =
+static const char *cmdlist[LISTSIZE] =
 {"AWAY", "ADMIN", "CONNECT", "CLOSE", "DIE", "DNS", "ERROR", "HELP",
  "HASH", "INVITE", "INFO", "ISON", "JOIN", "KICK", "KILL", "LIST", "LINKS",
  "LUSERS", "MSG", "MODE", "MOTD", "NOTICE", "NICK", "NAMES", "NOTE", "OPER",
@@ -398,7 +445,7 @@ static int numargs[LISTSIZE] =
  3, 2, 1, 3, 1, 2, 2, 4,
  1, 1, 1, 1, 1, 1, 1, 1
 };
-static int (*docommand[LISTSIZE]) () =
+static int (*docommand[LISTSIZE]) (void) =
 {nop, nop, nop, nop, nop, nop, doerror, nop,
  nop, doinvite, nop, nop, dojoin, dokick, dokill, nop, nop,
  nop, nop, domode, nop, donotice, donick, nop, nop, nop,
@@ -407,22 +454,21 @@ static int (*docommand[LISTSIZE]) () =
  nop, nop, nop, nop, nop, nop, nop, nop
 };
 
-int wordwrapout(ptr, count)
-char *ptr;
-int count;
+int wordwrapout(char *ptrx, size_t count)
 {
-    while(ptr != NULL) {
-	if ((tmp = strchr(ptr, ' ')) != NULL)
+    while(ptrx != NULL) {
+	if ((tmp = strchr(ptrx, ' ')) != NULL)
 	    *(tmp++) = '\0';
-	if (strlen(ptr) < CO - count)
-	    count += printf(" %s", ptr);
+	if (strlen(ptrx) < CO - count)
+	    count += printf(" %s", ptrx);
 	else
-	    count = printf("\n\r   %s", ptr);
-	ptr = tmp;
+	    count = printf("\n\r   %s", ptrx);
+	ptrx = tmp;
     }
     return count;
 }
-int parsedata()
+
+int parsedata(void)
 {
     int i, found = 0;
 
@@ -480,8 +526,9 @@ int parsedata()
     return 1;
 }
 
-int serverinput()
-{   char ich;
+int serverinput(void)
+{
+    char ich;
     while (read(sockfd, &ich, 1) > 0)
 	if (ich != '\n' && ich != '\r')
 	    serverdata[cursd++] = ich;
@@ -495,7 +542,7 @@ int serverinput()
     return 1;
 }
 
-void parseinput()
+void parseinput(void)
 {
     int i, j, outcol, found = 0;
 
@@ -511,7 +558,7 @@ void parseinput()
     }
     if (*tok_out[0] == COMMANDCHAR) {
 	tok_out[0]++;
-	for (i = 0; i < strlen(tok_out[0]) && isalpha(tok_out[0][i]); i++)
+	for (i = 0; (size_t)i < strlen(tok_out[0]) && isalpha(tok_out[0][i]); i++)
 	    tok_out[0][i] = toupper(tok_out[0][i]);
 	for (i = 0; i < LISTSIZE && !found; i++)
 	    found = (strncmp(cmdlist[i], tok_out[0], strlen(tok_out[0])) == 0);
@@ -559,7 +606,7 @@ void parseinput()
     idletimer = time(NULL);
 }
 
-void histupdate()
+void histupdate(void)
 {
     linein = history[histline];
     curx = curli = strlen(linein);
@@ -568,8 +615,7 @@ void histupdate()
     tputs_x(CE);
 }
 
-void printpartial(fromx)
-int fromx;
+void printpartial(int fromx)
 {
     int i;
     tputs_x(tgoto(CM, 0, LI - 1));
@@ -578,7 +624,8 @@ int fromx;
     tputs_x(CE);
     tputs_x(tgoto(CM, curx % CO, LI - 1));
 }
-void userinput()
+
+void userinput(void)
 {
     int i, z;
     char ch;
@@ -692,8 +739,7 @@ void userinput()
     }
 }
 
-void cleanup(sig)
-int sig;
+void cleanup(int sig)
 {
     tputs_x(tgoto(CS, -1, -1));
     tputs_x(tgoto(CM, 0, LI - 1));
@@ -703,9 +749,7 @@ int sig;
 }
 
 int
-main(argc, argv)
-int argc;
-char **argv;
+main(int argc, char *argv[])
 {
     char hostname[64];
     int i;
@@ -761,23 +805,23 @@ char **argv;
 	    fprintf(stderr, "tinyirc: no termcap entry for %s\n", term);
 	    exit(1);
 	}
-	if ((CO = tgetnum("co") - 2) < 1)
+	if ((CO = tgetnum(s_co) - 2) < 1)
 	    CO = 78;
-	if ((LI = tgetnum("li")) == -1)
+	if ((LI = tgetnum(s_li)) == -1)
 	    LI = 24;
-	if ((CM = (char *) tgetstr("cm", &ptr)) == NULL)
-	    CM = (char *) tgetstr("CM", &ptr);
-	if ((SO = (char *) tgetstr("so", &ptr)) == NULL)
+	if ((CM = tgetstr(s_cm, &ptr)) == NULL)
+	    CM = tgetstr(s_CM, &ptr);
+	if ((SO = tgetstr(s_so, &ptr)) == NULL)
 	    SO = "";
-	if ((SE = (char *) tgetstr("se", &ptr)) == NULL)
+	if ((SE = tgetstr(s_se, &ptr)) == NULL)
 	    SE = "";
-	if (!CM || !(CS = (char *) tgetstr("cs", &ptr)) ||
-	    !(CE = (char *) tgetstr("ce", &ptr))) {
+	if (!CM || !(CS = tgetstr(s_cs, &ptr)) ||
+	    !(CE = tgetstr(s_ce, &ptr))) {
 	    printf("tinyirc: sorry, no termcap cm,cs,ce: dumb mode set\n");
 	    dumb = 1;
 	}
 	if (!dumb) {
-	    DC = (char *) tgetstr("dc", &ptr);
+	    DC = tgetstr(s_dc, &ptr);
 	    wasdate = 0;
 	    signal(SIGINT, cleanup);
 	    signal(SIGHUP, cleanup);
@@ -792,7 +836,7 @@ char **argv;
 	}
     }
     for (i = 0; i < HISTLEN; i++)
-	history[i] = (char *) calloc(512, sizeof(char));
+	history[i] = calloc(512, sizeof(char));
     linein = history[histline = 0];
     while (sok && !beenden) {
 	FD_ZERO(&readfs);
