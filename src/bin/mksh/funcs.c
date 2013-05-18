2f0f3961f0a806a7485c62035a21bdaf219d7f15
tg <>
2013-05-18 17:13:32 +0000

    remove some debugging code

--- src/bin/mksh/funcs.c
+++ src/bin/mksh/funcs.c
@@ -25,7 +25,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.147 2009/12/12 22:27:07 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/funcs.c,v 1.148 2010/01/25 14:11:26 tg Exp $");
 
 #if HAVE_KILLPG
 /*
@@ -2661,10 +2661,6 @@ c_mknod(const char **wp)
 		umask(oldmode);
 	return (rv);
  c_mknod_usage:
-#if 0
-	/* XXX doesn't help */
-	builtin_argv0 = NULL;
-#endif
 	bi_errorf("usage: mknod [-m mode] name [b | c] major minor");
 	bi_errorf("usage: mknod [-m mode] name p");
 	return (1);
