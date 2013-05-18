16216da15f52687abe6682e20252755345411e00
tg <>
2013-05-18 15:45:58 +0000

    • cover for   | checking for pkg-config... /usr/mpkg/bin/pkg-config   | checking for sigc++-2.0 libcurl >= 7.12.0 libtorrent >= 0.11.3... Unknown keyword 'URL' in '/usr/mpkg/lib/pkgconfig/libcurl.pc'   by adding dummy support for said keyword • bump to pkgconfig-0.15.0-MirPorts-2 • pkgconfig.port.mk: require pkgconfig->=0.15.0-2 (force the user to upgrade)

--- ports/devel/pkgconfig/pkgconfig.port.mk
+++ ports/devel/pkgconfig/pkgconfig.port.mk
@@ -1,7 +1,7 @@
-# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.1 2005/12/28 00:40:19 bsiegert Exp $
+# $MirOS: ports/devel/pkgconfig/pkgconfig.port.mk,v 1.2 2007/05/08 22:38:58 tg Exp $
 #
 # module file for ports that use pkgconfig
 
-BUILD_DEPENDS+=		:pkgconfig-*:devel/pkgconfig
+BUILD_DEPENDS+=		:pkgconfig->=0.15.0-2:devel/pkgconfig
 PKG_CONFIG_PATH?=	${LOCALBASE}/lib/pkgconfig:${X11BASE}/lib/pkgconfig:/usr/share/pkgconfig
 CONFIGURE_ENV+=		PKG_CONFIG_PATH=${PKG_CONFIG_PATH:Q}
