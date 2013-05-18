25a12fcee56b210ea1f047cc02dcb6fcbfc4c1ab
tg <>
2013-05-18 16:20:52 +0000

    • preserve RCS IDs • indent style(9)

--- src/usr.bin/lex/mkskel.sh
+++ src/usr.bin/lex/mkskel.sh
@@ -1,5 +1,5 @@
 #!/bin/mksh
-# $MirOS$
+# $MirOS: src/usr.bin/lex/mkskel.sh,v 1.2 2005/07/07 13:39:50 tg Exp $
 # $OpenBSD: mkskel.sh,v 1.2 1996/06/26 05:35:39 deraadt Exp $
 
 
@@ -11,9 +11,14 @@ cat <<!
 const char *skel[] = {
 !
 
-sed 's/\\/&&/g' $* | sed 's/"/\\"/g' | sed 's/.*/  "&",/'
+sed \
+    -e 's/\$Mir''OS:/$miros:/g' \
+    -e 's/\\/&&/g' \
+    -e 's/"/\\"/g' \
+    -e 's/.*/	"&",/' \
+    "$@"
 
 cat <<!
-  0
+	NULL
 };
 !
