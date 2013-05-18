25fb36c3a4d008910d256f6e1036d65520f8b446
tg <>
2013-05-18 16:12:02 +0000

    USE_PNG?=any ☺

--- ports/graphics/png/png.port.mk
+++ ports/graphics/png/png.port.mk
@@ -1,6 +1,21 @@
-# $MirOS: ports/graphics/png/png.port.mk,v 1.3 2006/06/25 01:56:00 tg Exp $
+# $MirOS: ports/graphics/png/png.port.mk,v 1.4 2006/11/03 22:06:15 tg Exp $
 
-.if !exists(/usr/lib/libpng.a)
+# Valid choices: any, base, port
+USE_PNG?=		any
+
+.if ${USE_PNG:L:Many}
+.  if ${OStype} != "MirBSD"
+USE_PNG=		port
+.  elif exists(/usr/lib/libncursesw.a)
+USE_PNG=		base
+.  else
+USE_PNG=		port
+.  endif
+.endif
+
+.if ${USE_PNG:L:Mport}
 LIB_DEPENDS+=		png::graphics/png
 CPPFLAGS+=		-I${LOCALBASE}/include/libpng
+.elif !${USE_PNG:L:Mbase}
+.  error USE_PNG=${USE_PNG:L} invalid
 .endif
