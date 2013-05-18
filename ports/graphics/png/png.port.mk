b0e3ead8be42157d025741647c5277a5ecdc5875
bsiegert <>
2013-05-18 14:54:31 +0000

    The port is called png, not libpng.
    How TF could this go undetected?

--- ports/graphics/png/png.port.mk
+++ ports/graphics/png/png.port.mk
@@ -1,4 +1,4 @@
-# $MirOS$
+# $MirOS: ports/graphics/png/png.port.mk,v 1.1 2006/02/09 11:11:51 tg Exp $
 
-LIB_DEPENDS+=		png::graphics/libpng
+LIB_DEPENDS+=		png::graphics/png
 CPPFLAGS+=		-I${LOCALBASE}/libpng
