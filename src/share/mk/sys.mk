8619bef1a2e4a232e0b9a561c9d066bb964c37f9
tg <>
2013-08-06 20:34:03 +0000

    bump

--- src/share/mk/sys.mk
+++ src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/sys.mk,v 1.165 2012/12/31 21:08:12 tg Exp $
+# $MirOS: src/share/mk/sys.mk,v 1.166 2013/05/31 23:27:51 tg Exp $
 # $OpenBSD: sys.mk,v 1.45 2005/03/07 00:06:00 deraadt Exp $
 # $NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 # @(#)sys.mk	5.11 (Berkeley) 3/13/91
@@ -21,7 +21,7 @@ OStype=		MirBSD
 # Sync these with <sys/param.h>
 unix=		We run ${OStype}.
 OSrev=		10		# MirOS version (major)
-OSrpl=		179		# MirOS patchlevel
+OSrpl=		180		# MirOS patchlevel
 OScompat=	3.5		# OpenBSD compatibility revision
 .if !defined(OSNAME) || empty(OSNAME)
 OSNAME!=	uname -s
