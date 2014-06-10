ac8295b42e2909622785841c1fc09e296267f13c
tg <>
2014-06-10 07:36:42 +0000

    I wonder if the OpenSSL developers are malicious or just utterly incompetent
    Debian #750103 contains the full story, and this is one of the rare
    OpenSSL bugs that are so old that even we are affected…

--- src/lib/libssl/src/crypto/rand/randfile.c
+++ src/lib/libssl/src/crypto/rand/randfile.c
@@ -60,6 +60,7 @@
 #define _XOPEN_SOURCE 500
 
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -80,6 +81,8 @@
 #else
 # include <sys/stat.h>
 #endif
+
+__RCSID("$MirOS$");
 
 #undef BUFSIZE
 #define BUFSIZE	1024
