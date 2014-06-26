58d9cc37f5f5b345e40d1483054c4d92006dfe0d
tg <>
2014-06-26 17:42:54 +0000

    move NELEM out of i18n.c

--- contrib/code/jupp/i18n.h
+++ contrib/code/jupp/i18n.h
@@ -1,10 +1,13 @@
-/* $MirOS: contrib/code/jupp/i18n.h,v 1.3 2013/05/31 23:27:18 tg Exp $ */
+/* $MirOS: contrib/code/jupp/i18n.h,v 1.4 2013/12/01 00:19:08 tg Exp $ */
 
 #ifndef _Ii18n
 #define _Ii18n 1
 
 #include "config.h"
 #include "types.h"
+
+/* from mksh */
+#define NELEM(a)	(sizeof(a) / sizeof((a)[0]))
 
 int joe_iswupper PARAMS((struct charmap *,int c));
 int joe_iswlower PARAMS((struct charmap *,int c));
