68e325d4fa097393d5e58f6ee5480bcb762cd2f8
tg <>
2014-10-03 17:32:12 +0000

    overhaul IFS handling, fix bugs reported by Stephane Chazelas and mikeserv
    now we’re at: 486 passed testsuite items, 0 failed
    ifs.sh still: # tests 6856 passed 6856 failed 0

--- src/bin/mksh/Build.sh
+++ src/bin/mksh/Build.sh
@@ -2316,7 +2316,7 @@ addsrcs '!' HAVE_STRLCPY strlcpy.c
 addsrcs USE_PRINTF_BUILTIN printf.c
 test 1 = "$USE_PRINTF_BUILTIN" && add_cppflags -DMKSH_PRINTF_BUILTIN
 test 1 = "$HAVE_CAN_VERB" && CFLAGS="$CFLAGS -verbose"
-add_cppflags -DMKSH_BUILD_R=502
+add_cppflags -DMKSH_BUILD_R=503
 
 $e $bi$me: Finished configuration testing, now producing output.$ao
 
