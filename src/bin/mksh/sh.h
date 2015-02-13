51a28e88aabbe15ea59ab29ff4ef598de86b75d3
tg <>
2015-02-13 12:51:33 +0000

    oksh sync; unsure if it applies (with us using old_source and all) but better be safe than sorry; it at least passes our testsuite
    NOTE: people who can figure out how afl can be used for mksh welcome! ;-)

--- src/bin/mksh/sh.h
+++ src/bin/mksh/sh.h
@@ -171,7 +171,7 @@
 #ifdef EXTERN
 __RCSID("$MirOS: src/bin/mksh/sh.h,v 1.704 2014/11/19 21:49:12 tg Exp $");
 #endif
-#define MKSH_VERSION "R50 2015/02/06"
+#define MKSH_VERSION "R50 2015/02/13"
 
 /* arithmetic types: C implementation */
 #if !HAVE_CAN_INTTYPES
