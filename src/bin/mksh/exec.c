8b42a67035d15aa5d6b41213f3a42cdcf1cc06ab
tg <>
2015-02-19 22:00:03 +0000

    basically unbreak use of $*/$@ in here documents
    a bit funny:
    with NUL IFS, $* behaves like dash ('ab') and $@ like bash/ksh93 ('a b')
    
    with WS or NWS IFS, all shells behave alike though

--- src/bin/mksh/exec.c
+++ src/bin/mksh/exec.c
@@ -1492,7 +1492,7 @@ hereinval(const char *content, int sub, char **resbuf, struct shf *shf)
 		if (yylex(sub) != LWORD)
 			internal_errorf("%s: %s", "herein", "yylex");
 		source = osource;
-		ccp = evalstr(yylval.cp, 0);
+		ccp = evalstr(yylval.cp, DOSCALAR);
 	}
 
 	if (resbuf == NULL)
