2f12733f5fa685502ece1cd6e4d940f100ca8659
tg <>
2015-04-19 14:40:09 +0000

    a bit oksh-inspired getn return value checking, plus some code optimisation

--- src/bin/mksh/exec.c
+++ src/bin/mksh/exec.c
@@ -1597,9 +1597,9 @@ do_selectargs(const char **ap, bool print_menu)
 		if (call_builtin(findcom("read", FC_BI), read_args, Tselect,
 		    false))
 			return (NULL);
-		s = str_val(global("REPLY"));
-		if (*s && getn(s, &i))
-			return ((i >= 1 && i <= argct) ? ap[i - 1] : null);
+		if (*(s = str_val(global("REPLY"))))
+			return ((getn(s, &i) && i >= 1 && i <= argct) ?
+			    ap[i - 1] : null);
 		print_menu = true;
 	}
 }
