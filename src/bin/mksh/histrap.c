dfa5008645696a029e20769184d1088fcda2b761
tg <>
2015-07-05 16:47:28 +0000

    when storing commands with embedded newlines, don’t take those as end of string (inspired fix from Debian #783978; bug in original pdksh COMPLEX_HISTORY code)

--- src/bin/mksh/histrap.c
+++ src/bin/mksh/histrap.c
@@ -634,11 +634,13 @@ void
 histsave(int *lnp, const char *cmd, bool dowrite MKSH_A_UNUSED, bool ignoredups)
 {
 	char **hp;
-	char *c, *cp;
+	char *c;
+	const char *ccp;
 
-	strdupx(c, cmd, APERM);
-	if ((cp = strchr(c, '\n')) != NULL)
-		*cp = '\0';
+	ccp = cmd + strlen(cmd);
+	while (ccp > cmd && ccp[-1] == '\n')
+		--ccp;
+	strndupx(c, cmd, ccp - cmd, APERM);
 
 	if (ignoredups && !strcmp(c, *histptr)
 #if !defined(MKSH_SMALL) && HAVE_PERSISTENT_HISTORY
