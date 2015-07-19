975adfdca38e7b5ff9851bc460b26243a3f25405
tg <>
2015-07-19 12:24:34 +0000

    take a page out of LibreSSL’s book and assume application writers are stupid… or “just” not interested in the innards of their SSL library: merge patch to load SHA2 by default, plus doc update, into MirSSL

--- src/lib/libssl/src/ssl/ssl_algs.c
+++ src/lib/libssl/src/ssl/ssl_algs.c
@@ -61,6 +61,8 @@
 #include <openssl/lhash.h>
 #include "ssl_locl.h"
 
+__RCSID("$MirOS$");
+
 int SSL_library_init(void)
 	{
 
@@ -94,17 +96,20 @@ int SSL_library_init(void)
 	EVP_add_digest(EVP_sha1()); /* RSA with sha1 */
 	EVP_add_digest_alias(SN_sha1,"ssl3-sha1");
 	EVP_add_digest_alias(SN_sha1WithRSAEncryption,SN_sha1WithRSA);
+#ifndef OPENSSL_NO_SHA256
+	EVP_add_digest(EVP_sha224());
+	EVP_add_digest(EVP_sha256());
+#endif
+#ifndef OPENSSL_NO_SHA512
+	EVP_add_digest(EVP_sha384());
+	EVP_add_digest(EVP_sha512());
+#endif
 #endif
 #if !defined(OPENSSL_NO_SHA) && !defined(OPENSSL_NO_DSA)
 	EVP_add_digest(EVP_dss1()); /* DSA with sha1 */
 	EVP_add_digest_alias(SN_dsaWithSHA1,SN_dsaWithSHA1_2);
 	EVP_add_digest_alias(SN_dsaWithSHA1,"DSS1");
 	EVP_add_digest_alias(SN_dsaWithSHA1,"dss1");
-#endif
-	/* If you want support for phased out ciphers, add the following */
-#if 0
-	EVP_add_digest(EVP_sha());
-	EVP_add_digest(EVP_dss());
 #endif
 	return(1);
 	}
