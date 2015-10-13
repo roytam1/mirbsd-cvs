1c514aa3f35da699390a6e51bf5c4e5439671d4f
cvs2git <admin@example.com>
2015-10-13 20:34:15 +0000

    This commit was manufactured by cvs2git to create tag 'paxmirabilis-20151013'.

--- ports/lang/ghc/ghc.port.mk
+++
@@ -1,9 +0,0 @@
-# $OpenBSD: ghc.port.mk,v 1.3 2004/04/26 23:48:50 dons Exp $
-# Module for Glasgow Haskell Compiler
-
-# Not yet ported to other architectures
-# See comments in lang/ghc/Makefile for more information
-ONLY_FOR_PLATFORM=	OpenBSD:*:i386 OpenBSD:*:sparc OpenBSD:*:amd64
-
-BUILD_DEPENDS+=	bin/ghc::lang/ghc
-RUN_DEPENDS+=	bin/ghc::lang/ghc
