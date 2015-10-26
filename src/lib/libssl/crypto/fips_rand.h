c478ee13bca65fc00f869ddbb8f6f614bcc3b130
tg <>
2015-10-26 21:54:02 +0000

    “"des.h"” is not unique enough; remove rather than fail to see any fallout

--- src/lib/libssl/crypto/fips_rand.h
+++ src/lib/libssl/crypto/fips_rand.h
@@ -1,9 +1,7 @@
-/* $MirOS$ */
+/* $MirOS: src/lib/libssl/crypto/fips_rand.h,v 1.1 2006/09/20 20:38:14 tg Exp $ */
 
 #ifndef HEADER_FIPS_RAND_H
 #define HEADER_FIPS_RAND_H
-
-#include "des.h"
 
 #ifdef OPENSSL_FIPS
 # error FIPS not supported!
