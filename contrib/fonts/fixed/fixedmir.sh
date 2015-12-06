2a41faef919bd8aa02f4e44b8b6938413edb8354
tg <>
2015-12-06 01:04:38 +0000

    new font release, we ship all mirf*.bdf now, but not the others, to not conflict with peoples’ X setup, unless they pull the sources from CVS

--- contrib/fonts/fixed/fixedmir.sh
+++ contrib/fonts/fixed/fixedmir.sh
@@ -8,3 +8,9 @@ ed -s mirf18hw.bdfc <fixedmir.ed
 ed -s mirf18fw.bdfc <fixedmir.ed
 bdfctool -Fd <mirf18hw.bdfc >mirf18hw.bdf
 bdfctool -Fd <mirf18fw.bdfc >mirf18fw.bdf
+[[ $1 = dist ]] || exit 0
+mkdir dist
+bdfctool -Fd <mirf16v8.bdfc >dist/mirf16v8.bdf
+mv mirf18hw.bdf mirf18fw.bdf dist/
+rm mirf18hw.bdfc mirf18fw.bdfc
+cp README.dist dist/README
