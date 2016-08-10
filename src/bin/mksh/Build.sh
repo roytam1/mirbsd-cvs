fe70a071db1d2f59b3c7251b0c15ea8941f2e5f4
tg <>
2016-08-10 18:20:19 +0000

    bump, required by XTaran

--- src/bin/mksh/Build.sh
+++ src/bin/mksh/Build.sh
@@ -2342,7 +2342,7 @@ addsrcs '!' HAVE_STRLCPY strlcpy.c
 addsrcs USE_PRINTF_BUILTIN printf.c
 test 1 = "$USE_PRINTF_BUILTIN" && add_cppflags -DMKSH_PRINTF_BUILTIN
 test 1 = "$HAVE_CAN_VERB" && CFLAGS="$CFLAGS -verbose"
-add_cppflags -DMKSH_BUILD_R=529
+add_cppflags -DMKSH_BUILD_R=530
 
 $e $bi$me: Finished configuration testing, now producing output.$ao
 
