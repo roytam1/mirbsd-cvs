2a727207a1587ad5d4bb9ef53b73213375087f8c
tg <>
2016-08-11 20:19:08 +0000

    completely re-do Fefe’s find_client_loc refactoring, but do it right instead

--- ports/net/djbdns/files/build/src/clientloc.c
+++ ports/net/djbdns/files/build/src/clientloc.c
@@ -1,39 +1,69 @@
+#include <sys/cdefs.h>
 #include "open.h"
 #include "byte.h"
 #include "cdb.h"
 #include "ip6.h"
 
-int find_client_loc(char loc[2],const char ip[16],struct cdb *cp)
+__RCSID("$MirOS$");
+
+char clientloc[2];
+
+static int
+try_find_client_loc(struct cdb *cp, const char key[], int len)
 {
-  int r, fd;
-  char key[32+3];
+	int r;
 
-  byte_zero(loc,2);
-  key[0] = 0;
-  key[1] = '%';
-  if (byte_equal(ip,12,V4mappedprefix)) {
-    key[2] = 'f';
-    byte_copy(key + 3,4,ip+12);
-    r = cdb_find(cp,key,7);
-    if (!r) r = cdb_find(cp,key,6);
-    if (!r) r = cdb_find(cp,key,5);
-    if (!r) r = cdb_find(cp,key,4);
-    if (!r) r = cdb_find(cp,key,3);
-    if (r == -1) return 0;
-    if (r && (cdb_datalen(cp) == 2))
-      if (cdb_read(cp,loc,2,cdb_datapos(cp)) == -1) return 0;
-  } else {
-    unsigned int n;
-    key[2] = 's';
-    ip6_fmt_flat(key+3,ip);
-    for (n=19; n>3; --n) {
-      r = cdb_find(cp,key,n);
-      if (r) break;
-    }
-    if (r == -1) return 0;
-    if (r && (cdb_datalen(cp) == 2))
-      if (cdb_read(cp,loc,2,cdb_datapos(cp)) == -1) return 0;
-  }
+	do {
+		r = cdb_find(cp, key, len);
+	} while (!r && --len >= 3);
 
-  return r;
+	if (r == -1)
+		/* read error */
+		return (-1);
+	if (r && (cdb_datalen(cp) == 2)) {
+		if (cdb_read(cp, clientloc, 2, cdb_datapos(cp)) == -1)
+			/* read error */
+			return (-1);
+		/* found */
+		return (0);
+	}
+	/* not found */
+	return (1);
+}
+
+int
+find_client_loc(const char ip[16], struct cdb *cp)
+{
+	char key[3 + 32];
+
+	byte_zero(clientloc, 2);
+	key[0] = 0;
+	key[1] = '%';
+	if (byte_equal(ip, 12, V4mappedprefix)) {
+		key[2] = 'f';
+		byte_copy(key + 3, 4, ip + 12);
+		switch (try_find_client_loc(cp, key, 3 + 4)) {
+		case -1:
+			/* read error */
+			return (-1);
+		case 0:
+			/* found */
+			return (0);
+		}
+		/* retry as v4-mapped */
+	}
+	key[2] = 's';
+	ip6_fmt_flat(key + 3, ip);
+	switch (try_find_client_loc(cp, key, 3 + 32)) {
+	case -1:
+		/* read error */
+		return (-1);
+	default:
+		/* no match */
+		byte_zero(clientloc, 2);
+		/* FALLTHROUGH */
+	case 0:
+		/* found */
+		return (0);
+	}
 }
--- ports/net/djbdns/files/build/src/clientloc.h
+++ ports/net/djbdns/files/build/src/clientloc.h
@@ -1,6 +1,8 @@
 #ifndef CLIENTLOC_H
 #define CLIENTLOC_H
 
-extern int find_client_loc(char loc[2],const char ip[16],struct cdb *);
+extern char clientloc[2];
+
+extern int find_client_loc(const char ip[16], struct cdb *);
 
 #endif
