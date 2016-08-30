8a0efb0db52af0f222dc0547785267c4a6d3074b
cvs2git <admin@example.com>
2016-08-30 19:33:52 +0000

    This commit was manufactured by cvs2git to create tag 'kwalletcli-3_00'.

--- ports/net/djbdns/files/build/src/maxclient.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAXCLIENT_H
-#define MAXCLIENT_H
-
-#define MAXUDP 200
-#define MAXTCP 20
-
-#endif /* MAXCLIENT_H */
--- src/lib/libssl/src/crypto/cast/c_skey.c
+++
@@ -1,9 +0,0 @@
-#include <openssl/cast.h>
-
-#undef CAST_set_key
-
-void
-CAST_set_key(CAST_KEY *key, int len, const unsigned char *data)
-{
-	cast_setkey(key, data, len);
-}
