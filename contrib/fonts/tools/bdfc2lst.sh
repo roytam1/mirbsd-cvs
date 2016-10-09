1b6c4269d27c3aa02a925c39673e66321ed380e6
tg <>
2016-10-09 18:31:57 +0000

    add extra flag to show each char’s width (as determined by mksh)

--- contrib/fonts/tools/bdfc2lst.sh
+++ contrib/fonts/tools/bdfc2lst.sh
@@ -1,10 +1,33 @@
-#!/bin/sh
-# $MirOS: contrib/fonts/fixed/bdfc2lst,v 1.1 2012/09/01 20:57:14 tg Exp $
+#!/bin/mksh
+# $MirOS: contrib/fonts/tools/bdfc2lst.sh,v 1.1 2016/10/09 18:11:57 tg Exp $
 #-
 # .bdfc -> .lst (list of characters contained within)
+
+if [[ $1 = -w ]]; then
+	# enable widths
+	alias postprocess='sort | addwidths'
+else
+	alias postprocess=sort
+fi
+
+function addwidths {
+	local hex s
+	local -i1 ch
+	set -U
+
+	while read hex; do
+		ch=0x$hex
+		if (( ch )); then
+			s=${ch#1#}
+			print -r -- "$hex ${%s}"
+		else
+			print -r -- "$hex 0"
+		fi
+	done
+}
 
 for i in *.bdfc; do
 	o=${i%.bdfc}.lst
 	sed -n '/^c \([0-9A-F][0-9A-F][0-9A-F][0-9A-F]\) .*$/s//\1/p' \
-	    <"$i" | sort >"$o"
+	    <"$i" | postprocess >"$o"
 done
