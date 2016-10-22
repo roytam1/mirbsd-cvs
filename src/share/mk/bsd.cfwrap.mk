05d79a4702b66fdd06f489cce87aa7b79c814f46
tg <>
2016-10-22 16:13:34 +0000

    automatically append to configure calls in <bsd.cfwrap.mk> the same we append from MirPorts’ <gnu.port.mk> on MirOS BSD

--- src/share/mk/bsd.cfwrap.mk
+++ src/share/mk/bsd.cfwrap.mk
@@ -1,4 +1,4 @@
-# $MirOS: src/share/mk/bsd.cfwrap.mk,v 1.24 2009/04/17 17:32:31 tg Exp $
+# $MirOS: src/share/mk/bsd.cfwrap.mk,v 1.25 2013/08/19 17:35:39 tg Exp $
 
 .if !defined(BSD_CFWRAP_MK)
 BSD_CFWRAP_MK=1
@@ -87,7 +87,16 @@ XARGS+=	INSTALL_PROGRAM=${INSTALL_PROGRAM:Q} INSTALL_DATA=${INSTALL_DATA:Q} \
 XVARS+=	CPPFLAGS=${CPPFLAGS:C/ *$//:Q} LDFLAGS=${LDFLAGS:Q}\ ${LDSTATIC:Q} \
 	INSTALL_STRIP=${INSTALL_STRIP:Q} GCC_HONOUR_COPTS=1
 
-_CFVARS+=${XVARS} ${XARGS} GCC_NO_WERROR=1 GCC_HONOUR_COPTS=0 ${CFVARS}
+_CFVARS+=${XVARS} ${XARGS} GCC_NO_WERROR=1 GCC_HONOUR_COPTS=0
+
+# reported to be misused by bsiegert@
+_CFVARS+=ac_cv_func_malloc_0_nonnull=yes
+# caught by systrace, tries to write to ${LOCALBASE}/lib as check
+_CFVARS+=ac_cv_sys_long_file_names=yes
+# cannot be used on MirBSD
+_CFVARS+=ac_cv_type_long_double=no ac_cv_sizeof_long_double=0
+
+_CFVARS+=${CFVARS}
 
 CFARGS+=--build=${OStriplet} --host=${GCCHOST} --srcdir=${FSFSRC}
 .if ${GCCTARGET} != ${GCCHOST}
