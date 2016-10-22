c165212b0ee7ff78192b14c0c713ca48e7eb0b74
tg <>
2016-10-22 20:15:03 +0000

    with the patched getdate.m4 we _can_ have the .y in the MAIN branch

--- src/gnu/usr.bin/cvs/lib/getdate.h
+++ src/gnu/usr.bin/cvs/lib/getdate.h
@@ -1,4 +1,4 @@
-/* $MirOS: src/gnu/usr.bin/cvs/lib/getdate.h,v 1.5 2010/09/19 19:42:59 tg Exp $ */
+/* $MirOS: src/gnu/usr.bin/cvs/lib/getdate.h,v 1.6 2016/10/21 23:28:49 tg Exp $ */
 
 /* Parse a string into an internal time stamp.
 
@@ -22,21 +22,12 @@
 #ifndef GETDATE_H
 #define GETDATE_H
 
-#if HAVE_CONFIG_H
-# include <config.h>
-#endif
-
 #include <stdbool.h>
 #ifndef IN_RCS
 #include "timespec.h"
 #else
 #include <sys/time.h>
 #include <time.h>
-#define gettime(ts)	clock_gettime(CLOCK_REALTIME,(ts))
-#ifndef HAVE_CONFIG_H
-#define HAVE_TM_GMTOFF		1
-#define HAVE_STRUCT_TM_TM_ZONE	1
-#endif
 #endif
 
 bool get_date (struct timespec *, char const *, struct timespec const *);
