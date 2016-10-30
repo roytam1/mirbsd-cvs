5c009caa7a0fce2a8cc877c7c60a009e4143bc00
tg <>
2016-10-30 00:19:58 +0000

    sprinke some const-cleanliness

--- contrib/code/jupp/vs.c
+++ contrib/code/jupp/vs.c
@@ -146,7 +146,7 @@ sELEMENT *_vsset(sELEMENT *vary, int pos, sELEMENT el)
 	return vary;
 }
 
-int vsbsearch(sELEMENT *ary, int len, sELEMENT el)
+int vsbsearch(const sELEMENT *ary, int len, sELEMENT el)
 {
 	int x, y, z;
 
@@ -201,7 +201,7 @@ int vscmp(sELEMENT *a, sELEMENT *b)
 	return vscmpn(sv(a), sv(b));
 }
 
-int vsscan(sELEMENT *a, int alen, sELEMENT *b, int blen)
+int vsscan(const sELEMENT *a, int alen, const sELEMENT *b, int blen)
 {
 	int x;
 
