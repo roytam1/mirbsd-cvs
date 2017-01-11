84697b9da8316f14a7cb68a4658472f84393771d
cvs2git <admin@example.com>
2017-01-11 22:56:51 +0000

    This commit was manufactured by cvs2git to create tag 'joe-3_1jupp30'.

--- src/gnu/usr.bin/perl/ext/POSIX/hints/mirbsd.pl
+++
@@ -1,5 +0,0 @@
-# $MirOS$
-#
-# BSD platforms have extra fields in struct tm that need to be initialized.
-#  XXX A Configure test is needed.
-$self->{CCFLAGS} = $Config{ccflags} . ' -DSTRUCT_TM_HASZONE' ;
--- src/share/tmac/tab/tabcolhl.c
+++
@@ -1,2 +0,0 @@
-#define col_halflines
-#include "tabcol.c"
