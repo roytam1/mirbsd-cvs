eb3d0031c41f874a2438a1c7596e7e594038abad
tg <>
2017-04-03 02:38:53 +0000

    bump

--- gcc/gcc/version.c
+++ gcc/gcc/version.c
@@ -1,4 +1,4 @@
-/* $MirOS: gcc/gcc/version.c,v 1.56 2015/01/23 19:17:58 tg Exp $ */
+/* $MirOS: gcc/gcc/version.c,v 1.57 2016/07/27 00:04:38 tg Exp $ */
 
 #include "version.h"
 
@@ -7,7 +7,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.4.6 (propolice; gpc; MirOS 0AB7)";
+const char version_string[] = "3.4.6 (propolice; gpc; MirOS 0AB8)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
