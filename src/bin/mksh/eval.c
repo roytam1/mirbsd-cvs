ca391666748c41079651f74ef14302bc6c48b956
tg <>
2017-08-28 23:27:51 +0000

    quell a Fink compiler warning; not entirely correct but no harm done either

--- src/bin/mksh/eval.c
+++ src/bin/mksh/eval.c
@@ -23,7 +23,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/eval.c,v 1.211 2017/05/03 15:33:16 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/eval.c,v 1.214 2017/05/05 22:53:27 tg Exp $");
 
 /*
  * string expansion
@@ -637,7 +637,7 @@ expand(
 						 */
 						if (!Flag(FSH)) {
 							*dp++ = MAGIC;
-							*dp++ = 0x80 | '@';
+							*dp++ = ord(0x80 | '@');
 						}
 						break;
 					case ord('='):
