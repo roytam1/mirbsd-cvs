1f4d12fbbebb006586df3161727b16d6fe244cda
cvs2git <admin@example.com>
2017-08-29 13:38:32 +0000

    This commit was manufactured by cvs2git to create tag 'mksh-R56b'.

--- src/lib/libssl/crypto/fips_rand.h
+++
@@ -1,9 +0,0 @@
-/* $MirOS: src/lib/libssl/crypto/fips_rand.h,v 1.1 2006/09/20 20:38:14 tg Exp $ */
-
-#ifndef HEADER_FIPS_RAND_H
-#define HEADER_FIPS_RAND_H
-
-#ifdef OPENSSL_FIPS
-# error FIPS not supported!
-#endif
-#endif
