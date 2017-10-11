0abdb8a9bf2d977569eab8a13c90c88c7b58870b
tg <>
2017-10-11 19:06:44 +0000

    turns out the slash was already added (later), 10x komh

--- src/bin/mksh/misc.c
+++ src/bin/mksh/misc.c
@@ -1789,11 +1789,9 @@ do_realpath(const char *upath)
 					/* keep it */
 					Xput(xs, xp, *ip++);
 					Xput(xs, xp, *ip++);
-					/* as well as a leading (back)slash */
-					if (mksh_cdirsep(*ip))
-						Xput(xs, xp, *ip++);
 					/*
-					 * XXX else: get the cwd on that drive
+					 * XXX if (!mksh_cdirsep(*ip)): we
+					 * XXX must get the cwd on that drive
 					 * XXX and prepend it here as this is
 					 * XXX a drive-qualified relative path
 					 * XXX which we are supposed to convert
