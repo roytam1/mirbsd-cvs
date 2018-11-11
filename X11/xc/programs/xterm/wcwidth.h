c6f0aead3abb5c67c87b0291c64eb5fab28614e9
cvs2git <admin@example.com>
2018-11-11 18:56:02 +0000

    This commit was manufactured by cvs2git to create tag 'joe-3_1jupp38'.

--- X11/xc/programs/xterm/wcwidth.h
+++
@@ -1,9 +0,0 @@
-/* $MirOS$ */
-
-#ifndef XC_PROGRAMS_XTERM_WCWIDTH_H
-#define XC_PROGRAMS_XTERM_WCWIDTH_H
-
-extern int mb_wcwidth(unsigned int);
-extern int mb_wcwidth_cjk(unsigned int);
-
-#endif
--- ports/net/djbdns/files/build/src/clientloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENTLOC_H
-#define CLIENTLOC_H
-
-extern char clientloc[2];
-
-extern int find_client_loc(const char ip[16], struct cdb *);
-
-#endif
